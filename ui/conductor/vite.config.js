498904b8dc3180d12e18495032c863e487ee56e6
Matt Nathan <matt.nathan@vanti.co.uk>
2022-08-25 11:03:49 +0100

    fix(ui): update all dependencies which fixes some vue 2.7 issues in dependencies

--- ui/conductor/vite.config.js
+++ ui/conductor/vite.config.js
@@ -1,17 +1,14 @@
-import {URL, fileURLToPath} from 'url';
-import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue2';
-import Components from 'unplugin-vue-components/vite';
 import {VuetifyResolver} from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+import {fileURLToPath, URL} from 'url';
+import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-      // unplugin-vue-components hasn't get updated to support @vitejs/plugin-vue2, it looks for vite-plugin-vue2 to
-      // set this for you.
-      transformer: 'vue2',
       resolvers: [
         VuetifyResolver()
       ]
