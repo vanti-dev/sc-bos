c90fae1fd8fe508f4ef6bec0a317561d8d0679cb
Matt Nathan <matt.nathan@vanti.co.uk>
2022-10-11 11:44:05 +0100

    test: add test for pass.Compare

---
+++ internal/util/pass/compare_test.go
@@ -0,0 +1,28 @@
+package pass
+
+import "testing"
+
+func TestCompare(t *testing.T) {
+	tests := []struct {
+		name    string
+		secret  string
+		wantErr bool
+	}{
+		{name: "empty", secret: ""},
+		{name: "pass1", secret: "hello"},
+		{name: "pass2", secret: "0928u3j@£F@*DF@N 23d9n2f3oAS"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for i := 0; i < 5; i++ {
+				hash, err := Hash([]byte(tt.secret))
+				if err != nil {
+					t.Errorf("hash failed %v", err)
+				}
+				if err := Compare(hash, []byte(tt.secret)); (err != nil) != tt.wantErr {
+					t.Errorf("Compare() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			}
+		})
+	}
+}
