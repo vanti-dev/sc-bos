d629439858f834e5dc98599a1eaff7a8f9c087b0
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2022-11-15 16:22:22 +0200

    feat(ui): make footer text color more visible

--- ui/conductor/src/components/Footer.vue
+++ ui/conductor/src/components/Footer.vue
@@ -2,9 +2,7 @@
   <v-footer absolute color="#111721">
     <v-col class="text-center" cols="12">
       <v-divider></v-divider>
-      <p class="my-2 text-caption" style="color: #292f36">
-        Smart Core v2022.11
-      </p>
+      <p class="my-2 text-caption">Smart Core v2022.11</p>
     </v-col>
   </v-footer>
 </template>
