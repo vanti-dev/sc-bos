5ba62a29c5bdba56b3eb021aa1aeddf26a61fc10
Matt Randell <Matt.Randell@vanti.co.uk>
2022-11-22 17:39:42 +0000

    dev(auto/testlight): new data structures for emergency light log
    Data stored by testlight automation changed.
    
    The automation stores the most recent status for each emergency light, keyed by the light's name.
    This facilitates common queries like "is light X OK right now" and "when was the data for light X last updated".
    
    There is also a log which stores events:
      - function test passes
      - duration test passes
      - status changes (only recorded when the latest status is different from the previous status to avoid cluttering the DB with useless data)
    
    New structure allows runner.process to be more straightforward and easy to follow. Refactored the polling into pollDaliEmergency.
    
    Added some utilities for dealing with slices of faults. Protobuf requires them to be slices, but semantically they are sets, so added utility functions for order- and count-independent comparisons, and normalisation.

--- internal/auto/testlight/auto.go
+++ internal/auto/testlight/auto.go
@@ -15,6 +15,8 @@ import (
 	"github.com/vanti-dev/bsp-ew/internal/task"
 	"github.com/vanti-dev/bsp-ew/internal/util/minibus"
 	"github.com/vanti-dev/bsp-ew/internal/util/state"
+	"github.com/vanti-dev/bsp-ew/pkg/gen"
+	"go.etcd.io/bbolt"
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
@@ -125,77 +127,94 @@ type runner struct {
 	configUpdates <-chan Config
 }
 
-func (r *runner) process(ctx context.Context, name string, test rpc.Test) error {
+func (r *runner) process(ctx context.Context, name string) error {
 	var client rpc.DaliApiClient
 	err := r.clients.Client(&client)
 	if err != nil {
 		return err
 	}
-
 	scanTime := time.Now()
-	result, err := client.GetTestResult(ctx, &rpc.GetTestResultRequest{
-		Name: name,
-		Test: test,
-	})
-	if code := status.Code(err); code == codes.NotFound {
-		result = nil
-	} else if err != nil {
-		return fmt.Errorf("GetTestResult: %w", err)
-	}
+	logger := r.logger.With(zap.String("deviceName", name))
 
-	// we have successfully scanned for new data, so update last scan time
-	lastScan, _, err := updateScanTime(r.db, name, test, scanTime)
-	if err != nil {
-		r.logger.Warn("failed to update last scan time - test After times may be inaccurate",
-			zap.Error(err), zap.String("deviceName", name), zap.Any("test", test))
-	}
+	result := pollDaliEmergency(ctx, client, name)
 
-	if result != nil {
-		r.logger.Debug("emergency light has some test data",
-			zap.String("deviceName", name),
-			zap.String("test", test.String()),
-			zap.Bool("pass", result.Pass))
-
-		// store the test in the database
-		record := TestResultRecord{
-			Name:     name,
-			Kind:     test,
-			After:    lastScan,
-			Before:   scanTime,
-			TestPass: result.Pass,
-		}
-		if result.Duration != nil {
-			record.AchievedDuration = result.Duration.AsDuration()
-		}
-		err = saveTestResult(r.db, record)
+	// update the database with the test results
+	err = r.db.Bolt().Update(func(tx *bbolt.Tx) error {
+		// update the status for the light
+		statusChanged, err := updateLatestStatus(r.db, tx, name, scanTime, result.faults)
 		if err != nil {
-			return fmt.Errorf("store test result: %w", err)
+			logger.Error("error updating emergency light latest status", zap.Error(err),
+				zap.Any("faults", result.faults))
+			return err
 		}
 
-		if record.TestPass {
-			// clear the test result from the emergency light now we have safely captured it
-			// this can only be done for passes; failure status can only be cleared by a subsequent pass
-			_, err = client.DeleteTestResult(ctx, &rpc.DeleteTestResultRequest{
-				Name: name,
-				Test: test,
-			})
+		// if the status has changed, record it in the log
+		if statusChanged {
+			_, err = saveStatusReport(r.db, tx, name, scanTime, result.faults)
 			if err != nil {
-				return fmt.Errorf("DeleteTestResult: %w", err)
+				logger.Error("failed to save emergency light status report", zap.Error(err))
+				return err
 			}
 		}
+
+		// record any test passes
+		if result.functionTestPass {
+			_, err = saveFunctionTestPass(r.db, tx, name, scanTime)
+			if err != nil {
+				logger.Error("failed to save emergency light function test pass", zap.Error(err))
+				return err
+			}
+		}
+		if result.durationTestPass {
+			_, err = saveDurationTestPass(r.db, tx, name, scanTime, result.durationTestResult)
+			if err != nil {
+				logger.Error("failed to save emergency light duration test pass", zap.Error(err),
+					zap.Duration("result", result.durationTestResult))
+				return err
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		r.logger.Error("database transaction error", zap.Error(err))
+		return err
 	}
 
-	return nil
+	// now we have saved test passes in the database, we can delete them from the light without data loss
+	var errs error
+	if result.functionTestPass {
+		_, err = client.DeleteTestResult(ctx, &rpc.DeleteTestResultRequest{
+			Name: name,
+			Test: rpc.Test_FUNCTION_TEST,
+		})
+		if err != nil {
+			logger.Warn("couldn't delete function test pass from light; duplicate records may occur", zap.Error(err))
+			errs = multierr.Append(errs, err)
+		}
+	}
+	if result.durationTestPass {
+		_, err = client.DeleteTestResult(ctx, &rpc.DeleteTestResultRequest{
+			Name: name,
+			Test: rpc.Test_DURATION_TEST,
+		})
+		if err != nil {
+			logger.Warn("couldn't delete duration test pass from light; duplicate records may occur", zap.Error(err))
+			errs = multierr.Append(errs, err)
+		}
+	}
+
+	return errs
 }
 
-func (r *runner) runOneLoop(ctx context.Context, test rpc.Test) error {
+func (r *runner) runOneLoop(ctx context.Context) error {
 	var errs error
 
 	ticker := time.NewTicker(r.config.PollInterval.Duration)
 	defer ticker.Stop()
 
 	for _, name := range r.config.Devices {
-		err := r.process(ctx, name, test)
+		err := r.process(ctx, name)
 		if err != nil {
 			errs = multierr.Append(errs, fmt.Errorf("%q: %w", name, err))
 		}
@@ -214,14 +233,9 @@ func (r *runner) run(ctx context.Context) {
 	ticker := time.NewTicker(r.config.CycleInterval.Duration)
 	defer ticker.Stop()
 	for {
-		err := r.runOneLoop(ctx, rpc.Test_DURATION_TEST)
+		err := r.runOneLoop(ctx)
 		if err != nil {
-			r.logger.Error("errors retrieving duration test results", zap.Error(err))
-		}
-
-		err = r.runOneLoop(ctx, rpc.Test_FUNCTION_TEST)
-		if err != nil {
-			r.logger.Error("errors retrieving function test results", zap.Error(err))
+			r.logger.Error("errors polling for emergency test results", zap.Error(err))
 		}
 
 		select {
@@ -236,4 +250,72 @@ func (r *runner) run(ctx context.Context) {
 		}
 
 	}
+}
+
+type pollResult struct {
+	faults             []gen.EmergencyLightFault
+	errors             error
+	functionTestPass   bool
+	durationTestPass   bool
+	durationTestResult time.Duration // non-zero if durationTestPass is true
+}
+
+// gets new data from an emergency light
+// if communication fails completely, no error is returned
+func pollDaliEmergency(ctx context.Context, client rpc.DaliApiClient, name string) pollResult {
+	var result pollResult
+
+	// get the emergency light status
+	emStatus, err := client.GetEmergencyStatus(ctx, &rpc.GetEmergencyStatusRequest{Name: name})
+	if err != nil {
+		result.faults = []gen.EmergencyLightFault{gen.EmergencyLightFault_COMMUNICATION_FAILURE}
+		result.errors = err
+		return result
+	} else {
+		result.faults = translateFaults(emStatus.Failures)
+	}
+
+	// check for any test passes
+	funcRes, err := client.GetTestResult(ctx, &rpc.GetTestResultRequest{
+		Name: name,
+		Test: rpc.Test_FUNCTION_TEST,
+	})
+	if err == nil {
+		result.functionTestPass = funcRes.Pass
+	} else if status.Code(err) != codes.NotFound {
+		result.errors = multierr.Append(result.errors, err)
+	}
+
+	durRes, err := client.GetTestResult(ctx, &rpc.GetTestResultRequest{
+		Name: name,
+		Test: rpc.Test_DURATION_TEST,
+	})
+	if err == nil {
+		result.durationTestPass = durRes.Pass
+		result.durationTestResult = durRes.Duration.AsDuration()
+	} else if status.Code(err) != codes.NotFound {
+		result.errors = multierr.Append(result.errors, err)
+	}
+
+	return result
+}
+
+func translateFaults(daliFailures []rpc.EmergencyStatus_Failure) (faults []gen.EmergencyLightFault) {
+	for _, failure := range daliFailures {
+		switch failure {
+		case rpc.EmergencyStatus_DURATION_TEST_FAILED:
+			faults = append(faults, gen.EmergencyLightFault_DURATION_TEST_FAILED)
+		case rpc.EmergencyStatus_FUNCTION_TEST_FAILED:
+			faults = append(faults, gen.EmergencyLightFault_FUNCTION_TEST_FAILED)
+		case rpc.EmergencyStatus_BATTERY_DURATION_FAILURE, rpc.EmergencyStatus_BATTERY_FAILURE:
+			faults = append(faults, gen.EmergencyLightFault_BATTERY_FAULT)
+		case rpc.EmergencyStatus_LAMP_FAILURE:
+			faults = append(faults, gen.EmergencyLightFault_LAMP_FAULT)
+		default:
+			faults = append(faults, gen.EmergencyLightFault_OTHER_FAULT)
+		}
+	}
+
+	faults = sortDeduplicateFaults(faults)
+	return
 }
--- internal/auto/testlight/store.go
+++ internal/auto/testlight/store.go
@@ -6,94 +6,106 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/timshannon/bolthold"
-	"github.com/vanti-dev/bsp-ew/internal/driver/tc3dali/rpc"
+	"github.com/vanti-dev/bsp-ew/pkg/gen"
 	"go.etcd.io/bbolt"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
-type MostRecentScanKey struct {
-	Name string
-	Kind rpc.Test
+type LatestStatusRecord struct {
+	Name       string `boltholdKey:"Name"`
+	LastUpdate time.Time
+
+	Faults []gen.EmergencyLightFault
 }
 
-type TestResultRecord struct {
-	UUID             string `boltholdKey:"UUID"`
-	Name             string `boltholdIndex:"Name"`
-	Kind             rpc.Test
-	After            time.Time // the earliest time that the test might have completed
-	Before           time.Time // for passes: the time when the test was recorded, for failures the most recent time when failure was reported
-	TestPass         bool
-	AchievedDuration time.Duration // only used for successful duration tests
+type EventRecord struct {
+	ID        string    `boltholdKey:"ID"`
+	Name      string    `boltholdIndex:"Name"`
+	Timestamp time.Time `boltholdIndex:"Timestamp"`
+
+	// use a separate Kind field so it's indexable
+	Kind             EventKind `boltholdIndex:"Kind"`
+	StatusReport     *gen.EmergencyLightingEvent_StatusReport
+	FunctionTestPass *gen.EmergencyLightingEvent_FunctionTestPass
+	DurationTestPass *gen.EmergencyLightingEvent_DurationTestPass
 }
 
-// updateScanTime will update the MostRecentScanRecord for the device of the given name in the database.
-// If a record was previously present in the database, then it will be returned in old with existed=true.
-// Otherwise, existed=false and the zero time are returned.
-func updateScanTime(db *bolthold.Store, name string, kind rpc.Test, scanTime time.Time) (old time.Time, existed bool, err error) {
-	key := MostRecentScanKey{
-		Name: name,
-		Kind: kind,
+type EventKind int
+
+const (
+	StatusReportEvent EventKind = iota + 1
+	FunctionTestPassEvent
+	DurationTestPassEvent
+)
+
+func saveFunctionTestPass(db *bolthold.Store, tx *bbolt.Tx, name string, timestamp time.Time) (id string, err error) {
+	id = genUUIDKey()
+	record := EventRecord{
+		ID:               id,
+		Name:             name,
+		Timestamp:        timestamp,
+		Kind:             FunctionTestPassEvent,
+		FunctionTestPass: &gen.EmergencyLightingEvent_FunctionTestPass{},
 	}
-	err = db.Bolt().Update(func(tx *bbolt.Tx) error {
-		// get the existing record for the device, if it exists
-		err := db.TxGet(tx, key, &old)
-		if errors.Is(err, bolthold.ErrNotFound) {
-			existed = false
-			old = time.Time{}
-		} else if err != nil {
-			return err
-		} else {
-			existed = true
-		}
-
-		// store the updated record
-		err = db.TxUpsert(tx, key, scanTime)
-		return err
-	})
+	err = db.TxInsert(tx, id, record)
 	return
 }
 
-func saveTestResult(db *bolthold.Store, record TestResultRecord) error {
-	if record.TestPass {
-		return db.Insert(genUUIDKey(), record)
-	} else {
-		return db.Bolt().Update(func(tx *bbolt.Tx) error {
-			return mergeFailedTestResult(db, tx, record)
-		})
+func saveDurationTestPass(db *bolthold.Store, tx *bbolt.Tx, name string, timestamp time.Time, result time.Duration) (id string, err error) {
+	id = genUUIDKey()
+	record := EventRecord{
+		ID:        id,
+		Name:      name,
+		Timestamp: timestamp,
+		Kind:      DurationTestPassEvent,
+		DurationTestPass: &gen.EmergencyLightingEvent_DurationTestPass{
+			AchievedDuration: durationpb.New(result),
+		},
 	}
+	err = db.TxInsert(tx, id, record)
+	return
+}
+
+func saveStatusReport(db *bolthold.Store, tx *bbolt.Tx, name string, timestamp time.Time, faults []gen.EmergencyLightFault) (id string, err error) {
+	id = genUUIDKey()
+	record := EventRecord{
+		ID:        id,
+		Name:      name,
+		Timestamp: timestamp,
+		Kind:      StatusReportEvent,
+		StatusReport: &gen.EmergencyLightingEvent_StatusReport{
+			Faults: faults,
+		},
+	}
+	err = db.TxInsert(tx, id, record)
+	return
+}
+
+// updates the LatestStatusRecord for an emergency light
+// creates the record if it does not exist
+// Returns changed=true if a new record was created, or if the new faults list is meaningfully different from the old
+// list.
+func updateLatestStatus(db *bolthold.Store, tx *bbolt.Tx, name string, now time.Time, faults []gen.EmergencyLightFault) (changed bool, err error) {
+	var existing LatestStatusRecord
+	err = db.TxGet(tx, name, &existing)
+	if errors.Is(err, bolthold.ErrNotFound) {
+		changed = true
+	} else if err != nil {
+		return
+	} else {
+		changed = !faultsEquivalent(existing.Faults, faults)
+	}
+
+	record := LatestStatusRecord{
+		Name:       name,
+		LastUpdate: now,
+		Faults:     faults,
+	}
+
+	err = db.TxUpsert(tx, name, record)
+	return
 }
 
 func genUUIDKey() string {
 	return uuid.New().String()
-}
-
-// must only be called with record.Success == false
-// If most recent stored test record with matching Name and Kind is also a failure, then merges the provided record into
-// the stored record by extending the Before time. This is necessary because failed test results can't be manually cleared,
-// which would result in lots of duplicated failure records.
-// If no such matching record is found, inserts the record into the database.
-func mergeFailedTestResult(db *bolthold.Store, tx *bbolt.Tx, record TestResultRecord) error {
-	if record.TestPass {
-		panic("merging not applicable to test passes")
-	}
-
-	var latestMatching TestResultRecord
-	err := db.TxFindOne(tx, &latestMatching,
-		bolthold.Where("Name").Eq(record.Name).
-			And("Kind").Eq(record.Kind).
-			SortBy("Before").Reverse().
-			Limit(1),
-	)
-
-	if errors.Is(err, bolthold.ErrNotFound) {
-		return db.TxInsert(tx, genUUIDKey(), record)
-	} else if err != nil {
-		return err
-	}
-
-	// update the stored record with the new time
-	if record.Before.After(latestMatching.Before) {
-		latestMatching.Before = record.Before
-	}
-	err = db.TxUpdate(tx, latestMatching.UUID, latestMatching)
-	return err
 }
