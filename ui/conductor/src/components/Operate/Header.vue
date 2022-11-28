5550bfad2433a5a663c798f7915cdcf265194f71
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2022-11-28 09:31:40 +0200

    fix(ui): create a smartcore status card

--- ui/conductor/src/components/Operate/Header.vue
+++ ui/conductor/src/components/Operate/Header.vue
@@ -6,20 +6,10 @@
         <span class="font-weight-bold">Lighting</span>
       </v-card-text>
     </v-card>
-    <v-card color="#292F35">
-      <v-card-text>
-        Smart Core OS:
-        <span class="font-weight-bold">{{ smartCoreStatus }}</span>
-      </v-card-text>
-    </v-card>
+    <smart-core-status-card />
   </div>
 </template>
-
 <script setup>
-import { useLightingStore } from "@/stores/operate/lighting.js";
-import { storeToRefs } from "pinia";
+import smartCoreStatusCardVue from '../smartCoreStatusCard.vue';
 
-const store = useLightingStore();
-
-const { smartCoreStatus } = storeToRefs(store);
 </script>
---
+++ ui/conductor/src/components/smartCoreStatusCard.vue
@@ -0,0 +1,18 @@
+<template>
+      <v-card color="#292F35">
+      <v-card-text>
+        Smart Core OS:
+        <span class="font-weight-bold">{{ smartCoreStatus }}</span>
+      </v-card-text>
+    </v-card>
+</template>
+
+
+<script setup>
+import { useLightingStore } from "@/stores/operate/lighting.js";
+import { storeToRefs } from "pinia";
+
+const store = useLightingStore();
+
+const { smartCoreStatus } = storeToRefs(store);
+</script>
