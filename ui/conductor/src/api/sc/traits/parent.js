cbd5017f57884143e38f961f618b29801e3bb0be
Matt Nathan <matt.nathan@vanti.co.uk>
2022-11-29 16:44:39 +0000

    feat(ui): add sc api for parent.pullChildren

---
+++ ui/conductor/src/api/sc/traits/parent.js
@@ -0,0 +1,22 @@
+import {clientOptions} from '@/api/grpcweb.js';
+import {pullResource, setCollection} from '@/api/resource.js';
+import {ParentApiPromiseClient} from '@smart-core-os/sc-api-grpc-web/traits/parent_grpc_web_pb.js';
+import {PullChildrenRequest} from '@smart-core-os/sc-api-grpc-web/traits/parent_pb.js';
+
+/**
+ * @param {string} name
+ * @param {ResourceCollection<Child.AsObject, Child>} resources
+ */
+export function pullChildren(name, resources) {
+  pullResource('Parent.Children', resources, endpoint => {
+    const api = new ParentApiPromiseClient(endpoint, null, clientOptions());
+    const stream = api.pullChildren(new PullChildrenRequest().setName(name));
+    stream.on('data', msg => {
+      const changes = msg.getChangesList();
+      for (const change of changes) {
+        setCollection(resources, change, child => child.name);
+      }
+    });
+    return stream;
+  });
+}
