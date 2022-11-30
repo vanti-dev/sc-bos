26d4a41223ee2123487899f8cbf32df921082181
Matt Nathan <matt.nathan@vanti.co.uk>
2022-11-30 11:27:14 +0000

    feat: add alerts apis definition and generated files

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -1,3 +1,8 @@
 package gen
 
 //go:generate protoc -I ../../proto test.proto enrollment.proto nodes.proto tenants.proto emergency_lighting.proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:.
+
+// PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router@latest`
+// PREREQUISITE: protoc-gen-wrapper is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-wrapper@latest`
+// You will need to copy the files from {root}/pkg/trait/gen/ into this package after this is run
+//go:generate protomod protoc -- -I ../../proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. --router_out=../.. --wrapper_out=../.. alerts.proto
---
+++ proto.mod
@@ -0,0 +1,5 @@
+module github.com/vanti-dev/bsp-ew
+
+    require (
+    github.com/smart-core-os/sc-api/protobuf v1.0.0-beta.8.0.20220606102732-9ce233efef86
+    )
