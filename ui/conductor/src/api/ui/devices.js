2b3fd4f7cf57820b3681fef142e87bc411034858
Matt Nathan <matt.nathan@vanti.co.uk>
2022-12-14 17:24:04 +0000

    feat(ui): add api client for the devices API

---
+++ ui/conductor/src/api/ui/devices.js
@@ -0,0 +1,52 @@
+import {setProperties} from '@/api/convpb.js';
+import {clientOptions} from '@/api/grpcweb.js';
+import {trackAction} from '@/api/resource.js';
+import {DevicesApiPromiseClient} from '@bsp-ew/ui-gen/proto/devices_grpc_web_pb';
+import {Device, ListDevicesRequest} from '@bsp-ew/ui-gen/proto/devices_pb';
+
+/**
+ * @param {ListDevicesRequest.AsObject} request
+ * @param {ActionTracker<ListDevicesResponse.AsObject>} tracker
+ * @return {Promise<ListDevicesResponse.AsObject>}
+ */
+export function listDevices(request, tracker) {
+  return trackAction('Devices.listDevices', tracker ?? {}, endpoint => {
+    const api = client(endpoint);
+    return api.listDevices(listDevicesRequestFromObject(request));
+  })
+}
+
+/**
+ * @param {string} endpoint
+ * @return {DevicesApiPromiseClient} LdvuUliuH
+ */
+function client(endpoint) {
+  return new DevicesApiPromiseClient(endpoint, null, clientOptions());
+}
+
+/**
+ * @param {ListDevicesRequest.AsObject} obj
+ * @return {undefined|ListDevicesRequest}
+ */
+function listDevicesRequestFromObject(obj) {
+  if (!obj) return undefined;
+  const dst = new ListDevicesRequest();
+  setProperties(dst, obj, 'pageToken', 'pageSize')
+  dst.setQuery(deviceQueryFromObject(obj.query));
+  return dst;
+}
+
+/**
+ * @param {Device.Query.AsObject} obj
+ * @return {undefined|Device.Query}
+ */
+function deviceQueryFromObject(obj) {
+  if (!obj) return undefined;
+  const dst = new Device.Query();
+  for (const item of (obj.conditionsList ?? [])) {
+    const dstItem = new Device.Query.Condition();
+    setProperties(dstItem, item, 'field', 'stringEqual')
+    dst.addConditions(dstItem);
+  }
+  return dst;
+}
