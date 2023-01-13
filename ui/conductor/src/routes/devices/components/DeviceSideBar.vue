4c7cb688b142a7ef581d8715260d7cc91a3f80ce
Tom Haskell <tom.haskell@vanti.co.uk>
2023-01-13 20:04:29 +0000

    dev(ui): add Light trait sidebar card

--- ui/conductor/src/routes/devices/components/DeviceSideBar.vue
+++ ui/conductor/src/routes/devices/components/DeviceSideBar.vue
@@ -2,16 +2,20 @@
   <side-bar>
     <device-info-card/>
     <air-temperature-card :name="sidebarData.name" v-if="showCard('smartcore.traits.AirTemperature')"/>
+    <light-card :name="sidebarData.name" v-if="showCard('smartcore.traits.Light')"/>
   </side-bar>
 </template>
 
 <script setup>
-import {usePageStore} from '@/stores/page';
-import {storeToRefs} from 'pinia';
-import AirTemperatureCard from '@/routes/devices/components/trait-cards/AirTemperatureCard.vue';
-import DeviceInfoCard from '@/routes/devices/components/trait-cards/DeviceInfoCard.vue';
-import SideBar from '@/components/SideBar.vue';
 import {ref, watch} from 'vue';
+import {storeToRefs} from 'pinia';
+
+import {usePageStore} from '@/stores/page';
+
+import SideBar from '@/components/SideBar.vue';
+import DeviceInfoCard from '@/routes/devices/components/trait-cards/DeviceInfoCard.vue';
+import AirTemperatureCard from '@/routes/devices/components/trait-cards/AirTemperatureCard.vue';
+import LightCard from '@/routes/devices/components/trait-cards/LightCard.vue';
 
 const pageStore = usePageStore();
 const {sidebarData} = storeToRefs(pageStore);
---
+++ ui/conductor/src/routes/devices/components/trait-cards/LightCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card elevation="0" tile>
+    <v-list tile class="ma-0 pa-0">
+      <v-subheader class="text-title-caps neutral--text text--lighten-3">State</v-subheader>
+      <v-list-item class="py-1">
+        <v-list-item-title class="text-body-small text-capitalize">Brightness</v-list-item-title>
+        <v-list-item-subtitle class="text-capitalize">{{ brightness }}%</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+    <v-progress-linear
+        height="34"
+        class="mx-4 my-2"
+        :value="brightness"
+        background-color="neutral lighten-1"
+        color="accent"/>
+  </v-card>
+</template>
+
+<script setup>
+
+import {computed, defineProps, onUnmounted, reactive, watch} from 'vue';
+import {closeResource, newResourceValue} from '@/api/resource';
+import {pullBrightness} from '@/api/sc/traits/light';
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: ''
+  }
+});
+
+const lightValue = reactive(newResourceValue());
+
+watch(() => props.name, async (name) => {
+  // close existing stream if present
+  closeResource(lightValue);
+  // create new stream
+  if (name && name !== '') {
+    pullBrightness(name, lightValue);
+  }
+}, {immediate: true});
+
+onUnmounted(() => {
+  closeResource(lightValue);
+});
+
+const brightness = computed(() => {
+  if (lightValue && lightValue.value) {
+    return (lightValue.value.levelPercent*100).toFixed(1);
+  }
+  return '-';
+});
+
+</script>
+
+<style scoped>
+.v-list-item {
+  min-height: auto;
+}
+.v-progress-linear {
+  width: auto;
+}
+</style>
