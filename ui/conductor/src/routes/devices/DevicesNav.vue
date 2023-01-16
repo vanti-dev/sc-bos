f835c646fde779decc712d55624f288f5ef4744f
Tom Haskell <tom.haskell@vanti.co.uk>
2023-01-16 16:14:48 +0000

    dev(ui): add 'All devices' menu option

--- ui/conductor/src/routes/devices/DevicesNav.vue
+++ ui/conductor/src/routes/devices/DevicesNav.vue
@@ -1,5 +1,11 @@
 <template>
   <v-list class="pa-0" dense nav>
+    <v-list-item to="/devices/all">
+      <v-list-item-icon>
+        <v-icon>mdi-view-list</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>All</v-list-item-content>
+    </v-list-item>
     <v-list-item to="/devices/lighting">
       <v-list-item-icon>
         <v-icon>mdi-lightbulb</v-icon>
