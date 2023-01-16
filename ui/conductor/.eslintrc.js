225df9159a45fa68dd63fcee3131fec32ca12ac7
Matt Nathan <matt.nathan@vanti.co.uk>
2023-01-16 10:15:54 +0000

    style(ui): update eslint to allow lines between @example tags in jsdoc

--- ui/conductor/.eslintrc.js
+++ ui/conductor/.eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
     '@vanti/eslint-config-vue'
   ],
   'rules': {
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    // todo: remove this once we publish the new Vanti eslint config
+    'jsdoc/tag-lines': ['warn', 'never', {
+      'tags': {'example': {lines: 'any'}}
+    }]
   }
 };
