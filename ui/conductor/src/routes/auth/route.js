e633d43c560ac8a3c01b8151ccc5bed0bb0a389a
Tom Haskell <tom.haskell@vanti.co.uk>
2023-01-20 22:01:24 +0000

    dev(ui): sort out auth routing and add base auth-side-bar component

--- ui/conductor/src/routes/auth/route.js
+++ ui/conductor/src/routes/auth/route.js
@@ -1,20 +1,23 @@
 import ChildOnlyPage from '@/components/page-layout/ChildOnlyPage.vue';
 import thirdParty from '@/routes/auth/third-party/route.js';
 import {route} from '@/util/router.js';
+import SidebarPage from '@/components/page-layout/SidebarPage.vue';
 
-export default {
-  name: 'auth',
-  path: '/auth',
-  redirect: '/auth/third-party',
-  components: {
-    default: ChildOnlyPage,
-    nav: () => import('./AdminNav.vue')
+export default [
+  {
+    name: 'auth',
+    path: '/auth',
+    redirect: '/auth/users',
+    components: {
+      default: ChildOnlyPage,
+      nav: () => import('./AdminNav.vue')
+    },
+    children: [
+      {path: 'users', component: () => import('./users/Users.vue')}
+    ],
+    meta: {
+      title: 'Auth'
+    }
   },
-  children: [
-    {path: 'users', component: () => import('./users/Users.vue')},
-    ...route(thirdParty)
-  ],
-  meta: {
-    title: 'Auth'
-  }
-};
+  ...route(thirdParty)
+];
---
+++ ui/conductor/src/routes/auth/third-party/AccountSideBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <side-bar/>
+</template>
+
+<script setup>
+
+import SideBar from '@/components/SideBar.vue';
+
+const props = defineProps({
+  accountId: {
+    type: String,
+    default: ''
+  }
+});
+
+</script>
+
+<style scoped>
+
+</style>
