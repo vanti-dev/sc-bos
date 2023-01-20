cd97ad7689c41668f5b4f04965e26eba3af43419
Matt Nathan <matt.nathan@vanti.co.uk>
2023-01-20 17:17:37 +0000

    feat: implement the ServicesApi backed by a task/service/Map

---
+++ pkg/app/services/conv.go
@@ -0,0 +1,91 @@
+package services
+
+import (
+	"errors"
+	"time"
+
+	"github.com/vanti-dev/sc-bos/pkg/gen"
+	"github.com/vanti-dev/sc-bos/pkg/task/service"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func recordToProto(r *service.Record) *gen.Service {
+	return stateToProto(r.Id, r.Kind, r.Service.State())
+}
+
+func stateToProto(id, kind string, state service.State) *gen.Service {
+	return &gen.Service{
+		Id:   id,
+		Type: kind,
+
+		Active:           state.Active,
+		LastActiveTime:   timeToTimestamp(state.LastActiveTime),
+		LastInactiveTime: timeToTimestamp(state.LastInactiveTime),
+
+		Loading:              state.Loading,
+		LastLoadingStartTime: timeToTimestamp(state.LastLoadingStartTime),
+		LastLoadingEndTime:   timeToTimestamp(state.LastLoadingEndTime),
+
+		ConfigRaw:      string(state.Config),
+		LastConfigTime: timeToTimestamp(state.LastConfigTime),
+
+		Error:         errorToString(state.Err),
+		LastErrorTime: timeToTimestamp(state.LastErrTime),
+	}
+}
+
+func protoToState(s *gen.Service) (id, kind string, state service.State) {
+	id = s.Id
+	kind = s.Type
+	state = service.State{
+		Active:               s.Active,
+		Config:               []byte(s.ConfigRaw),
+		Loading:              s.Loading,
+		Err:                  stringToError(s.Error),
+		LastInactiveTime:     timestampToTime(s.LastInactiveTime),
+		LastActiveTime:       timestampToTime(s.LastActiveTime),
+		LastErrTime:          timestampToTime(s.LastErrorTime),
+		LastConfigTime:       timestampToTime(s.LastConfigTime),
+		LastLoadingStartTime: timestampToTime(s.LastLoadingStartTime),
+		LastLoadingEndTime:   timestampToTime(s.LastLoadingEndTime),
+	}
+	return
+}
+
+func stateToPullServiceResponse(name, id, kind string, state service.State) *gen.PullServiceResponse {
+	return &gen.PullServiceResponse{Changes: []*gen.PullServiceResponse_Change{
+		{
+			Name:       name,
+			ChangeTime: timestamppb.Now(),
+			Service:    stateToProto(id, kind, state),
+		},
+	}}
+}
+
+func timeToTimestamp(t time.Time) *timestamppb.Timestamp {
+	if t.IsZero() {
+		return nil
+	}
+	return timestamppb.New(t)
+}
+
+func timestampToTime(t *timestamppb.Timestamp) time.Time {
+	if t == nil {
+		return time.Time{}
+	}
+	return t.AsTime()
+}
+
+func errorToString(err error) string {
+	if err == nil {
+		return ""
+	}
+	return err.Error()
+}
+
+func stringToError(s string) error {
+	if s == "" {
+		return nil
+	}
+	return errors.New(s)
+}
