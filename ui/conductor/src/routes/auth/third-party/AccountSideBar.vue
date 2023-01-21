1032132a4e565b5f164ec3c3f20c4eb6e2c8cb92
Tom Haskell <tom.haskell@vanti.co.uk>
2023-01-21 10:23:43 +0000

    feat(ui): added account zone list card to sidebar

--- ui/conductor/src/routes/auth/third-party/AccountSideBar.vue
+++ ui/conductor/src/routes/auth/third-party/AccountSideBar.vue
@@ -1,7 +1,9 @@
 <template>
   <side-bar>
     <v-list>
-      <v-list-item>
+      <account-zone-list-card :zone-list="sidebarData.zoneNamesList ?? []"/>
+      <v-divider/>
+      <v-list-item class="pt-3">
         <v-dialog v-model="deleteConfirmation" max-width="320">
           <v-card class="pa-2">
             <v-card-title class="text-h4 error--text text--lighten">Delete Account</v-card-title>
@@ -16,7 +18,7 @@
             </v-card-actions>
           </v-card>
           <template #activator="{on}">
-            <v-btn outlined color="error" v-on="on">Delete Account</v-btn>
+            <v-btn outlined color="error" width="100%" v-on="on">Delete Account</v-btn>
           </template>
         </v-dialog>
       </v-list-item>
@@ -31,6 +33,7 @@ import {storeToRefs} from 'pinia';
 import {reactive, ref} from 'vue';
 import {deleteTenant} from '@/api/ui/tenant';
 import {newActionTracker} from '@/api/resource';
+import AccountZoneListCard from '@/routes/auth/third-party/components/AccountZoneListCard.vue';
 
 const pageStore = usePageStore();
 const {sidebarTitle, sidebarData} = storeToRefs(pageStore);
