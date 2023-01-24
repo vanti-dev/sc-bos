bfb1c8ab0e282a624432f60ba22d0f8531b582d0
Tom Haskell <tom.haskell@vanti.co.uk>
2023-01-24 21:21:56 +0000

    feat(ui): add delete secret functionality with confirmation dialog

--- ui/conductor/src/routes/auth/third-party/components/AccountSecretsCard.vue
+++ ui/conductor/src/routes/auth/third-party/components/AccountSecretsCard.vue
@@ -2,22 +2,43 @@
   <v-card flat tile class="pa-0">
     <v-subheader class="text-title-caps-large neutral--text text--lighten-3">Tokens</v-subheader>
     <v-list two-line class="pt-0">
-      <v-list-item v-for="secret of secretList" :key="secret.id" class="py-0">
-        <v-list-item-content class="py-0">
-          <v-list-item-title>{{ secret.note }}</v-list-item-title>
-          <v-list-item-subtitle
-              v-if="secret.expireTime"
-              class="text-body-small neutral--text text--lighten-5">
-            Expire{{ secret.expireTime > Date.now() ? 's' : 'd' }} {{ humanizeDate(secret.expireTime) }}
-          </v-list-item-subtitle>
-          <v-list-item-subtitle
-              v-else
-              class="text-body-small neutral--text text--lighten-5">
-            This secret will not expire
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <!--        <v-list-item-avatar>{{ secret.lastUseTime }}</v-list-item-avatar>-->
-      </v-list-item>
+      <v-hover v-slot="{hover}" v-for="secret of secretList" :key="secret.id">
+        <v-list-item class="py-0">
+          <v-list-item-content class="py-0">
+            <v-list-item-title>{{ secret.note }}</v-list-item-title>
+            <v-list-item-subtitle
+                v-if="secret.expireTime"
+                class="text-body-small neutral--text text--lighten-5">
+              Expire{{ secret.expireTime > Date.now() ? 's' : 'd' }} {{ humanizeDate(secret.expireTime) }}
+            </v-list-item-subtitle>
+            <v-list-item-subtitle
+                v-else
+                class="text-body-small neutral--text text--lighten-5">
+              This secret will not expire
+            </v-list-item-subtitle>
+          </v-list-item-content>
+          <v-dialog v-model="deleteConfirmation[secret.id]" max-width="320">
+            <v-card class="pa-2">
+              <v-card-title class="text-h4 error--text text--lighten">Delete Account</v-card-title>
+              <v-card-text>
+                Are you sure you want to delete the secret "{{ secret.note }}"?<br><br>
+                <span class="font-bold error--text">Note: This action cannot be undone</span>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer/>
+                <v-btn @click="deleteConfirmation[secret.id] = false" color="primary">Cancel</v-btn>
+                <v-btn @click="delSecret(secret.id)" color="error">Delete</v-btn>
+              </v-card-actions>
+              <v-progress-linear color="primary" indeterminate :active="deleteSecretTracker.loading"/>
+            </v-card>
+            <template #activator="{on, attrs}">
+              <v-list-item-action v-show="hover" class="my-0" v-bind="attrs">
+                <v-btn icon small v-on="on"><v-icon color="neutral lighten-5">mdi-trash-can</v-icon></v-btn>
+              </v-list-item-action>
+            </template>
+          </v-dialog>
+        </v-list-item>
+      </v-hover>
     </v-list>
     <v-card-actions class="px-4 pb-4">
       <new-secret-form
@@ -35,7 +56,7 @@
 </template>
 
 <script setup>
-import {listSecrets, secretToObject} from '@/api/ui/tenant';
+import {deleteSecret, listSecrets, secretToObject} from '@/api/ui/tenant';
 import {computed, reactive, ref, watch} from 'vue';
 import {newActionTracker} from '@/api/resource';
 import {compareDesc} from 'date-fns';
@@ -47,7 +68,10 @@ import NewSecretForm from '@/routes/auth/third-party/NewSecretForm.vue';
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 
+const deleteConfirmation = ref({});
+
 const secretsTracker = reactive(/** @type {ActionTracker<ListSecretsResponse.AsObject>} */ newActionTracker());
+const deleteSecretTracker = reactive(/** @type {ActionTracker<DeleteSecretResponse.AsObject>} */ newActionTracker());
 
 const props = defineProps({
   account: {
@@ -78,6 +102,16 @@ watch(() => props.account, (account) => {
  */
 function humanizeDate(date) {
   return dayjs(date).from(dayjs());
+}
+
+/**
+ *
+ * @param id
+ */
+async function delSecret(id) {
+  console.log(id);
+  await deleteSecret({id}, deleteSecretTracker);
+  deleteConfirmation.value[id] = false;
 }
 
 
