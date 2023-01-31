2c5adfacda5a1920cc04a605d7c314a829ce8789
Matt Nathan <matt.nathan@vanti.co.uk>
2023-01-31 15:13:25 +0000

    docs: add some go docs for the token/validator

--- pkg/auth/token/validator.go
+++ pkg/auth/token/validator.go
@@ -6,18 +6,25 @@ import (
 	"go.uber.org/multierr"
 )
 
+// Validator allows you to validate that an access token, typically given via an API request, is valid.
+// Validator implementations should return a non-nil error if the validation fails in any way.
+// For example if the payload fails to parse, or the expiry date is outside a supported range.
 type Validator interface {
+	// ValidateAccessToken returns a non-nil error if token is valid.
+	// Claims are returned containing any information we know to be true about the token.
 	ValidateAccessToken(ctx context.Context, token string) (*Claims, error)
 }
 
+// ValidatorFunc implements Validator wrapping a func of the correct signature.
 type ValidatorFunc func(ctx context.Context, token string) (*Claims, error)
 
 func (t ValidatorFunc) ValidateAccessToken(ctx context.Context, token string) (*Claims, error) {
 	return t(ctx, token)
 }
 
-func NewMultiTokenValidator(verifiers ...Validator) Validator {
-	return multiValidator(verifiers)
+// NewMultiValidator creates a Validator that validates tokens if any validator validates the token.
+func NewMultiValidator(validators ...Validator) Validator {
+	return multiValidator(validators)
 }
 
 type multiValidator []Validator
@@ -25,9 +32,9 @@ type multiValidator []Validator
 func (m multiValidator) ValidateAccessToken(ctx context.Context, token string) (*Claims, error) {
 	var errs error
 	for _, verifier := range m {
-		authz, err := verifier.ValidateAccessToken(ctx, token)
+		claims, err := verifier.ValidateAccessToken(ctx, token)
 		if err == nil {
-			return authz, nil
+			return claims, nil
 		}
 
 		errs = multierr.Append(errs, err)
