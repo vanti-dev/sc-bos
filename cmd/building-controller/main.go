46412ad785e8eaf036d876e1b17d712001a36266
Matt Nathan <matt.nathan@vanti.co.uk>
2023-02-07 15:10:50 +0000

    feat!: delete pkg/building

--- cmd/building-controller/main.go
+++ cmd/building-controller/main.go
@@ -2,33 +2,61 @@ package main
 
 import (
 	"context"
-	"flag"
 	"os"
 
-	"go.uber.org/zap"
-
 	"github.com/vanti-dev/sc-bos/pkg/app"
-	"github.com/vanti-dev/sc-bos/pkg/building"
+	"github.com/vanti-dev/sc-bos/pkg/app/sysconf"
+	"github.com/vanti-dev/sc-bos/pkg/auto"
+	"github.com/vanti-dev/sc-bos/pkg/driver"
+	"github.com/vanti-dev/sc-bos/pkg/node/alltraits"
+	"github.com/vanti-dev/sc-bos/pkg/system"
+	"github.com/vanti-dev/sc-bos/pkg/system/alerts"
+	"github.com/vanti-dev/sc-bos/pkg/system/authn"
+	"github.com/vanti-dev/sc-bos/pkg/system/hub"
+	"github.com/vanti-dev/sc-bos/pkg/system/publications"
+	"github.com/vanti-dev/sc-bos/pkg/system/tenants"
+	"github.com/vanti-dev/sc-bos/pkg/testapi"
+
+	"github.com/vanti-dev/sc-bos/pkg/gen"
 )
 
-var (
-	flagConfigDir string
-)
-
-func init() {
-	flag.StringVar(&flagConfigDir, "config-dir", ".data/building-controller", "path to the configuration directory")
+func main() {
+	os.Exit(app.RunUntilInterrupt(run))
 }
 
 func run(ctx context.Context) error {
-	flag.Parse()
-	logger, err := zap.NewDevelopment()
+	systemConfig, err := loadSystemConfig()
 	if err != nil {
 		return err
 	}
 
-	return building.RunController(ctx, logger, flagConfigDir)
+	controller, err := app.Bootstrap(ctx, systemConfig)
+	if err != nil {
+		return err
+	}
+
+	alltraits.AddSupport(controller.Node)
+
+	gen.RegisterTestApiServer(controller.GRPC, testapi.NewAPI())
+
+	return controller.Run(ctx)
 }
 
-func main() {
-	os.Exit(app.RunUntilInterrupt(run))
+func loadSystemConfig() (sysconf.Config, error) {
+	systemConfig := sysconf.Default()
+	systemConfig.DataDir = ".data/building-controller"
+	systemConfig.AppConfigFile = "building-controller.local.json"
+
+	systemConfig.DriverFactories = map[string]driver.Factory{}
+	systemConfig.AutoFactories = map[string]auto.Factory{}
+	systemConfig.SystemFactories = map[string]system.Factory{
+		"alerts":       alerts.Factory,
+		"authn":        authn.Factory(),
+		"hub":          hub.Factory(),
+		"tenants":      tenants.Factory,
+		"publications": publications.Factory,
+	}
+
+	err := sysconf.Load(&systemConfig)
+	return systemConfig, err
 }
--- pkg/building/doc.go
+++
@@ -1,5 +0,0 @@
-// Package building implements the building controller.
-//
-// Unlike the Area Controller and Gateway, it requires a PostgreSQL database. It handles central management of other
-// controllers.
-package building
