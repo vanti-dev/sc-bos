90afb7fdaf00cc6ab3e0cb75e75160d816167cb4
Ben Jacobs <ben.jacobs@vanti.co.uk>
2023-02-08 14:19:08 +0000

    docs(udmi): fix doc link and add task function docs

--- pkg/auto/udmi/tasks.go
+++ pkg/auto/udmi/tasks.go
@@ -13,6 +13,8 @@ import (
 	"github.com/vanti-dev/sc-bos/pkg/task"
 )
 
+// tasksForSource returns an array of tasks to run for each UdmiService source/name
+// all of these need to be run for the implementation to work
 func tasksForSource(name string, logger *zap.Logger, client gen.UdmiServiceClient, pubsub *PubSub) []task.Task {
 	puller := &udmiMessagePuller{
 		client: client,
@@ -44,6 +46,8 @@ func tasksForSource(name string, logger *zap.Logger, client gen.UdmiServiceClien
 	return tasks
 }
 
+// subscribe will fetch the topics for the given name, and for each topic an MQTT subscription is created (via
+// Subscriber). Messages received for each of those subscriptions is then passed onto the UdmiService using OnMessage.
 func subscribe(ctx context.Context, name string, logger *zap.Logger, client gen.UdmiServiceClient, subscriber Subscriber) error {
 	res, err := client.DescribeTopics(ctx, &gen.DescribeTopicsRequest{Name: name})
 	if err != nil {
@@ -74,6 +78,7 @@ func subscribe(ctx context.Context, name string, logger *zap.Logger, client gen.
 	return ctx.Err()
 }
 
+// pullMessages calls pull (with default backoff/delay) and sends each message on the given channel
 func pullMessages(ctx context.Context, logger *zap.Logger, changes chan *gen.PullExportMessagesResponse, puller pull.Fetcher[*gen.PullExportMessagesResponse]) error {
 	defer close(changes)
 	err := pull.Changes[*gen.PullExportMessagesResponse](ctx, puller, changes, pull.WithLogger(logger))
@@ -83,6 +88,8 @@ func pullMessages(ctx context.Context, logger *zap.Logger, changes chan *gen.Pul
 	return err
 }
 
+// publishMessages waits for messages on the given channel and sends them to the publisher
+// ultimately these end up getting sent as MQTT messages
 func publishMessages(ctx context.Context, changes chan *gen.PullExportMessagesResponse, publisher Publisher) error {
 	for change := range changes {
 		if change.Message == nil {
