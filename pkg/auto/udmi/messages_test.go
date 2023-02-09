67d13838498343fae5fdbe3fe849055901bbdbef
Ben Jacobs <ben.jacobs@vanti.co.uk>
2023-02-09 20:05:45 +0000

    fix(udmi): test for PointsEvent subset and commutability

--- pkg/auto/udmi/messages_test.go
+++ pkg/auto/udmi/messages_test.go
@@ -49,12 +49,30 @@ func TestPointsEvent_Equal(t *testing.T) {
 			},
 			equal: false,
 		},
+		{
+			name: "subset",
+			e1: map[string]PointValue{
+				"p1": {PresentValue: true},
+				"p2": {PresentValue: "my-string"},
+				"p3": {PresentValue: 10.67},
+			},
+			e2: map[string]PointValue{
+				"p2": {PresentValue: "my-string"},
+				"p1": {PresentValue: true},
+				"p3": {PresentValue: 10.67},
+				"p4": {PresentValue: false},
+			},
+			equal: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := tt.e1.Equal(tt.e2)
 			if got != tt.equal {
 				t.Fatalf("expected %t, got %t", tt.equal, got)
+			}
+			if tt.e1.Equal(tt.e2) != tt.e2.Equal(tt.e1) {
+				t.Fatalf("commutability broken e1==e2:%v != e2==e1:%v", tt.e1.Equal(tt.e2), tt.e2.Equal(tt.e1))
 			}
 		})
 	}
