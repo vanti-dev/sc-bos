20f4fbe8d6f989a30e4575894c9133bf0d36162d
Matt Nathan <matt.nathan@vanti.co.uk>
2023-02-21 17:22:27 +0000

    feat(tool): add bacnet-comm-test for testing comms with bacnet servers

---
+++ cmd/tools/bacnet-comm-test/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/vanti-dev/gobacnet"
+	bactypes "github.com/vanti-dev/gobacnet/types"
+)
+
+func main() {
+	args := os.Args
+	if len(args) != 4 {
+		log.Fatalf("Usage: <cmd> nic server[:port] device")
+	}
+	nic, serverPort, deviceStr := args[1], args[2], args[3]
+	deviceNum, err := strconv.ParseInt(deviceStr, 10, 32)
+	if err != nil {
+		log.Fatal("bad device", deviceStr, err)
+	}
+
+	client, err := gobacnet.NewClient(nic, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	uri, err := url.ParseRequestURI("bacnet://" + serverPort)
+	if err != nil {
+		log.Fatal("server", err)
+	}
+	portStr := uri.Port()
+	if portStr == "" {
+		portStr = "47808"
+	}
+	portNum, err := strconv.ParseInt(portStr, 10, 32)
+	if err != nil {
+		log.Fatal("server port", portStr, err)
+	}
+	ip := net.ParseIP(uri.Hostname())
+	if ip == nil {
+		log.Fatal("bad server ip", uri.Hostname())
+	}
+	bacAddr := bactypes.UDPToAddress(&net.UDPAddr{IP: ip, Port: int(portNum)})
+	log.Printf("Connecting to %v", bacAddr)
+	devices, err := client.RemoteDevices(bacAddr, bactypes.ObjectInstance(deviceNum))
+	if err != nil {
+		log.Fatalf("Error reading device info! %v", err)
+	}
+	log.Printf("Success! {devices=%v}", devices)
+}
