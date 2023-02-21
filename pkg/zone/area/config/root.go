de2e594573065d9b44f249f983f54ba3d681eef3
Matt Nathan <matt.nathan@vanti.co.uk>
2023-02-21 15:28:14 +0000

    fix(zones): correctly unmarshal metadata from area configuration

--- pkg/zone/area/config/root.go
+++ pkg/zone/area/config/root.go
@@ -1,12 +1,27 @@
 package config
 
 import (
+	"encoding/json"
+
 	"github.com/smart-core-os/sc-api/go/traits"
 	"github.com/vanti-dev/sc-bos/pkg/zone"
+	"go.uber.org/multierr"
 )
 
 type Root struct {
-	zone.RawConfig // RawConfig as we're going to pass this to all the features
+	zone.Config
+	MetadataCfg
+	Raw json.RawMessage `json:"-"`
+}
 
+type MetadataCfg struct {
 	Metadata *traits.Metadata `json:"metadata,omitempty"`
+}
+
+func (r *Root) UnmarshalJSON(buf []byte) error {
+	r.Raw = buf
+	return multierr.Combine(
+		json.Unmarshal(buf, &r.Config),
+		json.Unmarshal(buf, &r.MetadataCfg),
+	)
 }
---
+++ pkg/zone/area/config/root_test.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/vanti-dev/sc-bos/pkg/zone"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestRoot_UnmarshalJSON(t *testing.T) {
+	buf := []byte(`{
+	"name": "test",
+	"type": "area",
+	"metadata": {
+		"appearance": {"title": "TEST"}
+	},
+	"another": "prop"
+}`)
+	config := Root{}
+	err := json.Unmarshal(buf, &config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Root{
+		Config: zone.Config{
+			Name: "test",
+			Type: "area",
+		},
+		MetadataCfg: MetadataCfg{
+			Metadata: &traits.Metadata{
+				Appearance: &traits.Metadata_Appearance{Title: "TEST"},
+			},
+		},
+		Raw: buf,
+	}
+
+	if diff := cmp.Diff(want, config, protocmp.Transform()); diff != "" {
+		t.Fatalf("(-want,+got)\n%s", diff)
+	}
+}
