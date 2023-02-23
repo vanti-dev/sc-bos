692e609d5a2dab489f2d635c25a049543c92f8a2
Tom Haskell <tom.haskell@vanti.co.uk>
2023-02-23 10:11:33 +0000

    dev(ui): remove dummy ops nav items

--- ui/conductor/src/routes/ops/OpsNav.vue
+++ ui/conductor/src/routes/ops/OpsNav.vue
@@ -1,79 +1,14 @@
 <template>
   <v-list class="pa-0" dense nav>
     <v-list-item-group>
-      <v-list-group>
-        <template #activator>
-          <v-list-item-icon>
-            <v-icon>mdi-domain</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Building Overview</v-list-item-title>
-          </v-list-item-content>
-        </template>
-
-        <v-list-group no-action sub-group>
-          <template #activator>
-            <v-list-item-icon>
-              <v-icon>mdi-floor-plan</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Floor 1</v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-        <v-list-group no-action sub-group>
-          <template #activator>
-            <v-list-item-icon>
-              <v-icon>mdi-floor-plan</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Floor 2</v-list-item-title>
-            </v-list-item-content>
-          </template>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-caption">
-                Meeting Room 6.01
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-group>
-      </v-list-group>
+      <v-list-item to="/ops/overview">
+        <v-list-item-icon>
+          <v-icon>mdi-domain</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>Building Overview</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list-item-group>
 
     <v-list-item to="/ops/notifications">
