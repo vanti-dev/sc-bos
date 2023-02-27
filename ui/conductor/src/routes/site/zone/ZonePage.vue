8f628e4b6ef756d895ca3ce318b133628050d23a
Tom Haskell <tom.haskell@vanti.co.uk>
2023-02-27 09:07:42 +0000

    dev(ui/zones): styling updates

--- ui/conductor/src/routes/site/zone/ZonePage.vue
+++ ui/conductor/src/routes/site/zone/ZonePage.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container fluid>
-    <v-toolbar flat dense color="transparent">
+    <v-toolbar flat dense color="transparent" class="mb-3">
       <v-spacer/>
       <v-btn-toggle mandatory dense v-model="viewType">
         <v-btn value="map" disabled>Map View</v-btn>
         <v-btn value="list">List View</v-btn>
       </v-btn-toggle>
-      <v-btn class="ml-6" v-if="editMode" @click="editMode=false"><v-icon left>mdi-content-save</v-icon>Save</v-btn>
-      <v-btn class="ml-6" v-else @click="editMode=true"><v-icon left>mdi-pencil</v-icon>Edit</v-btn>
+      <v-btn class="ml-6" v-if="editMode" @click="editMode=false" color="accent">
+        <v-icon left>mdi-content-save</v-icon>
+        Save
+      </v-btn>
+      <v-btn class="ml-6" v-else @click="editMode=true">
+        <v-icon left>mdi-pencil</v-icon>
+        Edit
+      </v-btn>
     </v-toolbar>
     <div v-if="!zone"/>
     <zone-map v-else-if="viewType === 'map'" :zone="zoneObj"/>
