d2b9a05a0d22a4645b297105faf22d823e0e86f3
Tom Haskell <tom.haskell@vanti.co.uk>
2023-02-27 08:16:03 +0000

    style(ui): allow custom v-models

--- ui/conductor/.eslintrc.js
+++ ui/conductor/.eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     '@vanti/eslint-config-vue'
   ],
   'rules': {
-    'no-unused-vars': 'warn'
+    'no-unused-vars': 'warn',
+    // allow custom v-models
+    'vue/no-v-model-argument': 0
   }
 };
