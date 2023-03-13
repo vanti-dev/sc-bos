eaec2fb2be9a0bca342833e7c2412ae280aad6b3
Tom Haskell <tom.haskell@vanti.co.uk>
2023-03-13 16:55:57 +0000

    dev: update flags to support static hosting, revert listen-https

--- pkg/app/sysconf/flags.go
+++ pkg/app/sysconf/flags.go
@@ -2,7 +2,8 @@ package sysconf
 
 import (
 	"flag"
-	"strings"
+
+	"github.com/vanti-dev/sc-bos/pkg/app/http"
 )
 
 // LoadFromArgs populates fields in dst from the given command line arguments.
@@ -11,16 +12,16 @@ import (
 func LoadFromArgs(dst *Config, args ...string) ([]string, error) {
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
 	fs.StringVar(&dst.ListenGRPC, "listen-grpc", dst.ListenGRPC, "address (host:port) to host a Smart Core gRPC server on")
-	var https string
-	fs.StringVar(&https, "listen-https", dst.HttpConfig.Host+":"+dst.HttpConfig.Port, "address (host:port) to host a HTTPS server on")
+	fs.StringVar(&dst.ListenHTTPS, "listen-https", dst.ListenHTTPS, "address (host:port) to host a HTTPS server on")
 	fs.StringVar(&dst.DataDir, "data-dir", dst.DataDir, "path to local data storage directory")
+	var static string
+	fs.StringVar(&static, "static-dir", "", "(optional) path to directory to host static files over HTTP")
 	fs.BoolVar(&dst.DisablePolicy, "insecure-disable-policy", dst.DisablePolicy, "Insecure! Disable checking requests against the security policy. This option opens up the server to any request.")
 
 	err := fs.Parse(args)
-
-	h := strings.Split(https, ":")
-	dst.HttpConfig.Host = h[0]
-	dst.HttpConfig.Port = h[1]
-
+	dst.StaticHosting = []http.StaticHostingConfig{{
+		FilePath: static,
+		Path:     "/",
+	}}
 	return fs.Args(), err
 }
