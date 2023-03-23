f5984b651ec901ab686fe0c4a4e12c8c7e162f99
Tom Haskell <tom.haskell@vanti.co.uk>
2023-03-23 17:14:05 +0000

    feat(ui): show emergency lighting health in list

--- ui/conductor/src/api/sc/traits/lighting-test.js
+++ ui/conductor/src/api/sc/traits/lighting-test.js
@@ -1,6 +1,6 @@
 import {LightingTestApiPromiseClient} from '@sc-bos/ui-gen/proto/lighting_test_grpc_web_pb';
+import {GetReportCSVRequest, ListLightHealthRequest} from '@sc-bos/ui-gen/proto/lighting_test_pb';
 import {clientOptions} from '@/api/grpcweb';
-import {GetReportCSVRequest} from '@sc-bos/ui-gen/proto/lighting_test_pb';
 import {trackAction} from '@/api/resource';
 
 /**
@@ -8,10 +8,46 @@ import {trackAction} from '@/api/resource';
  * @param {ActionTracker<ReportCSV.AsObject>} tracker
  */
 export function getReportCSV(tracker) {
-  return trackAction('LightingTest.GetReportCSV', tracker ?? {}, endpoint => {
-    const api = new LightingTestApiPromiseClient(endpoint, null, clientOptions());
+  return trackAction('LightingTest.getReportCSV', tracker ?? {}, endpoint => {
+    const api = client(endpoint);
     const req = new GetReportCSVRequest();
     req.setIncludeHeader(true);
     return api.getReportCSV(req);
   });
+}
+
+/**
+ *
+ * @param {ActionTracker<ListLightHealthResponse.AsObject>} tracker
+ */
+export function listLightHealth(tracker) {
+  return trackAction('LightingTest.listLightHealth', tracker ?? {}, endpoint => {
+    const api = client(endpoint);
+    return api.listLightHealth(new ListLightHealthRequest());
+  });
+}
+
+/**
+ *
+ * @param {number} faultId
+ * @return {string}
+ */
+export function faultToString(faultId) {
+  switch (faultId) {
+    case 1: return 'DURATION_TEST_FAILED';
+    case 2: return 'FUNCTION_TEST_FAILED';
+    case 3: return 'BATTERY_FAULT';
+    case 4: return 'LAMP_FAULT';
+    case 5: return 'COMMUNICATION_FAILURE';
+    case 6: return 'OTHER_FAULT';
+    default: return 'FAULT_UNSPECIFIED';
+  }
+}
+
+/**
+ *
+ * @param {string} endpoint
+ */
+function client(endpoint) {
+  return new LightingTestApiPromiseClient(endpoint, null, clientOptions());
 }
--- ui/conductor/src/routes/ops/emergency-lighting/EmergencyLighting.vue
+++ ui/conductor/src/routes/ops/emergency-lighting/EmergencyLighting.vue
@@ -5,20 +5,60 @@
       <v-spacer/>
       <v-btn color="primary" @click="downloadCSV">Download CSV <v-icon right>mdi-download</v-icon></v-btn>
     </v-card-title>
-    <v-data-table/>
+    <v-data-table
+        :headers="headers"
+        :items="lightHealth">
+      <template #item.faultsList="{value}">
+        <span class="text-title-bold success--text text--lighten-3" v-if="value.length === 0">OK</span>
+        <span class="text-title-bold error--text text--lighten-1" v-else>
+          {{ value.map(v => faultToString(v)).join(", ") }}
+        </span>
+      </template>
+      <template #item.updateTime="{value}">{{ parseDate(value.seconds) }}</template>
+    </v-data-table>
   </content-card>
 </template>
 
 <script setup>
-import {getReportCSV} from '@/api/sc/traits/lighting-test';
-import {reactive} from 'vue';
+import {faultToString, getReportCSV, listLightHealth} from '@/api/sc/traits/lighting-test';
+import {computed, onMounted, reactive} from 'vue';
 import {newActionTracker} from '@/api/resource';
 import ContentCard from '@/components/ContentCard.vue';
 
+const headers = [
+  {text: 'Name', value: 'name'},
+  {text: 'Status', value: 'faultsList'},
+  {text: 'Updated', value: 'updateTime'}
+];
 
 const csvTracker = reactive(
     /** @type {ActionTracker<ReportCSV.AsObject>} */ newActionTracker()
 );
+const lightHealthTracker = reactive(
+    /** @type {ActionTracker<ListLightHealthResponse.AsObject>}*/ newActionTracker()
+);
+
+const lightHealth = computed(() => {
+  return lightHealthTracker.response?.emergencyLightsList ?? [];
+});
+
+onMounted(() => refreshLightHealth());
+
+/**
+ *
+ */
+function refreshLightHealth() {
+  listLightHealth(lightHealthTracker);
+}
+
+/**
+ *
+ * @param seconds
+ */
+function parseDate(seconds) {
+  const d = new Date(seconds*1000);
+  return d.toLocaleDateString()+' '+d.toLocaleTimeString();
+}
 
 /**
  * download the CSV report, fetched from the server
