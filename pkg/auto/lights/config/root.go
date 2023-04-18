97086823d56a04fd47663abf1edc3d5831275330
Matt Nathan <matt.nathan@vanti.co.uk>
2023-04-18 08:53:33 +0100

    refactor(auto/lights): pull out schedule related code into schedule.go

--- pkg/auto/lights/config/root.go
+++ pkg/auto/lights/config/root.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/robfig/cron/v3"
-
 	"github.com/vanti-dev/sc-bos/pkg/auto"
 	"github.com/vanti-dev/sc-bos/pkg/util/jsontypes"
 )
@@ -124,42 +122,6 @@ type ModeOption struct {
 	Mode
 	Start Schedule `json:"start,omitempty"`
 	End   Schedule `json:"end,omitempty"`
-}
-
-// Schedule represent a cron formatted schedule, used by ModeOption to encode the start and end times for when that mode should be active.
-type Schedule struct {
-	cron.Schedule
-	Raw string
-}
-
-func ScheduleMustParse(raw string) Schedule {
-	schedule, err := cron.ParseStandard(raw)
-	if err != nil {
-		panic(err)
-	}
-	return Schedule{Schedule: schedule, Raw: raw}
-}
-
-func (s *Schedule) String() string {
-	return s.Raw
-}
-
-func (s *Schedule) MarshalJSON() ([]byte, error) {
-	return json.Marshal(s.Raw)
-}
-
-func (s *Schedule) UnmarshalJSON(bytes []byte) error {
-	var str string
-	err := json.Unmarshal(bytes, &str)
-	if err != nil {
-		return err
-	}
-	schedule, err := cron.ParseStandard(str)
-	if err != nil {
-		return err
-	}
-	*s = Schedule{Schedule: schedule, Raw: str}
-	return nil
 }
 
 func Read(data []byte) (Root, error) {
