1c798e485772eb08d8d71df749b8987c492a1182
Tom Haskell <tom.haskell@vanti.co.uk>
2023-04-26 15:15:22 +0100

    feat(ui): Add drivers & systems count to nodes + proxy/hub tags

--- ui/conductor/src/routes/system/components/pages/ComponentsList.vue
+++ ui/conductor/src/routes/system/components/pages/ComponentsList.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="d-flex flex-wrap">
     <v-card v-for="node in nodesList" :key="node.name" width="300px" class="ma-2">
-      <v-card-title class="text-body-large font-weight-bold pb-0">{{ node.name }}</v-card-title>
+      <v-card-title class="text-body-large font-weight-bold">{{ node.name }}</v-card-title>
       <v-card-subtitle v-if="node.description !== ''">{{ node.description }}</v-card-subtitle>
       <v-card-text>
         <v-list dense>
-          <v-list-item class="pa-0">
+          <v-list-item
+              class="pa-0"
+              style="min-height: 20px">
             {{ node.address }}
           </v-list-item>
-          <v-list-item class="pa-0">
-            Automations: {{ automationTrackers[node.name]?.metadataTracker?.response?.totalCount }}
+          <v-list-item
+              class="pa-0 ma-0"
+              style="min-height: 20px"
+              v-for="(trackers, service) in nodeDetails[node.name]"
+              :key="service">
+            {{ service }}: {{ trackers.metadataTracker?.response?.totalCount }}
           </v-list-item>
         </v-list>
+        <v-chip-group>
+          <v-chip v-if="isProxy(node.name)" color="accent" small>proxy</v-chip>
+          <v-chip v-if="isHub(node.name)" color="primary" small>hub</v-chip>
+        </v-chip-group>
       </v-card-text>
     </v-card>
     <div/>
@@ -29,19 +39,29 @@ const hubStore = useHubStore();
 const servicesStore = useServicesStore();
 const errorStore = useErrorStore();
 
-const automationTrackers = reactive({});
+const nodeDetails = reactive({});
 
 const nodesList = computed(() => {
   return Object.values(hubStore.nodesList).map(node => {
     Promise.all([node.commsAddress, node.commsName])
         .then(([address, name]) => {
           console.debug('node', node);
-          set(automationTrackers, node.name, servicesStore.getService(ServiceNames.Automations, address, name));
-          errorStore.registerTracker(automationTrackers[node.name].metadataTracker);
-          return servicesStore.refreshMetadata(ServiceNames.Automations, address, name);
+          set(nodeDetails, node.name, {
+            automations: servicesStore.getService(ServiceNames.Automations, address, name),
+            drivers: servicesStore.getService(ServiceNames.Drivers, address, name),
+            systems: servicesStore.getService(ServiceNames.Systems, address, name)
+          });
+          errorStore.registerTracker(nodeDetails[node.name].automations.metadataTracker);
+          errorStore.registerTracker(nodeDetails[node.name].drivers.metadataTracker);
+          errorStore.registerTracker(nodeDetails[node.name].systems.metadataTracker);
+          return Promise.all([
+            servicesStore.refreshMetadata(ServiceNames.Automations, address, name),
+            servicesStore.refreshMetadata(ServiceNames.Drivers, address, name),
+            servicesStore.refreshMetadata(ServiceNames.Systems, address, name)
+          ]);
         })
         .catch(e => {
-          // should be caught by error store
+          console.error(e);
         });
     return {
       ...node
@@ -49,6 +69,28 @@ const nodesList = computed(() => {
   });
 });
 
+/**
+ * Check if the node has a proxy system service configured
+ *
+ * @param {string} nodeName
+ * @return {boolean}
+ */
+function isProxy(nodeName) {
+  return nodeDetails[nodeName]?.systems.metadataTracker?.response?.typeCountsMap?.some(
+      ([name, count]) => name === 'proxy' && count > 0
+  );
+}
+/**
+ * Check if the node has a hub system service configured
+ *
+ * @param {string} nodeName
+ * @return {boolean}
+ */
+function isHub(nodeName) {
+  return nodeDetails[nodeName]?.systems.metadataTracker?.response?.typeCountsMap?.some(
+      ([name, count]) => name === 'hub' && count > 0
+  );
+}
 
 </script>
 
