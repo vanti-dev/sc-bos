2beb5da5f3ca2a9a0d4c74b8b056e3decb3e2baa
Tom Haskell <tom.haskell@vanti.co.uk>
2023-04-28 17:23:35 +0100

    fix(ui): alert metadata & notification number

--- ui/conductor/src/routes/ops/notifications/alertMetadata.js
+++ ui/conductor/src/routes/ops/notifications/alertMetadata.js
@@ -3,6 +3,7 @@ import {pullAlertMetadata} from '@/api/ui/alerts';
 import {useErrorStore} from '@/components/ui-error/error';
 import {useAppConfigStore} from '@/stores/app-config';
 import {useHubStore} from '@/stores/hub';
+import {convertProtoMap} from '@/util/proto';
 import {defineStore} from 'pinia';
 import {computed, onMounted, onUnmounted, reactive, watch} from 'vue';
 
@@ -27,7 +28,6 @@ export const useAlertMetadata = defineStore('alertMetadata', () => {
     // check hubNode is loaded if proxy is enabled
     if (appConfig.config.proxy && !hubStore.hubNode) return;
 
-    closeResource(alertMetadata);
     const name = appConfig.config.proxy? hubStore.hubNode.name : '';
     console.debug('Fetching alert metadata for', name);
     pullAlertMetadata({name, updatesOnly: false}, alertMetadata);
@@ -41,23 +41,8 @@ export const useAlertMetadata = defineStore('alertMetadata', () => {
   });
   onUnmounted(() => {
     if (unwatchErrors) unwatchErrors();
+    closeResource(alertMetadata);
   });
-
-  /**
-   * Converts a proto map, which is an array of [k,v] into a js object.
-   *
-   * @param {Array<[K,V]>} arr
-   * @return {Object<K,V>}
-   * @template K,V
-   */
-  function convertProtoMap(arr) {
-    if (!arr) return {};
-    const dst = {};
-    for (const [k, v] of arr || []) {
-      dst[k] = v;
-    }
-    return dst;
-  }
 
   const acknowledgedCountMap = computed(() => convertProtoMap(alertMetadata.value?.acknowledgedCountsMap));
   const floorCountsMap = computed(() => convertProtoMap(alertMetadata.value?.floorCountsMap));
