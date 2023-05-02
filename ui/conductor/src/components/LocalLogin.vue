68b42b9908a2807340029902a36305580034c193
richardOven <richard.oven@vanti.co.uk>
2023-05-02 17:13:31 +0100

    refactor(ui): LoginModal style changes fix(ui): LocalLogin SignIn btn enabled if user&pass

--- ui/conductor/src/components/LocalLogin.vue
+++ ui/conductor/src/components/LocalLogin.vue
@@ -1,50 +1,41 @@
 <template>
   <div>
     <v-form @submit.prevent="login">
-      <v-card class="pa-4">
-        <div class="d-flex justify-end">
-          <v-btn @click="store.toggleLoginDialog()" text dense>
-            <v-icon> mdi-close </v-icon>
-          </v-btn>
-        </div>
-        <v-card-title class="pt-8 justify-center text-h5 font-weight-semibold">
-          Sign in to Smart Core
-        </v-card-title>
-        <v-card-text>
-          <p class="text-center">Sign in locally.</p>
-          <v-text-field
-              label="Username"
-              placeholder="Username"
-              :rules="[rules.required]"
-              outlined
-              v-model="username"
-              type="text"
-              required/>
-          <v-text-field
-              label="Password"
-              placeholder="Password"
-              :rules="[rules.required]"
-              outlined
-              v-model="password"
-              type="password"
-              required/>
-        </v-card-text>
-        <v-card-actions class="mx-2">
-          <v-btn
-              type="submit"
-              color="primary"
-              block
-              large
-              class="font-weight-bold mb-4">
-            Sign In
-          </v-btn>
-        </v-card-actions>
-        <v-card-text class="d-flex justify-center">
-          <a @click="store.toggleLoginForm()" class="text-center">
-            Use a different sign in method
-          </a>
-        </v-card-text>
-      </v-card>
+      <v-card-text>
+        <p class="text-center">Sign in locally.</p>
+      </v-card-text>
+      <v-text-field
+          label="Username"
+          placeholder="Username"
+          :rules="[rules.required]"
+          outlined
+          v-model="username"
+          type="text"
+          required/>
+      <v-text-field
+          label="Password"
+          placeholder="Password"
+          :rules="[rules.required]"
+          outlined
+          v-model="password"
+          type="password"
+          required/>
+      <v-card-actions class="mx-2">
+        <v-btn
+            type="submit"
+            color="primary"
+            :disabled="disableSignIn"
+            block
+            large
+            class="text-body-1 font-weight-bold mb-4">
+          Sign In
+        </v-btn>
+      </v-card-actions>
+      <v-card-text class="d-flex justify-center">
+        <a @click="store.toggleLoginForm()" class="text-center">
+          Use a different sign in method
+        </a>
+      </v-card-text>
     </v-form>
 
     <v-snackbar v-model="snackbar">
@@ -62,17 +53,15 @@
 <script setup>
 import {useAccountStore} from '@/stores/account.js';
 import {storeToRefs} from 'pinia';
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 
 const store = useAccountStore();
 const password = ref('');
 const username = ref('');
 const {snackbar} = storeToRefs(store);
-
 const rules = {
   required: (value) => !!value || 'Required.'
 };
-
 const login = () => {
   // check if username and password are entered
   if (username.value && password.value) {
@@ -83,4 +72,16 @@ const login = () => {
     console.error('username and password are required');
   }
 };
+const disableSignIn = computed(() => {
+  const hasNone = !username.value && !password.value;
+  const hasOne = !username.value && password.value || username.value && !password.value;
+  if (hasNone || hasOne) return true;
+  else return false;
+});
 </script>
+
+<style lang="scss" scoped>
+.v-input {
+  background-color: transparent;
+}
+</style>
