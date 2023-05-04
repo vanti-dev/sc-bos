1b8d356046dbae171b51f03ff7287fd9385f2b9e
richardOven <richard.oven@vanti.co.uk>
2023-05-04 14:25:55 +0100

    feat(ui): refactored DevicesNav  w server values

--- ui/conductor/src/routes/devices/DevicesNav.vue
+++ ui/conductor/src/routes/devices/DevicesNav.vue
@@ -1,33 +1,67 @@
 <template>
   <v-list class="pa-0" dense nav>
-    <v-list-item to="/devices/all">
+    <v-list-item v-for="(device, key) in availableSubSystems" :key="key" :to="device.to">
       <v-list-item-icon>
-        <v-icon>mdi-view-list</v-icon>
+        <v-icon>{{ device.icon }}</v-icon>
       </v-list-item-icon>
-      <v-list-item-content>All</v-list-item-content>
-    </v-list-item>
-    <v-list-item to="/devices/lighting">
-      <v-list-item-icon>
-        <v-icon>mdi-lightbulb</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>Lighting</v-list-item-content>
-    </v-list-item>
-    <v-list-item to="/devices/hvac">
-      <v-list-item-icon>
-        <v-icon>mdi-thermometer</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>HVAC</v-list-item-content>
-    </v-list-item>
-    <v-list-item to="/devices/metering">
-      <v-list-item-icon>
-        <v-icon>mdi-meter-electric</v-icon>
-      </v-list-item-icon>
-      <v-list-item-content>Metering</v-list-item-content>
+      <v-list-item-content :class="device.class">{{ device.label }}</v-list-item-content>
     </v-list-item>
   </v-list>
 </template>
 
 <script setup>
+import {computed, reactive} from 'vue';
+import {useDevicesStore} from './store';
+import {newActionTracker} from '@/api/resource';
+
+const deviceStore = useDevicesStore();
+const tracker = reactive(newActionTracker());
+
+// computed
+//
+// Generating the Devices nav options
+const availableSubSystems = computed(() => {
+  // Pinia store value
+  const subSystems = deviceStore.subSystems.subs;
+
+  // navigationItemIcons
+  const navigationItemIcons = {
+    lighting: 'mdi-lightbulb',
+    hvac: 'mdi-thermometer',
+    metering: 'mdi-meter-electric'
+  };
+
+  // array of navigationItems with a default item value - 'All'
+  const navigationItems = [
+    {to: '/devices/all', icon: 'mdi-view-list', label: 'all', class: 'text-capitalize'}
+  ];
+
+  // if we have the Pinia store value
+  if (subSystems) {
+    // then loop through this value
+    for (const subSystem in subSystems) {
+      // ignore noType key/value pair
+      if (subSystem !== 'noType') {
+        // and reconstruct the object according to the template needs
+        const listItem = {
+          to: '/devices/' + subSystem,
+          icon: navigationItemIcons[subSystem],
+          label: subSystem,
+          class: subSystem === 'hvac' ? 'text-uppercase' : 'text-capitalize' // potential issue with acronyms
+        };
+
+        // then fill the array with the new object(s)
+        navigationItems.push(listItem);
+      }
+    }
+  }
+
+  // finally return the array of objects
+  return navigationItems;
+});
+
+// onCreate
+deviceStore.fetchDevicesMetadata(tracker);
 </script>
 
 <style scoped>
