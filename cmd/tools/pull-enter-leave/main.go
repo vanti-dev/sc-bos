c0fd431c1793cd422ee8893e1731979ebd2b5ea4
Matt Nathan <matt.nathan@vanti.co.uk>
2023-05-16 10:18:08 +0100

    feat: quick tool to test enter-leave trait

---
+++ cmd/tools/pull-enter-leave/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"log"
+	"time"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/vanti-dev/sc-bos/pkg/task"
+)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	task.Run(context.Background(), func(ctx context.Context) (task.Next, error) {
+		tlsConfig := &tls.Config{
+			InsecureSkipVerify: true,
+		}
+		conn, err := grpc.Dial("localhost:23557", grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+		if err != nil {
+			return 0, err
+		}
+		client := traits.NewEnterLeaveSensorApiClient(conn)
+		stream, err := client.PullEnterLeaveEvents(ctx, &traits.PullEnterLeaveEventsRequest{Name: "enter-leave"})
+		if err != nil {
+			return 0, err
+		}
+		for {
+			event, err := stream.Recv()
+			if err != nil {
+				return task.ResetBackoff, err
+			}
+			log.Printf("Got event: %+v", event)
+		}
+	}, task.WithRetry(task.RetryUnlimited), task.WithBackoff(time.Second, 30*time.Second), task.WithErrorLogger(logger))
+}
