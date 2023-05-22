0f3be80cef40438a7f071eab9175f32c021ab320
richardOven <richard.oven@vanti.co.uk>
2023-05-22 16:57:35 +0100

    New component: TableHeader

---
+++ ui/conductor/src/components/composables/DataTable/TableHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <thead>
+    <tr>
+      <th
+          v-for="header in props.tableHeaders"
+          :key="header.text"
+          :align="header?.align"
+          :width="header?.width">
+        {{ header.text }}
+      </th>
+    </tr>
+  </thead>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  tableHeaders: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
+<style lang="scss" scoped>
+:deep(.v-data-table-header) {
+  margin-left: 8px;
+}
+</style>
