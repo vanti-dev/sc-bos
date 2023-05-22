082b260221e65c271fc56cb083d20a9604296405
Matt Nathan <matt.nathan@vanti.co.uk>
2023-05-22 15:27:39 +0100

    fix(ui/ops): make the energy graph on the ops dashboard work

--- ui/conductor/src/api/sc/traits/meter-history.js
+++ ui/conductor/src/api/sc/traits/meter-history.js
@@ -1,18 +1,20 @@
+import {fieldMaskFromObject, setProperties} from '@/api/convpb';
 import {clientOptions} from '@/api/grpcweb';
 import {trackAction} from '@/api/resource';
+import {periodFromObject} from '@/api/sc/types/period';
 import {MeterHistoryPromiseClient} from '@sc-bos/ui-gen/proto/history_grpc_web_pb';
 import {ListMeterReadingHistoryRequest} from '@sc-bos/ui-gen/proto/history_pb';
 
 /**
  *
- * @param {string} name
+ * @param {ListMeterReadingHistoryRequest.AsObject} request
  * @param {ActionTracker<ListMeterReadingHistoryResponse.AsObject>} tracker
  * @return {Promise<ListMeterReadingHistoryResponse.AsObject>}
  */
-export function listMeterReadingHistory(name, tracker) {
+export function listMeterReadingHistory(request, tracker) {
   return trackAction('MeterReadingHistory.listMeterReadingHistory', tracker, endpoint => {
     const api = client(endpoint);
-    return api.listMeterReadingHistory(new ListMeterReadingHistoryRequest().setName(name));
+    return api.listMeterReadingHistory(listMeterReadingHistoryRequestFromObject(request));
   });
 }
 
@@ -24,4 +26,17 @@ export function listMeterReadingHistory(name, tracker) {
  */
 function client(endpoint) {
   return new MeterHistoryPromiseClient(endpoint, null, clientOptions());
+}
+
+/**
+ * @param {ListMeterReadingHistoryRequest.AsObject} obj
+ * @return {ListMeterReadingHistoryRequest|undefined}
+ */
+function listMeterReadingHistoryRequestFromObject(obj) {
+  if (!obj) return undefined;
+  const dst = new ListMeterReadingHistoryRequest();
+  setProperties(dst, obj, 'name', 'pageToken', 'pageSize');
+  dst.setReadMask(fieldMaskFromObject(obj.readMask));
+  dst.setPeriod(periodFromObject(obj.period));
+  return dst;
 }
--- ui/conductor/src/routes/ops/components/EnergyGraph.vue
+++ ui/conductor/src/routes/ops/components/EnergyGraph.vue
@@ -1,66 +1,149 @@
 <template>
-  <div id="energy-graph">
-    <apexchart type="area" height="180" :options="options" :series="series" v-if="data.length > 0"/>
+  <div id="energy-graph" :style="{width, height}">
+    <apexchart type="area" height="100%" :options="options" :series="series" v-if="data.length > 0"/>
     <v-card-text v-else class="error">No data available</v-card-text>
   </div>
 </template>
 
 <script setup>
-import {computed, onMounted, onUnmounted, reactive, watch} from 'vue';
-import {closeResource, newActionTracker} from '@/api/resource';
+import {timestampToDate} from '@/api/convpb';
 import {listMeterReadingHistory} from '@/api/sc/traits/meter-history';
 import {useErrorStore} from '@/components/ui-error/error';
+import {computed, onMounted, onUnmounted, ref, watch} from 'vue';
 
 const props = defineProps({
   name: {
     type: String,
-    default: 'building'
+    default: ''
+  },
+  width: {
+    type: String,
+    default: '430px'
+  },
+  height: {
+    type: String,
+    default: '230px'
+  },
+  span: { // how wide the bars of the histogram are
+    type: Number,
+    default: 15 * 60 * 1000 // in ms
   }
 });
 
-const meterHistoryValue = reactive(newActionTracker());
-
-// UI Error handling
-const errorStore = useErrorStore();
-let unwatchMeterHistoryErrors;
+const pollDelay = computed(() => props.span / 10);
+const now = ref(Date.now());
+const nowHandle = ref(0);
 onMounted(() => {
-  unwatchMeterHistoryErrors = errorStore.registerTracker(meterHistoryValue);
+  nowHandle.value = setInterval(() => {
+    now.value = Date.now();
+  }, pollDelay.value);
 });
 onUnmounted(() => {
-  if (unwatchMeterHistoryErrors) unwatchMeterHistoryErrors();
+  clearInterval(nowHandle.value);
+});
+const baseRequest = computed(() => {
+  if (!props.name) return undefined;
+  const period = {
+    startTime: new Date(now.value - 24 * 60 * 60 * 1000)
+  };
+  return ({
+    name: props.name,
+    period,
+    pageSize: 1000,
+    pageToken: ''
+  });
+});
+const meterHistoryRecords = ref(/** @type {MeterReadingRecord.AsObject[]} */ []);
+
+const pollHandle = ref(0);
+
+async function pollReadings() {
+  const req = baseRequest.value;
+  const all = [];
+  try {
+    while (true) {
+      const page = await listMeterReadingHistory(req, {});
+      all.push(...page.meterReadingRecordsList);
+      req.pageToken = page.nextPageToken;
+      if (!req.pageToken) {
+        break;
+      }
+    }
+  } catch (e) {
+    console.error('error getting meter readings', e);
+  }
+  meterHistoryRecords.value = all;
+  pollHandle.value = setTimeout(pollReadings, pollDelay.value);
+}
+
+onUnmounted(() => {
+  clearTimeout(pollHandle.value);
 });
 
-watch(() => props.name, (name) => {
+watch(() => baseRequest.value, (baseRequest) => {
   // close existing stream if present
-  closeResource(meterHistoryValue);
+  clearTimeout(pollHandle.value);
+  meterHistoryRecords.value = [];
 
   // create new stream
-  if (name && name !== '') {
-    listMeterReadingHistory(name, meterHistoryValue)
-        .catch((e) => {});
+  if (baseRequest) {
+    pollReadings();
   }
 }, {immediate: true});
 
 const data = computed(() => {
-  const res = meterHistoryValue.response;
-  if (res?.meterReadingRecordsList) {
+  const span = props.span;
+  const dst = [];
+  const records = meterHistoryRecords.value;
+  if (records.length > 0) {
     // create a list of data points that show the change in value since the previous reading
-    return res.meterReadingRecordsList.map((reading, i) => {
-      if (i === 0) {
-        return {
-          x: new Date(reading.meterReading.endTime.seconds * 1000),
-          y: reading.meterReading.usage
-        };
-      } else {
-        return {
-          x: new Date(reading.meterReading.endTime.seconds * 1000),
-          y: reading.meterReading.usage - res.meterReadingRecordsList[i - 1].meterReading.usage
-        };
+    /** @type {MeterReadingRecord.AsObject} */
+    let lastReading = null;
+    /** @type {MeterReadingRecord.AsObject} */
+    let readingCur = null;
+    for (const record of records) {
+      if (!lastReading) {
+        lastReading = record;
+        readingCur = record;
+        continue;
       }
-    });
-  } else {
-    return [];
+
+      // special case if the meter was reset
+      if (readingCur.meterReading.usage > record.meterReading.usage) {
+        const diff = readingCur.meterReading.usage - lastReading.meterReading.usage;
+        dst.push({
+          x: new Date(timestampToDate(readingCur.recordTime)),
+          y: diff
+        });
+        lastReading = readingCur = record;
+        continue;
+      }
+      readingCur = record;
+      const t0 = timestampToDate(lastReading.recordTime);
+      const t1 = timestampToDate(record.recordTime);
+      const d = t1 - t0;
+      if (d > span) {
+        const diff = record.meterReading.usage - lastReading.meterReading.usage;
+        lastReading = record;
+        dst.push({
+          x: new Date(t1),
+          y: diff
+        });
+      }
+    }
+    // process the last reading, if we haven't already
+    const finalReading = records[records.length - 1];
+    const t0 = timestampToDate(lastReading.recordTime);
+    const t1 = timestampToDate(finalReading.recordTime);
+    if (t0 !== t1) {
+      const diff = finalReading.meterReading.usage - lastReading.meterReading.usage;
+      dst.push({
+        x: new Date(t1),
+        y: diff
+      });
+    }
   }
+  return dst;
 });
 
 const options = {
@@ -113,6 +196,19 @@ const series = computed(() => {
     name: 'Energy Use',
     data: Object.values(data.value)
   }];
+});
+
+
+// UI Error handling
+const errorStore = useErrorStore();
+const unwatchErrors = [];
+onMounted(() => {
+  unwatchErrors.push(errorStore.registerTracker(meterHistoryRecords));
+});
+onUnmounted(() => {
+  for (const unwatchError of unwatchErrors) {
+    unwatchError();
+  }
 });
 
 </script>
