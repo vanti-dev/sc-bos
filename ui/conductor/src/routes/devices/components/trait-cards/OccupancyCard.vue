95395f72f63ac9790830c630d82807bb469ec9d3
richardOven <richard.oven@vanti.co.uk>
2023-05-30 22:42:20 +0100

    update(ui): occupancy card uses new components

--- ui/conductor/src/routes/devices/components/trait-cards/OccupancyCard.vue
+++ ui/conductor/src/routes/devices/components/trait-cards/OccupancyCard.vue
@@ -2,66 +2,46 @@
   <v-card elevation="0" tile>
     <v-list tile class="ma-0 pa-0">
       <v-subheader class="text-title-caps-large neutral--text text--lighten-3">Occupancy Sensor</v-subheader>
-      <v-list-item class="py-1">
-        <v-list-item-title class="text-body-small text-capitalize">State</v-list-item-title>
-        <v-list-item-subtitle class="text-capitalize" :class="state.toLowerCase()">{{ state }}</v-list-item-subtitle>
-      </v-list-item>
-      <v-list-item class="py-1" v-if="count !== 0">
-        <v-list-item-title class="text-body-small text-capitalize">Count</v-list-item-title>
-        <v-list-item-subtitle class="text-capitalize">{{ count }}</v-list-item-subtitle>
-      </v-list-item>
+      <HotPoint :item="sidebarData" :item-key="props.name">
+        <template #hotpoint>
+          <WithOccupancy :name="props.name">
+            <template #occupancy="{occupancyData}">
+              <v-list-item class="py-1">
+                <v-list-item-title class="text-body-small text-capitalize">State</v-list-item-title>
+                <v-list-item-subtitle
+                    :class="[
+                      occupancyData.occupancyState.toLowerCase(),
+                      'text-capitalize text-subtitle-2 py-1 font-weight-medium'
+                    ]">
+                  {{ occupancyData.occupancyState }}
+                </v-list-item-subtitle>
+              </v-list-item>
+              <v-list-item class="py-1" v-if="occupancyData.occupantCount !== 0">
+                <v-list-item-title class="text-body-small text-capitalize">Count</v-list-item-title>
+                <v-list-item-subtitle class="text-capitalize">{{ occupancyData.occupantCount }}</v-list-item-subtitle>
+              </v-list-item>
+              <v-progress-linear color="primary" indeterminate :active="occupancyData.occupancyValue.loading"/>
+            </template>
+          </WithOccupancy>
+        </template>
+      </HotPoint>
     </v-list>
-    <v-progress-linear color="primary" indeterminate :active="occupancyValue.loading"/>
   </v-card>
 </template>
 
 <script setup>
+import WithOccupancy from '../renderless/WithOccupancy.vue';
+import HotPoint from '@/components/HotPoint.vue';
+import {usePageStore} from '@/stores/page';
 
-import {closeResource, newResourceValue} from '@/api/resource';
-import {occupancyStateToString, pullOccupancy} from '@/api/sc/traits/occupancy';
-import {useErrorStore} from '@/components/ui-error/error';
-import {computed, onMounted, onUnmounted, reactive, watch} from 'vue';
+const pageStore = usePageStore();
+const {sidebarData} = pageStore;
 
 const props = defineProps({
-  // unique name of the device
   name: {
     type: String,
     default: ''
   }
-});
-
-const occupancyValue = reactive(/** @type{ResourceValue<Occupancy.AsObject, Occupancy>} */newResourceValue());
-
-const state = computed(() => {
-  if (occupancyValue.value) {
-    return occupancyStateToString(occupancyValue.value.state);
-  }
-  return 'unknown';
-});
-const count = computed(() => {
-  if (occupancyValue.value) {
-    return occupancyValue.value.peopleCount;
-  }
-  return 0;
-});
-
-watch(() => props.name, async (name) => {
-  // close existing stream if present
-  closeResource(occupancyValue);
-  // create new stream
-  if (name && name !== '') {
-    await pullOccupancy(name, occupancyValue);
-  }
-}, {immediate: true});
-
-// UI error handling
-const errorStore = useErrorStore();
-let unwatchOccupancyError;
-onMounted(() => {
-  unwatchOccupancyError = errorStore.registerValue(occupancyValue);
-});
-onUnmounted(() => {
-  if (unwatchOccupancyError) unwatchOccupancyError();
 });
 
 </script>
