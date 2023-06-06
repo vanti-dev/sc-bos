0d946e6c046f05177b176fde5cb1c9e72721d563
richardOven <richard.oven@vanti.co.uk>
2023-06-06 12:35:55 +0100

    update(ui.ops): Humidity gauge if > 0

--- ui/conductor/src/routes/ops/components/EnvironmentalCard.vue
+++ ui/conductor/src/routes/ops/components/EnvironmentalCard.vue
@@ -1,6 +1,6 @@
 <template>
   <content-card class="mb-8 d-flex flex-column px-6 pt-md-6">
-    <h4 class="text-h4 order-lg-last pb-4 pb-lg-0 pt-0 pt-lg-4">Environmental</h4>
+    <h4 class="text-h4 pb-8 pt-0">Environmental</h4>
     <div class="d-flex flex-column flex-md-row flex-lg-column">
       <circular-gauge
           :value="temperature"
@@ -11,7 +11,9 @@
         {{ temperature.toFixed(1) }}&deg;
         <template #title>Avg. Indoor Temperature</template>
       </circular-gauge>
-      <div class="align-self-center mb-6 mb-md-0 mb-lg-8 mr-md-8 mr-lg-0" style="width: 205px;">
+      <div
+          :class="[humidity > 0 ? 'mb-6 mb-md-0 mb-lg-8' : 'mb-2', 'align-self-center mr-md-8 mr-lg-0']"
+          style="width: 205px;">
         <span
             class="text-title"
             style="display: inline-block; width: 100px;">External Temperature</span>
@@ -20,6 +22,7 @@
             style="display: inline-block; float: right;">{{ externalTemperature.toFixed(1) }}&deg;</span>
       </div>
       <circular-gauge
+          v-if="humidity > 0"
           :value="humidity"
           segments="30"
           class="align-self-center">
