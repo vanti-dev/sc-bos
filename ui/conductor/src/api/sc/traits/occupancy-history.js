13b65fde92e97bab3adcdd48daa2f0e3b5a607bc
richardOven <richard.oven@vanti.co.uk>
2023-06-08 17:34:15 +0100

    feat(ui.ops): pull in OccupancySensorHistory

--- ui/conductor/src/api/sc/traits/occupancy-history.js
+++ ui/conductor/src/api/sc/traits/occupancy-history.js
@@ -2,20 +2,21 @@ import {clientOptions} from '@/api/grpcweb';
 import {trackAction} from '@/api/resource';
 import {OccupancySensorHistoryPromiseClient} from '@sc-bos/ui-gen/proto/history_grpc_web_pb';
 import {ListOccupancyHistoryRequest} from '@sc-bos/ui-gen/proto/history_pb';
+import {fieldMaskFromObject, setProperties} from '@/api/convpb';
+import {periodFromObject} from '@/api/sc/types/period';
 
 /**
  *
- * @param {string} name
+ * @param {ListOccupancyHistoryRequest.AsObject} request
  * @param {ResourceValue<ListOccupancyHistoryResponse.AsObject>} tracker
  * @return {Promise<ListOccupancyHistoryResponse.AsObject>}
  */
-export function listOccupancySensorHistory(name, tracker) {
-  return trackAction('OccupancySensorHistory.listOccupancySensorHistory', tracker, endpoint => {
+export function listOccupancySensorHistory(request, tracker) {
+  return trackAction('OccupancySensorHistory.listOccupancySensorHistory', tracker, (endpoint) => {
     const api = client(endpoint);
-    return api.listOccupancyHistory(new ListOccupancyHistoryRequest().setName(name));
+    return api.listOccupancyHistory(listOccupancySensorHistoryRequestFromObject(request));
   });
 }
-
 
 /**
  *
@@ -24,4 +25,17 @@ export function listOccupancySensorHistory(name, tracker) {
  */
 function client(endpoint) {
   return new OccupancySensorHistoryPromiseClient(endpoint, null, clientOptions());
+}
+
+/**
+ * @param {ListOccupancyHistoryRequest.AsObject} obj
+ * @return {ListOccupancyHistoryRequest|undefined}
+ */
+function listOccupancySensorHistoryRequestFromObject(obj) {
+  if (!obj) return undefined;
+  const dst = new ListOccupancyHistoryRequest();
+  setProperties(dst, obj, 'name', 'pageToken', 'pageSize');
+  dst.setReadMask(fieldMaskFromObject(obj.readMask));
+  dst.setPeriod(periodFromObject(obj.period));
+  return dst;
 }
