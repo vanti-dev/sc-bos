220fa37acadbef1caa542ccebf1717da7dea9490
richardOven <richard.oven@vanti.co.uk>
2023-06-08 17:33:31 +0100

    feat(ui.ops): display live Occupancy count

--- ui/conductor/src/routes/ops/components/OccupancyCard.vue
+++ ui/conductor/src/routes/ops/components/OccupancyCard.vue
@@ -1,32 +1,30 @@
 <template>
   <content-card class="mb-8 d-flex flex-column px-6 pt-md-6">
-    <v-row class="px-6 mb-6">
+    <v-row class="px-6 mb-6 pt-1">
       <h4 class="text-h4 pb-4 pb-lg-0 pt-0 pt-lg-4">Occupancy</h4>
       <v-spacer/>
-      <v-col
-          cols="2"
-          class="d-flex flex-row flex-nowrap mx-0 px-0 justify-space-between"
-          style="max-width: 175px;">
-        <div class="text-h2">
-          {{ occupancy }}
-          <span class="text-caption neutral--text text--lighten-5">/{{ maxOccupancy }}</span>
+      <v-col cols="3" class="d-flex flex-row flex-nowrap mx-0 px-0 justify-space-between" style="max-width: 160px;">
+        <WithOccupancy v-slot="{ resource }" :name="props.name">
+          <div class="text-h2 d-flex flex-row flex-nowrap align-end">
+            {{ collectOccupancyData(resource.value) }}
+            <span class="text-body-1 neutral--text text--lighten-5 pb-1 ml-1">/ {{ maxOccupancy }}</span>
+          </div>
+        </WithOccupancy>
+        <div class="text-h2 d-flex flex-row flex-nowrap align-end">
+          {{ occupancyPercentageDisplay }}
+          <span class="text-body-1 neutral--text text--lighten-9 pb-1 ml-1">%</span>
         </div>
-        <div class="text-h2">{{ occupancyPercentage.toFixed(0) }}%</div>
       </v-col>
     </v-row>
-    <OccupancyGraph/>
+    <OccupancyGraph class="flex-grow-1 d-none d-md-block" width="100%" :name="props.name"/>
   </content-card>
 </template>
 
 <script setup>
-
-import {computed, onMounted, onUnmounted, reactive, watch} from 'vue';
+import {computed, ref} from 'vue';
 import ContentCard from '@/components/ContentCard.vue';
+import WithOccupancy from '@/routes/devices/components/renderless/WithOccupancy.vue';
 import OccupancyGraph from '@/routes/ops/components/OccupancyGraph.vue';
-
-import {closeResource, newResourceValue} from '@/api/resource';
-import {pullOccupancy} from '@/api/sc/traits/occupancy';
-import {useErrorStore} from '@/components/ui-error/error';
 
 const props = defineProps({
   name: {
@@ -35,41 +33,21 @@ const props = defineProps({
   }
 });
 
-const occupancyValue = reactive(newResourceValue());
-// todo: where do we get this from?
+const peopleCount = ref(0);
 const maxOccupancy = 1234;
 
-const occupancy = computed(() => {
-  return occupancyValue.value?.peopleCount ?? 0;
-});
+const occupancyPercentage = computed(() => (peopleCount.value / maxOccupancy) * 100);
+const occupancyPercentageDisplay = computed(() =>
+  occupancyPercentage.value > 0 ? occupancyPercentage.value.toFixed(1) : occupancyPercentage.value.toFixed(0)
+);
 
-const occupancyPercentage = computed(() => occupancy.value/maxOccupancy*100);
-
-watch(() => props.name, async (name) => {
-  // close existing stream if present
-  closeResource(occupancyValue);
-  // create new stream
-  if (name && name !== '') {
-    pullOccupancy(name, occupancyValue);
-  }
-}, {immediate: true});
-
-onUnmounted(() => {
-  closeResource(occupancyValue);
-});
-
-// UI Error Handling
-const errorStore = useErrorStore();
-let unwatchErrors;
-onMounted(() => {
-  unwatchErrors = errorStore.registerValue(occupancyValue);
-});
-onUnmounted(() => {
-  if (unwatchErrors) unwatchErrors();
-});
-
+/**
+ *
+ * @param {*} value
+ * @return {number}
+ */
+function collectOccupancyData(value) {
+  if (value) peopleCount.value = value.peopleCount;
+  return peopleCount.value;
+}
 </script>
-
-<style scoped>
-
-</style>
