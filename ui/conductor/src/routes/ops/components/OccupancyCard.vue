ef9481bde6a7c5a903133b3ddb57c67064b58509
richardOven <richard.oven@vanti.co.uk>
2023-06-09 15:57:30 +0100

    fix(ui.ops): moved logic to own component

--- ui/conductor/src/routes/ops/components/OccupancyCard.vue
+++ ui/conductor/src/routes/ops/components/OccupancyCard.vue
@@ -3,17 +3,12 @@
     <v-row class="px-6 mb-6 pt-1">
       <h4 class="text-h4 pb-4 pb-lg-0 pt-0 pt-lg-4">Occupancy</h4>
       <v-spacer/>
-      <v-col cols="3" class="d-flex flex-row flex-nowrap mx-0 px-0 justify-space-between" style="max-width: 160px;">
-        <WithOccupancy v-slot="{ resource }" :name="props.name">
-          <div class="text-h2 d-flex flex-row flex-nowrap align-end">
-            {{ collectOccupancyData(resource.value) }}
-            <span class="text-body-1 neutral--text text--lighten-5 pb-1 ml-1">/ {{ maxOccupancy }}</span>
-          </div>
+      <v-col cols="3" class="d-flex flex-row flex-nowrap mx-0 px-0 justify-space-between" style="max-width: 150px;">
+        <WithOccupancy
+            v-slot="{ resource }"
+            :name="props.name">
+          <OccupancyPeopleCount :people-count="resource?.value?.peopleCount"/>
         </WithOccupancy>
-        <div class="text-h2 d-flex flex-row flex-nowrap align-end">
-          {{ occupancyPercentageDisplay }}
-          <span class="text-body-1 neutral--text text--lighten-9 pb-1 ml-1">%</span>
-        </div>
       </v-col>
     </v-row>
     <OccupancyGraph class="flex-grow-1 d-none d-md-block" width="100%" :name="props.name"/>
@@ -21,10 +16,10 @@
 </template>
 
 <script setup>
-import {computed, ref} from 'vue';
 import ContentCard from '@/components/ContentCard.vue';
 import WithOccupancy from '@/routes/devices/components/renderless/WithOccupancy.vue';
 import OccupancyGraph from '@/routes/ops/components/OccupancyGraph.vue';
+import OccupancyPeopleCount from './OccupancyPeopleCount.vue';
 
 const props = defineProps({
   name: {
@@ -32,22 +27,4 @@ const props = defineProps({
     default: 'building'
   }
 });
-
-const peopleCount = ref(0);
-const maxOccupancy = 1234;
-
-const occupancyPercentage = computed(() => (peopleCount.value / maxOccupancy) * 100);
-const occupancyPercentageDisplay = computed(() =>
-  occupancyPercentage.value > 0 ? occupancyPercentage.value.toFixed(1) : occupancyPercentage.value.toFixed(0)
-);
-
-/**
- *
- * @param {*} value
- * @return {number}
- */
-function collectOccupancyData(value) {
-  if (value) peopleCount.value = value.peopleCount;
-  return peopleCount.value;
-}
 </script>
