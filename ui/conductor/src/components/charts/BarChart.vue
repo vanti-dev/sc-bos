cd09975f5e78e7bc0e41c1bd89d37d4c614fcb56
richardOven <richard.oven@vanti.co.uk>
2023-06-22 14:37:26 +0100

    new component: BarChart

---
+++ ui/conductor/src/components/charts/BarChart.vue
@@ -0,0 +1,50 @@
+<template>
+  <Bar
+      :options="props.chartOptions"
+      :data="props.chartData"
+      :dataset-id-key="props.datasetIdKey"
+      :css-classes="props.cssClasses"
+      :styles="props.styles"/>
+</template>
+
+<script setup>
+import {Bar} from 'vue-chartjs';
+import {Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+const props = defineProps({
+  chartId: {
+    type: String,
+    default: 'bar-chart'
+  },
+  datasetIdKey: {
+    type: String,
+    default: 'label'
+  },
+  cssClasses: {
+    type: String,
+    default: 'position-relative'
+  },
+  styles: {
+    type: Object,
+    default: () => {
+      return {
+        height: ''
+      };
+    }
+  },
+  chartData: {
+    type: Object,
+    default: () => {
+      return {};
+    }
+  },
+  chartOptions: {
+    type: Object,
+    default: () => {
+      return {};
+    }
+  }
+});
+</script>
