a72cf632fe5622548fadc0276c4012b9203e765e
richardOven <richard.oven@vanti.co.uk>
2023-06-23 14:01:18 +0100

    new component: LineChart

---
+++ ui/conductor/src/components/charts/LineChart.vue
@@ -0,0 +1,56 @@
+<template>
+  <LineChartGenerator
+      :options="props.chartOptions"
+      :data="props.chartData"
+      :dataset-id-key="props.datasetIdKey"
+      :css-classes="props.cssClasses"
+      :styles="props.styles"/>
+</template>
+
+<script setup>
+import {Line as LineChartGenerator} from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  Filler,
+  LinearScale,
+  CategoryScale,
+  PointElement
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, LinearScale, Filler, CategoryScale, PointElement);
+
+const props = defineProps({
+  datasetIdKey: {
+    type: String,
+    default: 'label'
+  },
+  cssClasses: {
+    type: String,
+    default: 'position-relative'
+  },
+  styles: {
+    type: Object,
+    default: () => {
+      return {
+        height: ''
+      };
+    }
+  },
+  chartData: {
+    type: Object,
+    default: () => {
+      return {};
+    }
+  },
+  chartOptions: {
+    type: Object,
+    default: () => {
+      return {};
+    }
+  }
+});
+</script>
