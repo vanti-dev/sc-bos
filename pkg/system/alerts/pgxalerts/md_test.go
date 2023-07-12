0b16c94dcb09b82ff6b3989293377aa2e7af47df
Matt Nathan <matt.nathan@vanti.co.uk>
2023-07-12 17:30:09 +0100

    alerts: fix tests that broke because of the new metadata fields

--- pkg/system/alerts/pgxalerts/md_test.go
+++ pkg/system/alerts/pgxalerts/md_test.go
@@ -19,6 +19,16 @@ func Test_applyMdDelta(t *testing.T) {
 			true:  80,
 			false: 20,
 		},
+		ResolvedCounts: map[bool]uint32{
+			true:  20,
+			false: 80,
+		},
+		NeedsAttentionCounts: map[string]uint32{
+			"ack_resolved":    10,
+			"ack_unresolved":  70,
+			"nack_resolved":   5,
+			"nack_unresolved": 15,
+		},
 		FloorCounts: map[string]uint32{
 			"Floor1": 20,
 			"Floor2": 30,
@@ -39,8 +49,22 @@ func Test_applyMdDelta(t *testing.T) {
 		},
 	}
 	// base structs for an added and removed alert without acknowledgement
-	added := patch(before, &gen.AlertMetadata{TotalCount: 101, AcknowledgedCounts: map[bool]uint32{false: 21}})
-	removed := patch(before, &gen.AlertMetadata{TotalCount: 99, AcknowledgedCounts: map[bool]uint32{false: 19}})
+	added := patch(before, &gen.AlertMetadata{
+		TotalCount:         101,
+		AcknowledgedCounts: map[bool]uint32{false: 21},
+		ResolvedCounts:     map[bool]uint32{false: 81},
+		NeedsAttentionCounts: map[string]uint32{
+			"nack_unresolved": 16,
+		},
+	})
+	removed := patch(before, &gen.AlertMetadata{
+		TotalCount:         99,
+		AcknowledgedCounts: map[bool]uint32{false: 19},
+		ResolvedCounts:     map[bool]uint32{false: 79},
+		NeedsAttentionCounts: map[string]uint32{
+			"nack_unresolved": 14,
+		},
+	})
 
 	tests := []struct {
 		name          string
@@ -53,8 +77,14 @@ func Test_applyMdDelta(t *testing.T) {
 
 		{"add empty", before, added, &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{}}, false},
 		{"remove empty", before, removed, &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{}}, false},
-		{"add ack", before, patch(added, &gen.AlertMetadata{AcknowledgedCounts: map[bool]uint32{true: 81, false: 20}}), &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
-		{"remove ack", before, patch(removed, &gen.AlertMetadata{AcknowledgedCounts: map[bool]uint32{true: 79, false: 20}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
+		{"add ack", before, patch(added, &gen.AlertMetadata{
+			AcknowledgedCounts:   map[bool]uint32{true: 81, false: 20},
+			NeedsAttentionCounts: map[string]uint32{"nack_unresolved": 15, "ack_unresolved": 71},
+		}), &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
+		{"remove ack", before, patch(removed, &gen.AlertMetadata{
+			AcknowledgedCounts:   map[bool]uint32{true: 79, false: 20},
+			NeedsAttentionCounts: map[string]uint32{"nack_unresolved": 15, "ack_unresolved": 69},
+		}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
 		{"add floor", before, patch(added, &gen.AlertMetadata{FloorCounts: map[string]uint32{"Floor1": 21}}), &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{Floor: "Floor1"}}, false},
 		{"remove floor", before, patch(removed, &gen.AlertMetadata{FloorCounts: map[string]uint32{"Floor1": 19}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Floor: "Floor1"}}, false},
 		{"add zone", before, patch(added, &gen.AlertMetadata{ZoneCounts: map[string]uint32{"Zone1": 21}}), &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{Zone: "Zone1"}}, false},
@@ -62,8 +92,14 @@ func Test_applyMdDelta(t *testing.T) {
 		{"add severity", before, patch(added, &gen.AlertMetadata{SeverityCounts: map[int32]uint32{1: 21}}), &gen.PullAlertsResponse_Change{NewValue: &gen.Alert{Severity: 1}}, false},
 		{"remove severity", before, patch(removed, &gen.AlertMetadata{SeverityCounts: map[int32]uint32{1: 19}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Severity: 1}}, false},
 
-		{"update ack (ack->nak)", before, patch(before, &gen.AlertMetadata{AcknowledgedCounts: map[bool]uint32{true: 79, false: 21}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}, NewValue: &gen.Alert{}}, false},
-		{"update ack (nak->ack)", before, patch(before, &gen.AlertMetadata{AcknowledgedCounts: map[bool]uint32{true: 81, false: 19}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{}, NewValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
+		{"update ack (ack->nak)", before, patch(before, &gen.AlertMetadata{
+			AcknowledgedCounts:   map[bool]uint32{true: 79, false: 21},
+			NeedsAttentionCounts: map[string]uint32{"nack_unresolved": 16, "ack_unresolved": 69},
+		}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}, NewValue: &gen.Alert{}}, false},
+		{"update ack (nak->ack)", before, patch(before, &gen.AlertMetadata{
+			AcknowledgedCounts:   map[bool]uint32{true: 81, false: 19},
+			NeedsAttentionCounts: map[string]uint32{"nack_unresolved": 14, "ack_unresolved": 71},
+		}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{}, NewValue: &gen.Alert{Acknowledgement: &gen.Alert_Acknowledgement{AcknowledgeTime: timestamppb.Now()}}}, false},
 		{"update floor", before, patch(before, &gen.AlertMetadata{FloorCounts: map[string]uint32{"Floor1": 19, "Floor2": 31}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Floor: "Floor1"}, NewValue: &gen.Alert{Floor: "Floor2"}}, false},
 		{"update floor (zero->)", before, patch(before, &gen.AlertMetadata{FloorCounts: map[string]uint32{"Floor1": 21}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Floor: ""}, NewValue: &gen.Alert{Floor: "Floor1"}}, false},
 		{"update floor (->zero)", before, patch(before, &gen.AlertMetadata{FloorCounts: map[string]uint32{"Floor1": 19}}), &gen.PullAlertsResponse_Change{OldValue: &gen.Alert{Floor: "Floor1"}, NewValue: &gen.Alert{Floor: ""}}, false},
@@ -93,10 +129,12 @@ func Test_applyMdDelta(t *testing.T) {
 
 	t.Run("empty old maps", func(t *testing.T) {
 		res := resource.NewValue(resource.WithInitialValue(&gen.AlertMetadata{
-			FloorCounts:        make(map[string]uint32),
-			ZoneCounts:         make(map[string]uint32),
-			SeverityCounts:     make(map[int32]uint32),
-			AcknowledgedCounts: make(map[bool]uint32),
+			FloorCounts:          make(map[string]uint32),
+			ZoneCounts:           make(map[string]uint32),
+			SeverityCounts:       make(map[int32]uint32),
+			AcknowledgedCounts:   make(map[bool]uint32),
+			ResolvedCounts:       make(map[bool]uint32),
+			NeedsAttentionCounts: make(map[string]uint32),
 		}))
 		err := applyMdDelta(res, &gen.PullAlertsResponse_Change{
 			NewValue: &gen.Alert{Floor: "foo", Zone: "bar", Severity: 1},
@@ -106,11 +144,13 @@ func Test_applyMdDelta(t *testing.T) {
 		}
 		got := res.Get()
 		want := &gen.AlertMetadata{
-			TotalCount:         1,
-			FloorCounts:        map[string]uint32{"foo": 1},
-			ZoneCounts:         map[string]uint32{"bar": 1},
-			SeverityCounts:     map[int32]uint32{1: 1},
-			AcknowledgedCounts: map[bool]uint32{false: 1},
+			TotalCount:           1,
+			FloorCounts:          map[string]uint32{"foo": 1},
+			ZoneCounts:           map[string]uint32{"bar": 1},
+			SeverityCounts:       map[int32]uint32{1: 1},
+			AcknowledgedCounts:   map[bool]uint32{false: 1},
+			ResolvedCounts:       map[bool]uint32{false: 1},
+			NeedsAttentionCounts: map[string]uint32{"nack_unresolved": 1},
 		}
 		if diff := cmp.Diff(want, got, protocmp.Transform()); diff != "" {
 			t.Fatalf("applyMdDelta md (-want,+got)\n%s", diff)
