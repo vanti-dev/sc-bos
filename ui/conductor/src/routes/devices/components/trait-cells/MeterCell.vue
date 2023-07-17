75b22cb8731e9410be8327ce84be31e8a5971597
richardOven <richard.oven@vanti.co.uk>
2023-07-17 12:43:11 +0100

    ops/ui: create and use MeterCell component

---
+++ ui/conductor/src/routes/devices/components/trait-cells/MeterCell.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="text-no-wrap ed-cell" v-if="meterReading">
+    <span>{{ meterReading }} kWh</span>
+  </span>
+</template>
+<script setup>
+import {computed} from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Object, // of MeterReading.AsObject
+    default: () => {
+    }
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const meterReading = computed(() => {
+  return props.value?.usage.toFixed(2);
+});
+</script>
+
+<style scoped>
+.el-cell {
+  display: flex;
+  align-items: center;
+}
+</style>
