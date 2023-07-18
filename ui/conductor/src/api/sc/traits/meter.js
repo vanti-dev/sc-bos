4f3a21ad68e2a3e6e79d39ebad8f865538ac131b
richardOven <richard.oven@vanti.co.uk>
2023-07-18 09:59:37 +0100

    ops/ui: add api call for meter unit type

--- ui/conductor/src/api/sc/traits/meter.js
+++ ui/conductor/src/api/sc/traits/meter.js
@@ -1,23 +1,35 @@
-import {pullResource, setValue} from '@/api/resource.js';
-import {MeterApiPromiseClient} from '@sc-bos/ui-gen/proto/meter_grpc_web_pb';
+import {pullResource, setValue, trackAction} from '@/api/resource.js';
+import {MeterApiPromiseClient, MeterInfoPromiseClient} from '@sc-bos/ui-gen/proto/meter_grpc_web_pb';
 import {clientOptions} from '@/api/grpcweb';
-import {PullMeterReadingsRequest} from '@sc-bos/ui-gen/proto/meter_pb';
-
+import {PullMeterReadingsRequest, DescribeMeterReadingRequest} from '@sc-bos/ui-gen/proto/meter_pb';
 
 /**
  * @param {string} name
  * @param {ResourceValue<MeterReading.AsObject, MeterReading>} resource
  */
 export function pullMeterReading(name, resource) {
-  pullResource('MeterApi.MeterReading', resource, endpoint => {
+  pullResource('MeterApi.MeterReading', resource, (endpoint) => {
     const api = new MeterApiPromiseClient(endpoint, null, clientOptions());
     const stream = api.pullMeterReadings(new PullMeterReadingsRequest().setName(name));
-    stream.on('data', msg => {
+    stream.on('data', (msg) => {
       const changes = msg.getChangesList();
       for (const change of changes) {
         setValue(resource, change.getMeterReading().toObject());
       }
     });
     return stream;
+  });
+}
+
+/**
+ *
+ * @param {string} name
+ * @param {ActionTracker<MeterReadingSupport.AsObject>} tracker
+ * @return {Promise<MeterReadingSupport.AsObject>}
+ */
+export function describeMeterReading(name, tracker) {
+  return trackAction('MeterApi.DescribeMeterReading', tracker ?? {}, (endpoint) => {
+    const api = new MeterInfoPromiseClient(endpoint, null, clientOptions());
+    return api.describeMeterReading(new DescribeMeterReadingRequest().setName(name));
   });
 }
