0b298c08d01cc7853cfde2fa5e766f40079ff727
richardOven <richard.oven@vanti.co.uk>
2023-07-18 11:30:46 +0100

    ops/ui: refactored device sidebar resize

--- ui/conductor/src/components/page-layout/SidebarPage.vue
+++ ui/conductor/src/components/page-layout/SidebarPage.vue
@@ -6,9 +6,9 @@
     <v-navigation-drawer
         v-if="hasSidebar"
         v-model="showSidebar"
-        id="right-sidebar"
+        ref="rightSidebar"
         app
-        class="pa-0"
+        class="rightSidebar pa-0"
         clipped
         color="neutral"
         floating
@@ -20,7 +20,7 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, onBeforeUnmount} from 'vue';
 import {storeToRefs} from 'pinia';
 import {usePage} from '@/components/page';
 import {usePageStore} from '@/stores/page';
@@ -30,73 +30,78 @@ const {hasSidebar} = usePage();
 const pageStore = usePageStore();
 const {showSidebar} = storeToRefs(pageStore);
 
+const rightSidebar = ref(null);
+const drawerBorder = ref(null);
 const sideBar = ref({
   width: 275,
   borderSize: 4
 });
 
-/**
- * Set the initial border width and styles of the sidebar.
- */
-const setBorderWidth = () => {
-  const mainElement = document.querySelector('#right-sidebar');
-  const innerElement = mainElement.querySelector('.v-navigation-drawer__border');
-  innerElement.style.width = sideBar.value.borderSize + 'px';
-  innerElement.style.cursor = 'ew-resize';
-  innerElement.style.backgroundColor = 'var(--v-primary-darken4)';
+let originalCursor = '';
+
+// Handle mouse move for sidebar resizing
+const handleMouseMove = (event) => {
+  document.body.style.cursor = 'ew-resize'; // Set cursor
+  drawerBorder.value.style.width = '8px'; // Set border width
+  let forced = document.body.scrollWidth - event.clientX; // Calculate width
+
+  forced = Math.min(forced, 600); // Set max width
+  forced = Math.max(forced, 275); // Set min width
+
+  rightSidebar.value.$el.style.width = `${forced}px`; // Set width
 };
 
-/**
- * Set event listeners for sidebar resizing.
- */
+const handleMouseUp = () => {
+  rightSidebar.value.$el.style.transition = ''; // Remove transition
+  sideBar.value.width = rightSidebar.value.$el.style.width; // Update sidebar width
+  drawerBorder.value.style.backgroundColor = ''; // Remove border highlight on button release
+  drawerBorder.value.style.width = ''; // Reset border width
+  document.removeEventListener('mousemove', handleMouseMove, false); // Remove event listener
+
+  document.body.style.cursor = originalCursor; // Restore original cursor value
+  rightSidebar.value.$el.style.userSelect = ''; // Enable text selection
+};
+
+// Set event listeners for sidebar resizing
 const setEvents = () => {
-  const minSize = sideBar.value.borderSize;
-  const mainElement = document.querySelector('#right-sidebar');
-  const drawerBorder = mainElement.querySelector('.v-navigation-drawer__border');
-  const direction = mainElement.classList.contains('v-navigation-drawer--right') ?
-    'right' :
-    'left';
+  drawerBorder.value.addEventListener('mousedown', (event) => {
+    originalCursor = originalCursor || document.body.style.cursor; // Store original cursor value
+    rightSidebar.value.$el.style.transition = 'all 0.1s ease'; // Add transition
+    drawerBorder.value.style.backgroundColor = 'var(--v-primary-darken1)'; // Highlight border while moving
+    drawerBorder.value.style.width = '8px'; // Set border width
+    document.addEventListener('mousemove', handleMouseMove, false); // Add event listener
 
-  /**
-   * Handle the resizing of the sidebar.
-   *
-   * @param {MouseEvent} event - The mouse event object.
-   */
-  const resize = (event) => {
-    document.body.style.cursor = 'ew-resize';
-    let forced =
-      direction === 'right' ?
-        document.body.scrollWidth - event.clientX :
-        event.clientX;
-
-    // Enforce maximum width of 600px
-    forced = Math.min(forced, 600);
-
-    // Enforce minimum width of 275px
-    forced = Math.max(forced, 275);
-
-    mainElement.style.width = forced + 'px';
-  };
-
-  drawerBorder.addEventListener('mousedown', (event) => {
-    if (event.offsetX < minSize) {
-      mainElement.style.transition = 'initial';
-      document.addEventListener('mousemove', resize, false);
-    }
+    rightSidebar.value.$el.style.userSelect = 'none'; // Disable text selection
   });
 
-  document.addEventListener('mouseup', () => {
-    mainElement.style.transition = '';
-    sideBar.value.width = mainElement.style.width;
-    document.body.style.cursor = '';
-    document.removeEventListener('mousemove', resize, false);
-  });
+
+  document.addEventListener('mouseup', handleMouseUp); // Add event listener
 };
 
 onMounted(() => {
-  if (hasSidebar.value) {
-    setBorderWidth();
-    setEvents();
+  // If sidebar exists and is mounted
+  if (hasSidebar.value && rightSidebar.value.$el) {
+    drawerBorder.value = rightSidebar.value.$el.querySelector('.v-navigation-drawer__border'); // Get border element
+    setEvents(); // Set event listeners
   }
 });
+
+onBeforeUnmount(() => {
+  document.removeEventListener('mousemove', handleMouseMove, false); // Remove event listener
+  document.removeEventListener('mouseup', handleMouseUp, false); // Remove event listener
+});
+
 </script>
+
+<style lang="scss">
+.rightSidebar > .v-navigation-drawer__border {
+  width: 4px;
+  background-color: var(--v-primary-darken4);
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    width: 8px;
+    cursor: ew-resize;
+    background-color: var(--v-primary-darken1);
+  }
+}
+</style>
