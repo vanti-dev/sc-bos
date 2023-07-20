d0f3ca23f53de9ea8d8f0e283f0223e498cebcf7
Matt Nathan <matt.nathan@vanti.co.uk>
2023-07-20 14:03:31 +0100

    history: add history system for centralising history storage

---
+++ pkg/system/history/config/root.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/vanti-dev/sc-bos/internal/util/pgxutil"
+	"github.com/vanti-dev/sc-bos/pkg/system"
+)
+
+type Root struct {
+	system.Config
+	Storage *Storage `json:"storage,omitempty"`
+}
+
+type StorageType string
+
+const (
+	StorageTypePostgres StorageType = "postgres"
+)
+
+type Storage struct {
+	Type StorageType `json:"type,omitempty"`
+	pgxutil.ConnectConfig
+}
