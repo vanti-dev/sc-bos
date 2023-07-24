5f7b7f22331eae6b6038528391167913193a0be7
richardOven <richard.oven@vanti.co.uk>
2023-07-24 11:09:38 +0100

    ops/ui: refactor existing sidebar to use on notification sidebar

---
+++ ui/conductor/src/routes/devices/components/DeviceSideBarContent.vue
@@ -0,0 +1,65 @@
+<template>
+  <span>
+    <device-info-card/>
+    <WithStatus v-if="traits['smartcore.bos.Status']" :name="deviceId" v-slot="{resource}">
+      <v-divider class="mt-4 mb-1"/>
+      <status-log-card v-bind="resource"/>
+    </WithStatus>
+    <WithAirTemperature v-if="traits['smartcore.traits.AirTemperature']" :name="deviceId" v-slot="{resource, update}">
+      <v-divider class="mt-4 mb-1"/>
+      <air-temperature-card v-bind="resource" @updateAirTemperature="update"/>
+    </WithAirTemperature>
+    <v-divider v-if="traits['smartcore.traits.Light']" class="mt-4 mb-1"/>
+    <WithLighting v-if="traits['smartcore.traits.Light']" :name="deviceId" v-slot="{resource, update}">
+      <light-card v-bind="resource" @updateBrightness="update"/>
+    </WithLighting>
+    <v-divider v-if="traits['smartcore.traits.OccupancySensor']" class="mt-4 mb-1"/>
+    <WithOccupancy v-if="traits['smartcore.traits.OccupancySensor']" :name="deviceId" v-slot="{resource}">
+      <occupancy-card v-bind="resource"/>
+    </WithOccupancy>
+    <WithElectricDemand v-if="traits['smartcore.traits.Electric']" :name="deviceId" v-slot="{resource}">
+      <v-divider class="mt-4 mb-1"/>
+      <electric-demand-card v-bind="resource"/>
+    </WithElectricDemand>
+    <WithMeter v-if="traits['smartcore.bos.Meter']" :name="deviceId" v-slot="{resource, info}">
+      <v-divider class="mt-4 mb-1"/>
+      <meter-card v-bind="resource" :unit="info?.response?.unit"/>
+    </WithMeter>
+    <v-divider v-if="traits['smartcore.bsp.EmergencyLight']" class="mt-4 mb-1"/>
+    <emergency-light :name="deviceId" v-if="traits['smartcore.bsp.EmergencyLight']"/>
+    <v-divider v-if="traits['smartcore.traits.Mode']" class="mt-4 mb-1"/>
+    <mode-card :name="deviceId" v-if="traits['smartcore.traits.Mode']"/>
+    <v-divider v-if="traits['smartcore.bos.UDMI']" class="mt-4 mb-1"/>
+    <udmi-card :name="deviceId" v-if="traits['smartcore.bos.UDMI']"/>
+  </span>
+</template>
+
+<script setup>
+import WithAirTemperature from '@/routes/devices/components/renderless/WithAirTemperature.vue';
+import WithElectricDemand from '@/routes/devices/components/renderless/WithElectricDemand.vue';
+import WithLighting from '@/routes/devices/components/renderless/WithLighting.vue';
+import WithOccupancy from '@/routes/devices/components/renderless/WithOccupancy.vue';
+import WithStatus from '@/routes/devices/components/renderless/WithStatus.vue';
+import WithMeter from '@/routes/devices/components/renderless/WithMeter.vue';
+import AirTemperatureCard from '@/routes/devices/components/trait-cards/AirTemperatureCard.vue';
+import DeviceInfoCard from '@/routes/devices/components/trait-cards/DeviceInfoCard.vue';
+import ElectricDemandCard from '@/routes/devices/components/trait-cards/ElectricDemandCard.vue';
+import EmergencyLight from '@/routes/devices/components/trait-cards/EmergencyLight.vue';
+import LightCard from '@/routes/devices/components/trait-cards/LightCard.vue';
+import ModeCard from '@/routes/devices/components/trait-cards/ModeCard.vue';
+import OccupancyCard from '@/routes/devices/components/trait-cards/OccupancyCard.vue';
+import StatusLogCard from '@/routes/devices/components/trait-cards/StatusLogCard.vue';
+import UdmiCard from '@/routes/devices/components/trait-cards/UdmiCard.vue';
+import MeterCard from '@/routes/devices/components/trait-cards/MeterCard.vue';
+
+defineProps({
+  deviceId: {
+    type: String,
+    default: ''
+  },
+  traits: {
+    type: Object,
+    default: () => {}
+  }
+});
+</script>
