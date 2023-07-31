60ae99865367f7a65fba0ef9d510b980b5220258
richardOven <richard.oven@vanti.co.uk>
2023-07-31 12:49:47 +0100

    ops/ui: create a helper for chart annotation

---
+++ ui/conductor/src/components/charts/chartHelpers.js
@@ -0,0 +1,41 @@
+/**
+ * This function generates the background color for the chart based on the segments
+ *
+ * @see https://www.chartjs.org/chartjs-plugin-annotation/latest/samples/box/basic.html
+ * @param {number[]} segments
+ * @param {string} conditionValue
+ * @param {string} mainColor
+ * @param {string} secondaryColor
+ * @return {{
+ *   annotations: Record<string, {
+ *     backgroundColor: string,
+ *     drawTime: string,
+ *     type: string,
+ *     xMin: number,
+ *     xMax: number
+ *   }>
+ * }}
+ */
+export const generateAreaBackground = (segments, conditionValue, mainColor, secondaryColor) => {
+  const annotations = {};
+
+  segments.forEach((segment, index) => {
+    const annotationKey = 'box' + index; // Generate a unique key for each annotation
+    const annotation = {
+      // Generate background color based on the segment type - given by the conditionValue
+      backgroundColor: segment[2] === conditionValue ? mainColor : secondaryColor,
+      // Set the draw time to after the dataset is drawn
+      drawTime: 'beforeDatasetsDraw',
+      //
+      type: 'box', // https://www.chartjs.org/docs/latest/developers/annotations.html#types
+      xMin: segment[0] - 0.5, // -0.5 to make it start at the beginning of the bar
+      xMax: segment[1] + 0.5 // +0.5 to make it end at the end of the bar
+    };
+
+    annotations[annotationKey] = annotation;
+  });
+
+  return {
+    annotations: {...annotations}
+  };
+};
