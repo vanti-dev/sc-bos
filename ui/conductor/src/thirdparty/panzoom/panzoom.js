5106a38dc48f52a6ca573b4e9669e42468b6f05a
richardOven <richard.oven@vanti.co.uk>
2023-08-23 13:34:40 +0100

    ops/ui: fix panzoom functionality

--- ui/conductor/src/thirdparty/panzoom/panzoom.js
+++ ui/conductor/src/thirdparty/panzoom/panzoom.js
@@ -9,7 +9,7 @@
  * @param global
  * @param factory
  */
-export default (function(global, factory) {
+(function(global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
       typeof define === 'function' && define.amd ? define(factory) :
           (global = global || self, global.Panzoom = factory());
