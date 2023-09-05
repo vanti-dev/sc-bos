fd6fbc73dbf1ce49e2ecda2caa4d173894fa66d8
Matt Randell <Matt.Randell@vanti.co.uk>
2023-09-05 15:09:54 +0100

    steinel hpd3 driver - test trait implementations

---
+++ pkg/driver/steinel/hpd3/trait_airtemperature_test.go
@@ -0,0 +1,57 @@
+package hpd3
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-api/go/types"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestAirTemperatureServer_GetAirTemperature(t *testing.T) {
+	type testCase struct {
+		name   string
+		points PointData
+		expect *traits.AirTemperature
+	}
+
+	cases := []testCase{
+		{
+			name: "basic",
+			points: PointData{
+				Temperature: 25.5,
+				Humidity:    50.5,
+				DewPoint:    12.125,
+			},
+			expect: &traits.AirTemperature{
+				AmbientTemperature: &types.Temperature{ValueCelsius: 25.5},
+				AmbientHumidity:    ptr[float32](50.5),
+				DewPoint:           &types.Temperature{ValueCelsius: 12.125},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			client := &memoryClient{points: c.points}
+			server := &airTemperatureServer{
+				client: client,
+				logger: testLogger(),
+			}
+			ctx, cancel := testCtx(t)
+			defer cancel()
+
+			res, err := server.GetAirTemperature(ctx, &traits.GetAirTemperatureRequest{})
+			if err != nil {
+				t.Error(err)
+			}
+
+			diff := cmp.Diff(c.expect, res, protocmp.Transform(), cmpopts.EquateApprox(0.0001, 0))
+			if diff != "" {
+				t.Errorf("incorrect Air Temperature result (-want +got):\n%s\n", diff)
+			}
+		})
+	}
+}
