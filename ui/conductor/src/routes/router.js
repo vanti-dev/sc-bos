ded14611d4085cf01cdc943b60823458265bfb9d
richardOven <richard.oven@vanti.co.uk>
2023-09-14 13:05:25 +0100

    ops/ui: fixed sidebar resizing and empty sidebar on main page navigation

--- ui/conductor/src/routes/router.js
+++ ui/conductor/src/routes/router.js
@@ -8,6 +8,8 @@ import {route, routeTitle} from '@/util/router.js';
 import Vue, {nextTick} from 'vue';
 import VueRouter from 'vue-router';
 import {useAppConfigStore} from '@/stores/app-config';
+import {usePageStore} from '@/stores/page';
+import {storeToRefs} from 'pinia';
 
 Vue.use(VueRouter);
 
@@ -41,6 +43,17 @@ if (window) {
       next(appConfig.homePath);
     } else {
       next(appConfig.pathEnabled(to.path));
+    }
+
+    // Clear the sidebar when navigating to a different main path
+    const {showSidebar, sidebarTitle, sidebarData} = storeToRefs(usePageStore());
+    const mainPathFrom = from.path.split('/')[1];
+    const mainPathTo = to.path.split('/')[1];
+
+    if (mainPathFrom !== mainPathTo) {
+      showSidebar.value = false;
+      sidebarTitle.value = '';
+      sidebarData.value = {};
     }
   });
 }
