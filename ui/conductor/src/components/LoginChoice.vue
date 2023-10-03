e78ea5721248abe0546b701f9c84434ada1fb484
richardOven <richard.oven@vanti.co.uk>
2023-10-03 12:04:47 +0100

    ops/ui: remove `keycloak` option if config doesn't include it

--- ui/conductor/src/components/LoginChoice.vue
+++ ui/conductor/src/components/LoginChoice.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <v-card-text class="text-center mb-5">
-      You can sign in using Keycloak or sign in locally.
+    <v-card-text :class="[{'mb-5': appConfig?.config?.keycloak }, 'text-center']">
+      {{ keycloakMessage.top }}
     </v-card-text>
     <v-card-actions>
       <v-btn
+          v-if="appConfig?.config?.keycloak"
           @click="doLogin()"
           color="primary"
           block
@@ -24,16 +25,32 @@
       </v-btn>
     </v-card-actions>
     <v-card-text class="text-center mt-4">
-      Local accounts are used to setup Smart Core, prefer signing in with
-      Keycloak.
+      {{ keycloakMessage.bottom }}
     </v-card-text>
   </div>
 </template>
 
 <script setup>
+import {computed} from 'vue';
 import {useAccountStore} from '@/stores/account.js';
+import {useAppConfigStore} from '@/stores/app-config';
 
+const appConfig = useAppConfigStore();
 const store = useAccountStore();
 const doLogin = () => store.login(['profile', 'roles'])
     .catch(err => console.error(err));
+
+const keycloakMessage = computed(() => {
+  if (appConfig.config.keycloak) {
+    return {
+      top: 'You can sign in using Keycloak or sign in locally.',
+      bottom: 'Local accounts are used to setup Smart Core, prefer signing in with Keycloak.'
+    };
+  } else {
+    return {
+      top: 'You can sign in using a local account.',
+      bottom: 'Local accounts are used to setup Smart Core.'
+    };
+  }
+});
 </script>
