56645285866f2f5ba19ba4330d4e0d8ed89c5c78
richardOven <richard.oven@vanti.co.uk>
2023-10-08 03:44:24 +0100

    ops/ui: move `floor list` import and collection to composable

--- ui/conductor/src/composables/useDevices.js
+++ ui/conductor/src/composables/useDevices.js
@@ -1,43 +1,34 @@
 import {reactive, ref, computed, onMounted, onUnmounted, watch} from 'vue';
-import {closeResource, newResourceValue} from '@/api/resource';
-import {pullDevicesMetadata} from '@/api/ui/devices';
 
 import {useErrorStore} from '@/components/ui-error/error';
 import {useDevicesStore} from '@/routes/devices/store';
+
+import useFloors from '@/composables/useFloors';
 
 const NO_FLOOR = '< no floor >';
 
 /**
  *
  * @param {Object} props
+ * @return {{
+ * floorList: ComputedRef<Array>,
+ * filterFloor: Ref<string>,
+ * search: Ref<string>,
+ * query: ComputedRef<Object>,
+ * devicesData: ComputedRef<Array>
+ * }}
  */
 export default function(props) {
   const devicesStore = useDevicesStore();
   const errorStore = useErrorStore();
+  const {listOfFloors} = useFloors();
 
-  // Create reactive resources and data
-  const floorListResource = reactive(newResourceValue());
   const filterFloor = ref('All');
   const search = ref('');
 
-  // Fetch floor list on component mount
-  onMounted(() => {
-    const req = {includes: {fieldsList: ['metadata.location.floor']}, updatesOnly: false};
-    pullDevicesMetadata(req, floorListResource);
-  });
-
-  // Close resource on component unmount
-  onUnmounted(() => {
-    closeResource(floorListResource);
-  });
-
   // Computed property for the floor list
   const floorList = computed(() => {
-    const fieldCounts = floorListResource.value?.fieldCountsList || [];
-    const floorFieldCounts = fieldCounts.find(v => v.field === 'metadata.location.floor');
-    if (!floorFieldCounts || floorFieldCounts.countsMap.size <= 0) return [];
-
-    const floors = floorFieldCounts.countsMap.map(([k]) => (k === '' ? NO_FLOOR : k));
+    const floors = listOfFloors.value;
     floors.unshift('All');
     return floors;
   });
---
+++ ui/conductor/src/composables/useFloors.js
@@ -0,0 +1,41 @@
+import {reactive, computed, onMounted, onUnmounted} from 'vue';
+import {closeResource, newResourceValue} from '@/api/resource';
+import {pullDevicesMetadata} from '@/api/ui/devices';
+
+const NO_FLOOR = '< no floor >';
+
+/**
+ *
+ * @return {{
+ * listOfFloors: ComputedRef<Array>
+ * }}
+ */
+export default function() {
+  // Create reactive resources and data
+  const floorListResource = reactive(newResourceValue());
+
+  // Fetch floor list on component mount
+  onMounted(() => {
+    const req = {includes: {fieldsList: ['metadata.location.floor']}, updatesOnly: false};
+    pullDevicesMetadata(req, floorListResource);
+  });
+
+  // Close resource on component unmount
+  onUnmounted(() => {
+    closeResource(floorListResource);
+  });
+
+  // Computed property for the floor list
+  const listOfFloors = computed(() => {
+    const fieldCounts = floorListResource.value?.fieldCountsList || [];
+    const floorFieldCounts = fieldCounts.find(v => v.field === 'metadata.location.floor');
+    if (!floorFieldCounts || floorFieldCounts.countsMap.size <= 0) return [];
+
+    const floors = floorFieldCounts.countsMap.map(([k]) => (k === '' ? NO_FLOOR : k));
+    return floors;
+  });
+
+  return {
+    listOfFloors
+  };
+}
