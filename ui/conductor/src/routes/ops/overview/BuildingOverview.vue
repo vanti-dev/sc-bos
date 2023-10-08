0288997938369053854a0a98400bef01485bf65e
richardOven <richard.oven@vanti.co.uk>
2023-10-08 05:58:07 +0100

    ops/ui: finalise `Building Overview` Ops navigation and it's sub-list + routes

--- ui/conductor/src/routes/ops/overview/BuildingOverview.vue
+++ ui/conductor/src/routes/ops/overview/BuildingOverview.vue
@@ -2,7 +2,7 @@
   <v-container fluid class="d-flex flex-row flex-nowrap pt-0 pr-0">
     <v-col class="ml-0 pl-0" :style="graphWidth">
       <v-row class="ml-0 pl-0">
-        <h3 class="text-h3 pt-2 pb-6">Status: Building Overview</h3>
+        <h3 class="text-h3 pt-2 pb-6">Building Status Overview</h3>
       </v-row>
       <v-row class="d-flex flex-row">
         <v-col cols="12">
