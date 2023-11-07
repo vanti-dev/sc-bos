f09d6cd3e8a8008c2c36c3f2fa23dbd78eadc5dc
Tom Haskell <tom.haskell@vanti.co.uk>
2023-11-07 09:57:29 +0000

    feat: add airQualitySensors support for zones

---
+++ pkg/zone/feature/airquality/airquality.go
@@ -0,0 +1,58 @@
+package airquality
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-golang/pkg/trait"
+	"github.com/smart-core-os/sc-golang/pkg/trait/airqualitysensor"
+	"github.com/vanti-dev/sc-bos/pkg/node"
+	"github.com/vanti-dev/sc-bos/pkg/task/service"
+	"github.com/vanti-dev/sc-bos/pkg/zone"
+	"github.com/vanti-dev/sc-bos/pkg/zone/feature/airquality/config"
+)
+
+var Feature = zone.FactoryFunc(func(services zone.Services) service.Lifecycle {
+	services.Logger = services.Logger.Named("airquality")
+	f := &feature{
+		announce: services.Node,
+		devices:  services.Devices,
+		clients:  services.Node,
+		logger:   services.Logger,
+	}
+	f.Service = service.New(service.MonoApply(f.applyConfig))
+	return f
+})
+
+type feature struct {
+	*service.Service[config.Root]
+	announce node.Announcer
+	devices  *zone.Devices
+	clients  node.Clienter
+	logger   *zap.Logger
+}
+
+func (f *feature) applyConfig(ctx context.Context, cfg config.Root) error {
+	announce := node.AnnounceContext(ctx, f.announce)
+	logger := f.logger
+
+	if len(cfg.AirQualitySensors) > 0 {
+		var client traits.AirQualitySensorApiClient
+		if err := f.clients.Client(&client); err != nil {
+			return err
+		}
+
+		group := &Group{
+			client: client,
+			names:  cfg.AirQualitySensors,
+			logger: logger,
+		}
+
+		f.devices.Add(cfg.AirQualitySensors...)
+		announce.Announce(cfg.Name, node.HasTrait(trait.AirQualitySensor, node.WithClients(airqualitysensor.WrapApi(group))))
+	}
+
+	return nil
+}
