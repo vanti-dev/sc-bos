847ac5170e813c03c57abdcc6aa8625e30160edd
richardOven <richard.oven@vanti.co.uk>
2023-11-09 13:03:31 +0000

    ops/ui: add `enroll / forget` hubNode api functionality

--- ui/conductor/src/api/sc/traits/hub.js
+++ ui/conductor/src/api/sc/traits/hub.js
@@ -1,7 +1,14 @@
 import {clientOptions} from '@/api/grpcweb';
 import {pullResource, setCollection, trackAction} from '@/api/resource';
 import {HubApiPromiseClient} from '@sc-bos/ui-gen/proto/hub_grpc_web_pb';
-import {ListHubNodesRequest, PullHubNodesRequest} from '@sc-bos/ui-gen/proto/hub_pb';
+import {
+  EnrollHubNodeRequest,
+  ForgetHubNodeRequest,
+  HubNode,
+  ListHubNodesRequest,
+  PullHubNodesRequest,
+  TestHubNodeRequest
+} from '@sc-bos/ui-gen/proto/hub_pb';
 
 /**
  *
@@ -33,10 +40,93 @@ export function pullHubNodes(resource) {
   });
 }
 
+
+/**
+ *
+ * @param {string} address
+ * @param {ActionTracker<EnrollHubNodeResponse.AsObject>} [tracker]
+ * @return {Promise<EnrollHubNodeResponse.AsObject>}
+ */
+export function enrollHubNode(address, tracker) {
+  return trackAction('Hub.enrollHubNode', tracker ?? {}, endpoint => {
+    const api = apiClient(endpoint);
+    const hubNode = setHubNodeFromObject(enrollHubNodeFromObject(address));
+    return api.enrollHubNode(hubNode);
+  });
+}
+
+/**
+ *
+ * @param {string} address
+ * @param {ActionTracker<ForgetHubNodeResponse.AsObject>} [tracker]
+ * @return {Promise<ForgetHubNodeResponse.AsObject>}
+ */
+export function forgetHubNode(address, tracker) {
+  return trackAction('Hub.forgetHubNode', tracker ?? {}, endpoint => {
+    const api = apiClient(endpoint);
+    const request = new ForgetHubNodeRequest();
+    request.setAddress(address);
+    request.setAllowMissing(true);
+    return api.forgetHubNode(request);
+  });
+}
+
+/**
+ * @param {string} address
+ * @param {ActionTracker<TestHubNodeResponse.AsObject>} [tracker]
+ * @return {Promise<TestHubNodeResponse.AsObject>}
+ */
+export function testHubNodes(address, tracker) {
+  return trackAction('Hub.testHubNode', tracker ?? {}, endpoint => {
+    const api = apiClient(endpoint);
+    return api.testHubNode(testHubNodesFromObject({address}));
+  });
+}
+
 /**
  * @param {string} endpoint
  * @return {HubApiPromiseClient}
  */
 function apiClient(endpoint) {
   return new HubApiPromiseClient(endpoint, null, clientOptions());
+}
+
+/**
+ *
+ * @param {TestHubNodeRequest.AsObject} obj
+ * @return {TestHubNodeRequest|undefined}
+ */
+function testHubNodesFromObject(obj) {
+  if (!obj) return undefined;
+
+  const dst = new TestHubNodeRequest();
+  dst.setAddress(obj.address);
+  return dst;
+}
+
+/**
+ * @param {HubNode.AsObject} obj
+ * @return {EnrollHubNodeRequest|undefined}
+ */
+function setHubNodeFromObject(obj) {
+  if (!obj) return undefined;
+
+  const dst = new EnrollHubNodeRequest();
+  dst.setNode(obj);
+
+  return dst;
+}
+
+
+/**
+ * @param {HubNode.AsObject} obj
+ * @return {HubNode|undefined}
+ */
+function enrollHubNodeFromObject(obj) {
+  if (!obj) return undefined;
+
+  const dst = new HubNode();
+  dst.setAddress(obj);
+
+  return dst;
 }
