9144b07191a8e710859d38c2bd922cb114a9b1ae
richardOven <richard.oven@vanti.co.uk>
2023-11-17 16:38:16 +0000

    ops/ui: define `theme` object with sub settings in `config` for customisable `branding`

---
+++ ui/conductor/src/components/BrandLogo.vue
@@ -0,0 +1,27 @@
+<template>
+  <sc-logo v-if="isDefault"/>
+
+  <img
+      v-else
+      :alt="props.theme?.appBranding?.brandLogo?.altText"
+      :src="props.theme?.appBranding?.brandLogo?.src">
+</template>
+
+<script setup>
+import {computed} from 'vue';
+
+import ScLogo from '@/components/ScLogo.vue';
+
+const props = defineProps({
+  theme: {
+    type: Object,
+    default: () => ({})
+  }
+});
+
+// Check if the default key and it's value exist and is true
+//
+const isDefault = computed(() => {
+  return !props.theme?.appBranding?.brandLogo?.src;
+});
+</script>
