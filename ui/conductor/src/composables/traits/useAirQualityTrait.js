ef3ad6c623736af90dae8f7b2ab9bbe0a2febfdc
richardOven <richard.oven@vanti.co.uk>
2023-11-20 13:20:52 +0000

    ops/ui: replace `comfort` number value with string in CSV

--- ui/conductor/src/composables/traits/useAirQualityTrait.js
+++ ui/conductor/src/composables/traits/useAirQualityTrait.js
@@ -316,6 +316,7 @@ export default function(props) {
       return {
         deviceName,
         ...record.airQuality,
+        comfort: readComfortValue(record.airQuality.comfort),
         recordTime: `${recordDate.toLocaleDateString()} ${recordDate.toLocaleTimeString()}`
       };
     });
