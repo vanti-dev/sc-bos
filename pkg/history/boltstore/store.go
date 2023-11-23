a673fcf2a5f80d0c0fa8b39dc838c8ad7465d66e
Tom Haskell <tom.haskell@vanti.co.uk>
2023-11-23 16:25:34 +0000

    fix[DEV-465]: nil checks won't work anymore

--- pkg/history/boltstore/store.go
+++ pkg/history/boltstore/store.go
@@ -141,7 +141,7 @@ func (s slice) Slice(from, to history.Record) history.Slice {
 }
 
 func (s slice) getQuery() *bolthold.Query {
-	query := &bolthold.Query{}
+	var query *bolthold.Query
 	if !s.from.IsZero() {
 		if s.from.ID != "" {
 			query = bolthold.Where("ID").Ge(s.from.ID)
@@ -163,6 +163,9 @@ func (s slice) getQuery() *bolthold.Query {
 				query = query.And("CreateTime").Lt(s.to.CreateTime)
 			}
 		}
+	}
+	if query == nil {
+		query = &bolthold.Query{}
 	}
 	return query.SortBy("CreateTime")
 }
