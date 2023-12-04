6d974b3db2dbe84113c1cf8d5af6c0572506a593
richardOven <richard.oven@vanti.co.uk>
2023-12-04 13:41:27 +0000

    ops/ui: display dynamically the `server/gateway/hub/node chips` depending on response and update `status text`

--- ui/conductor/src/components/smartCoreStatus/smartCoreStatusCard.vue
+++ ui/conductor/src/components/smartCoreStatus/smartCoreStatusCard.vue
@@ -3,41 +3,58 @@
     <template #activator="{ on, attrs }">
       <v-btn class="py-1" style="text-align: center" text v-bind="attrs" v-on="on" @click="showStatus = !showStatus">
         <span class="text-title mr-1">Smart Core OS:</span>
-        <span :class="`text-title-bold text-uppercase ${statusColor}`">{{ statusText }}</span>
+        <span :class="`text-title-bold text-uppercase ${generalStatus}--text`">{{ statusText }}</span>
       </v-btn>
     </template>
 
     <content-card class="popup__status">
       <v-card class="elevation-0" style="border: 2px solid var(--v-neutral-lighten2)" width="500px">
-        <v-card-title class="text-subtitle-1 mb-0 pb-0 mt-n1 mb-2">Connection status</v-card-title>
+        <div class="d-flex flex-row align-center">
+          <v-card-title class="text-subtitle-1 mb-0 pb-0 mt-n1 mb-2">Connection status</v-card-title>
+          <v-spacer/>
+          <v-tooltip left>
+            <template #activator="{ on, attrs }">
+              <v-btn
+                  v-bind="attrs"
+                  v-on="on"
+                  :class="['mr-4', {'rotate-icon': isRefreshing}]"
+                  icon
+                  x-small
+                  @click="triggerRefresh">
+                <v-icon>mdi-reload</v-icon>
+              </v-btn>
+            </template>
+            <span>Refresh</span>
+          </v-tooltip>
+        </div>
         <v-row class="d-flex flex-row justify-center mx-4 my-3">
           <!-- Test Hub to Nodes connection -->
           <div class="d-flex flex-row">
             <v-chip class="neutral lighten-1" small>UI</v-chip>
             <v-divider class="my-auto mx-2" style="min-width: 10px"/>
 
-            <!-- Test UI to Server connection -->
-            <status-alert
-                :color="uiToServerStatus.color"
-                :icon="uiToServerStatus.icon"
-                :resource="uiToServerStatus.resource"/>
-
-            <v-divider class="my-auto mx-2" style="min-width: 10px"/>
-            <v-chip class="neutral lighten-4" small>Server</v-chip>
-            <v-divider class="my-auto mx-2" style="min-width: 10px"/>
-
-            <!-- Test Server To Hub connection -->
-            <status-alert
-                :color="serverToHubStatus.color"
-                :icon="serverToHubStatus.icon"
-                :resource="serverToHubStatus.resource"/>
-
-            <v-divider class="my-auto mx-2" style="min-width: 10px"/>
-            <v-chip class="primary" small>Hub</v-chip>
-            <v-divider class="my-auto mx-2" style="min-width: 10px"/>
-
-            <!-- Test Hub to Nodes connection -->
-            <v-chip class="neutral lighten-2" small>Nodes</v-chip>
+            <!-- Display chips and status alerts -->
+            <div v-for="(chip, index) in statusPopupSetup" class="d-flex flex-row align-center" :key="chip.id">
+              <status-alert
+                  v-if="chip.id.includes('Status')"
+                  :click-action="chip.clickAction"
+                  :clickable="chip.isClickable"
+                  :color="chip.color"
+                  :icon="chip.icon"
+                  :loading="chip.loading"
+                  :resource="chip.resource"
+                  :type="chip.type"/>
+              <div v-if="!chip.id.includes('Status')" class="d-flex flex-row">
+                <v-divider class="my-auto mx-2" style="min-width: 10px"/>
+                <v-chip :class="chip.color" small>
+                  {{ chip.label }}
+                </v-chip>
+                <v-divider
+                    v-if="index !== statusPopupSetup.length - 1"
+                    class="my-auto mx-2"
+                    style="min-width: 10px"/>
+              </div>
+            </div>
           </div>
         </v-row>
       </v-card>
@@ -46,96 +63,339 @@
 </template>
 
 <script setup>
-import StatusAlert from '@/components/StatusAlert.vue';
 import {computed, ref} from 'vue';
-import {formatErrorMessage} from '@/util/error';
-import {statusCodeToString} from '@/components/ui-error/util';
-import useSystemComponents from '@/composables/useSystemComponents';
+import useSmartCoreStatus from '@/composables/useSmartCoreStatus';
 
+import StatusAlert from '@/components/StatusAlert.vue';
 import ContentCard from '@/components/ContentCard.vue';
 
 const showStatus = ref(false);
-const {nodesListCollection, hubNode, hubNodeValue} = useSystemComponents();
+const isRefreshing = ref(false);
 
-const uiToServerStatus = computed(() => {
-  const error = nodesListCollection?.streamError;
-  const response = nodesListCollection?.value;
+const {
+  listHubNodesValue,
+  enrollmentStatus,
+  triggerListHubNodesAction,
+  getEnrollmentAndListHubNodes,
+  nodeStatus,
+  updatingNodeStatus,
+  displayedChips
+} = useSmartCoreStatus();
 
-  if (error && !response || error && response) {
-    return {
-      color: 'warning',
-      icon: 'mdi-alert',
-      resource: error
-    };
-  } else if (!error && response) {
-    return {
-      color: 'success',
-      icon: 'mdi-check',
-      resource: {
-        status: {
-          code: '200',
-          message: 'Connected',
-          name: 'UI'
-        }
-      }
-    };
+const triggerRefresh = async () => {
+  isRefreshing.value = true;
+  await getEnrollmentAndListHubNodes();
+  setTimeout(() => {
+    isRefreshing.value = false;
+  }, 1000);
+};
+
+const statusText = computed(() => {
+  if (generalStatus.value === 'error') {
+    return 'offline';
+  } else if (generalStatus.value === 'warning') {
+    return 'online';
+  } else if (generalStatus.value === 'success') {
+    return 'online';
+  } else {
+    return 'offline';
   }
-
-  return {
-    color: 'warning',
-    icon: 'mdi-alert',
-    resource: 'Unavailable'
-  };
 });
 
-const serverToHubStatus = computed(() => {
-  const error = nodesListCollection?.streamError;
-  const response = nodesListCollection?.value;
+// Returns the actual UI status from the enrollment status
+const uiStatus = computed(() => {
+  const es = enrollmentStatus.value;
 
-  // If streamError is not null and value is also not null, then we have an error
-  if (error && Object.keys(response).length > 0) {
+  if (!es.enrollment && !es.error && !es.loading) {
+    // Unavailable state
     return {
       color: 'error',
       icon: 'mdi-close',
-      resource: error
+      loading: false,
+      resource: {error: {code: 14, message: 'Service is unavailable'}},
+      type: 'error'
     };
-  } else if (!error && response) {
+  }
+
+  if (es.loading || es.testLoading) {
+    // Loading state
+    return {
+      color: 'warning',
+      icon: 'mdi-loading',
+      loading: es.loading || es.testLoading,
+      resource: {error: {code: 2, message: 'Loading'}},
+      type: 'warning'
+    };
+  }
+
+  if (es.error) {
+    // Error state
+    return {
+      color: 'error',
+      icon: 'mdi-close',
+      loading: false,
+      resource: {error: es.error.error},
+      type: 'error'
+    };
+  }
+
+  if (!es.isTested && !es.testLoading) {
+    // Enrolled but not tested state
+    return {
+      color: 'warning',
+      icon: 'mdi-alert',
+      loading: false,
+      resource: {error: {code: 0, message: 'Enrollment successful, but not yet tested'}},
+      type: 'warning'
+    };
+  }
+
+  if (!es.isTested && es.testLoading) {
+    // Testing state
+    return {
+      color: 'warning',
+      icon: 'mdi-',
+      loading: true,
+      resource: {error: {code: 2, message: 'Testing in progress'}},
+      type: 'warning'
+    };
+  }
+
+  if (es.isTested) {
+    // Check if there's an error in the test
+    if (es.isTested.error && es.isTested.error !== '') {
+      // Enrolled and test failed state
+      return {
+        color: 'error',
+        icon: 'mdi-alert',
+        loading: false,
+        resource: {error: es.isTested},
+        type: 'error'
+      };
+    }
+  }
+
+  // Enrolled with pass state
+  return {
+    color: 'success',
+    icon: 'mdi-check',
+    loading: false,
+    resource: {status: {code: 0, message: 'Enrolled'}},
+    type: 'success'
+  };
+});
+// Returns the general status of the UI - Online, Offline, or Error
+const generalStatus = computed(() => {
+  const ui = uiStatus.value;
+
+  // Check if UI is in error
+  const isUIError = ui.color === 'error';
+
+  // Initialize flags
+  let hasError = false;
+  let allError = true;
+
+  // Check the status of each node
+  for (const status of Object.values(nodeStatus.value)) {
+    if (status.color === 'error') {
+      hasError = true;
+    } else {
+      allError = false;
+    }
+  }
+
+  // Determine the general status
+  if (isUIError && allError) {
+    // Return 'error' only if both UI and all nodes are in error state
+    return 'error';
+  } else if (isUIError || hasError) {
+    // Return 'warning' if UI is in error or some nodes are in error state
+    return 'warning';
+  } else {
+    // Return 'success' if no errors are found
+    return 'success';
+  }
+});
+
+// Returns server/gateway to hub status
+const serverToHubStatue = computed(() => {
+  const isTested = enrollmentStatus.value.isTested;
+
+  if (!isTested) {
+    return {
+      color: 'warning',
+      icon: 'mdi-alert',
+      loading: false,
+      resource: {error: {code: 2, message: 'Not tested'}},
+      type: 'warning'
+    };
+  }
+
+  if (isTested.error) {
+    return {
+      color: 'error',
+      icon: 'mdi-alert',
+      loading: false,
+      resource: {error: isTested.error},
+      type: 'error'
+    };
+  }
+
+  return {
+    color: 'success',
+    icon: 'mdi-check',
+    loading: false,
+    resource: {status: {code: 0, message: 'Tested'}},
+    type: 'success'
+  };
+});
+
+// Returns the overall status of the Node Status alert
+const nodeOverallStatus = computed(() => {
+  const nodes = nodeStatus.value;
+  const isAvailable = listHubNodesValue.response?.nodesList?.length > 0 && nodeStatus.value !== {};
+  const isUpdating = updatingNodeStatus.value;
+
+  // Check if nodes are not available
+  if (!isAvailable) {
+    return {
+      color: 'error',
+      icon: 'mdi-information-outline',
+      loading: false,
+      resource: {error: {code: 14, message: 'No nodes available'}},
+      type: 'error'
+    };
+  }
+
+  // Check if the node status is being updated
+  if (isUpdating) {
+    return {
+      color: 'warning',
+      icon: 'mdi-loading',
+      loading: true,
+      resource: {error: {code: 2, message: 'Updating node status'}},
+      type: 'warning'
+    };
+  }
+
+  let hasError = false;
+  let allError = true;
+  let allSuccess = true;
+
+  for (const status of Object.values(nodes)) {
+    if (status.color === 'error') {
+      hasError = true;
+    } else {
+      allError = false;
+    }
+    if (status.color !== 'success') {
+      allSuccess = false;
+    }
+  }
+
+  if (allError) {
+    return {
+      color: 'error',
+      icon: 'mdi-close',
+      loading: false,
+      resource: {error: {code: 14, message: 'All nodes are in error state'}},
+      type: 'error'
+    };
+  } else if (hasError) {
+    return {
+      color: 'warning',
+      icon: 'mdi-alert',
+      loading: false,
+      resource: {error: {code: 2, message: 'Some nodes have errors'}},
+      type: 'warning'
+    };
+  } else if (allSuccess) {
     return {
       color: 'success',
       icon: 'mdi-check',
-      resource: {
-        status: {
-          code: '200',
-          message: 'Connected',
-          name: hubNode.name
-        }
-      }
+      loading: false,
+      resource: {status: {code: 0, message: 'All nodes are operating normally'}},
+      type: 'success'
     };
   }
 
   return {
     color: 'warning',
-    icon: 'mdi-alert',
-    resource: 'Unavailable'
+    icon: 'mdi-information-outline',
+    loading: false,
+    resource: {error: {code: 2, message: 'Node status is mixed or unknown'}},
+    type: 'warning'
   };
 });
 
-const statusText = computed(() => {
-  if (uiToServerStatus.value.status) {
-    return 'Online';
-  } else {
-    return 'Offline';
+const statusPopupSetup = computed(() => {
+  const uiStatusObj = {
+    id: 'uiStatus',
+    ...uiStatus.value
+  };
+
+  const serverHubStatusObj = {
+    isClickable: false,
+    id: 'serverHubStatus',
+    ...serverToHubStatue.value
+  };
+
+  const nodeStatusObj = {
+    clickAction: async () => await triggerListHubNodesAction(),
+    isClickable: true,
+    id: 'nodeStatus',
+    ...nodeOverallStatus.value
+  };
+
+  const server = {
+    color: 'neutral lighten-4',
+    id: 'server',
+    label: 'Server'
+  };
+
+  const gateway = {
+    color: 'accent',
+    id: 'gateway',
+    label: 'Gateway'
+  };
+  const hub = {
+    color: 'primary',
+    id: 'hub',
+    label: 'Hub'
+  };
+
+  const nodes = {
+    color: 'neutral lighten-2',
+    id: 'nodes',
+    label: 'Nodes'
+  };
+
+
+  const chips = [uiStatusObj];
+  const isEnrolledTested = enrollmentStatus.value.enrollment && enrollmentStatus.value.isTested;
+
+  if (displayedChips.value.includes('server')) {
+    chips.push(server);
+    if (isEnrolledTested) chips.push(serverHubStatusObj);
   }
+
+  if (displayedChips.value.includes('gateway')) {
+    chips.push(gateway);
+    if (isEnrolledTested) chips.push(serverHubStatusObj);
+  }
+
+  if (displayedChips.value.includes('hub')) {
+    chips.push(hub);
+  }
+
+  if (displayedChips.value.includes('nodes')) {
+    chips.push(nodeStatusObj);
+    chips.push(nodes);
+  }
+
+  return chips;
 });
 
-// Computed property for calculating the status color
-const statusColor = computed(() => {
-  if (uiToServerStatus.value.status && serverToHubStatus.value.status) {
-    return 'success--text text--lighten-4';
-  } else {
-    return 'error--text';
-  }
-});
 </script>
 
 <style lang="scss" scoped>
@@ -163,6 +423,19 @@ const statusColor = computed(() => {
       border-color: transparent transparent var(--v-neutral-lighten2) transparent;
       transform: rotate(135deg);
     }
+  }
+}
+
+.rotate-icon {
+  animation: rotation 1s infinite linear;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
   }
 }
 </style>
