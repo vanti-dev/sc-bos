0a4b8be8f7fa94b9eb9f55273faf61e3448f089c
Matt Nathan <matt.nathan@vanti.co.uk>
2023-12-05 16:05:13 +0000

    util/chans: add RecvContext

--- pkg/util/chans/context.go
+++ pkg/util/chans/context.go
@@ -13,3 +13,14 @@ func SendContext[T any](ctx context.Context, c chan<- T, v T) error {
 		return nil
 	}
 }
+
+// RecvContext receives from c unless ctx is Done.
+func RecvContext[T any](ctx context.Context, c <-chan T) (T, error) {
+	select {
+	case <-ctx.Done():
+		var t T
+		return t, ctx.Err()
+	case v := <-c:
+		return v, nil
+	}
+}
