eccb2910d05d68ebac17b063f3799ea117628285
Matt Nathan <matt.nathan@vanti.co.uk>
2023-12-06 15:50:15 +0000

    driver/hpd3: enable driver

--- pkg/driver/alldrivers/factory.go
+++ pkg/driver/alldrivers/factory.go
@@ -5,6 +5,7 @@ import (
 	"github.com/vanti-dev/sc-bos/pkg/driver/bacnet"
 	"github.com/vanti-dev/sc-bos/pkg/driver/mock"
 	"github.com/vanti-dev/sc-bos/pkg/driver/proxy"
+	"github.com/vanti-dev/sc-bos/pkg/driver/steinel/hpd3"
 	"github.com/vanti-dev/sc-bos/pkg/driver/xovis"
 )
 
@@ -12,6 +13,7 @@ import (
 func Factories() map[string]driver.Factory {
 	return map[string]driver.Factory{
 		bacnet.DriverName: bacnet.Factory,
+		hpd3.DriverName:   hpd3.Factory,
 		mock.DriverName:   mock.Factory,
 		proxy.DriverName:  proxy.Factory,
 		xovis.DriverName:  xovis.Factory,
