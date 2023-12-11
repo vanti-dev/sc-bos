6f0eb52e8b2032ec7eb98c3d58d6809ea4222fb3
richardOven <richard.oven@vanti.co.uk>
2023-12-11 11:07:45 +0000

    ops/ui: update `error message formatter`

--- ui/conductor/src/util/error.js
+++ ui/conductor/src/util/error.js
@@ -4,6 +4,13 @@
  * @return {string}
  */
 export function formatErrorMessage(message) {
+  // Ensure that message is a string
+  if (typeof message !== 'string') {
+    // Handle non-string message appropriately
+    // For example, return a default error message or convert message to a string if possible
+    return 'Invalid error message format.';
+  }
+
   // Finding the start of the description and extracting it
   const descStart = message.indexOf('desc = ');
   const description = message.substring(descStart).split('"')[1];
@@ -16,4 +23,5 @@ export function formatErrorMessage(message) {
   // Formatting the message into a more structured and readable sentence
   return `${specificError}.`;
 }
+
 
