4a610a2a75d62cb7f4019bc6ecf9fbb6d47618c7
Matt Nathan <matt.nathan@vanti.co.uk>
2024-01-08 11:59:16 +0000

    proto: delete test.proto and related generated and util files

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -1,6 +1,6 @@
 package gen
 
-//go:generate protoc -I ../../proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. test.proto enrollment.proto lighting_test.proto
+//go:generate protoc -I ../../proto --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. enrollment.proto lighting_test.proto
 
 // PREREQUISITE: protomod is on PATH, i.e. `go install github.com/smart-core-os/protomod`
 // PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router@latest`
