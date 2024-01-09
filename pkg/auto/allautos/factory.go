02695fdd46452444c033c678ba082fda46bd3be5
DeanR <dean.redfern@vanti.co.uk>
2024-01-09 16:42:56 +0000

    add meteremail to allautos factory

--- pkg/auto/allautos/factory.go
+++ pkg/auto/allautos/factory.go
@@ -6,6 +6,7 @@ import (
 	"github.com/vanti-dev/sc-bos/pkg/auto/export"
 	"github.com/vanti-dev/sc-bos/pkg/auto/history"
 	"github.com/vanti-dev/sc-bos/pkg/auto/lights"
+	"github.com/vanti-dev/sc-bos/pkg/auto/meteremail"
 	"github.com/vanti-dev/sc-bos/pkg/auto/occupancyemail"
 	"github.com/vanti-dev/sc-bos/pkg/auto/resetenterleave"
 	"github.com/vanti-dev/sc-bos/pkg/auto/statusalerts"
@@ -20,6 +21,7 @@ func Factories() map[string]auto.Factory {
 		"export-mqtt":            export.MQTTFactory,
 		"history":                history.Factory,
 		lights.AutoType:          lights.Factory,
+		meteremail.AutoName:      meteremail.Factory,
 		occupancyemail.AutoName:  occupancyemail.Factory,
 		resetenterleave.AutoName: resetenterleave.Factory,
 		statusalerts.AutoName:    statusalerts.Factory,
