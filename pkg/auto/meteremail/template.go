2870d7ab3b79409bf33a9d57f975c50dde4150f3
DeanR <dean.redfern@vanti.co.uk>
2024-01-10 14:36:32 +0000

    add summary report to email & cleanup csv report

--- pkg/auto/meteremail/template.go
+++ pkg/auto/meteremail/template.go
@@ -12,8 +12,11 @@ const MeterTypeElectric = 2
 type MeterType uint8
 
 type Attrs struct {
-	Now   time.Time
-	Stats []Stats
+	Now                  time.Time
+	Stats                []Stats
+	ReadingsByFloorZone  map[string]map[string][]Stats // map floor -> zone -> reading
+	EnergySummaryReports []SummaryReport
+	WaterSummaryReports  []SummaryReport
 }
 
 type Stats struct {
@@ -25,4 +28,10 @@ type MeterReading struct {
 	MeterType MeterType
 	Date      time.Time
 	Reading   float32
+}
+
+type SummaryReport struct {
+	Floor        string
+	Zone         string
+	TotalReading float32
 }
