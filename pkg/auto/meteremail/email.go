f1af20ce923ac1f932fff6973c2a5e0fcb73a5f0
DeanR <dean.redfern@vanti.co.uk>
2024-01-10 11:02:46 +0000

    added basic attachment support

--- pkg/auto/meteremail/email.go
+++ pkg/auto/meteremail/email.go
@@ -1,98 +1,73 @@
 package meteremail
 
 import (
-	"crypto/tls"
+	"bytes"
+	"encoding/base64"
 	"fmt"
 	"github.com/vanti-dev/sc-bos/pkg/auto/meteremail/config"
-	"golang.org/x/exp/maps"
-	"mime"
-	"mime/quotedprintable"
+	"mime/multipart"
+	"net/http"
 	"net/smtp"
-	"net/textproto"
-	"sort"
 	"strings"
 )
 
 func sendEmail(dst config.Destination, attrs Attrs) error {
+	buf := bytes.NewBuffer(nil)
+	withAttachments := len(dst.Attachments) > 0
 	p := dst.Parsed
-	c, err := smtp.Dial(p.Addr)
-	if err != nil {
-		return err
-	}
 
-	err = c.StartTLS(&tls.Config{
-		ServerName: dst.Host,
-	})
-	if err != nil {
-		return err
-	}
-
-	err = c.Auth(smtp.PlainAuth("", p.Username, p.Password, dst.Host))
-	if err != nil {
-		return err
-	}
-
-	err = c.Mail(p.From.Address)
-	if err != nil {
-		return err
-	}
-
-	for _, to := range p.To {
-		err = c.Rcpt(to.Address)
-		if err != nil {
-			return err
-		}
-	}
-
-	wc, err := c.Data()
-	if err != nil {
-		return err
-	}
-
-	// write headers
-	headers := make(textproto.MIMEHeader)
-	headers.Add("From", p.From.String())
-	for _, addr := range p.To {
-		headers.Add("To", addr.String())
-	}
+	// Subject
 	var subj strings.Builder
 	if err := p.SubjectTemplate.Execute(&subj, attrs); err != nil {
 		return err
 	}
-	headers.Add("Subject", subj.String())
-	headers.Add("MIME-Version", `1.0`)
-	headers.Add("Content-Type", mime.FormatMediaType("text/html", map[string]string{"charset": "utf-8"}))
-	headers.Add("Content-Transfer-Encoding", `quoted-printable`)
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", subj.String()))
 
-	keys := maps.Keys(headers)
-	sort.Strings(keys)
-	for _, key := range keys {
-		for _, value := range headers.Values(key) {
-			_, err = fmt.Fprintf(wc, "%s: %s\r\n", key, value)
-			if err != nil {
-				return err
-			}
+	// From
+	buf.WriteString(fmt.Sprintf("From: %s\n", p.From.String()))
+
+	// To
+	var addrs []string
+	for _, a := range p.To {
+		addrs = append(addrs, a.Address)
+		buf.WriteString(fmt.Sprintf("To: %s\n", a.String()))
+	}
+
+	buf.WriteString("MIME-Version: 1.0\n")
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/alternative; boundary=%s\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+	} else {
+		buf.WriteString("Content-Type: text/html; charset=utf-8\n")
+		buf.WriteString("Content-Transfer-Encoding: quoted-printable")
+	}
+
+	if err := p.BodyTemplate.Execute(buf, attrs); err != nil {
+		return err
+	}
+
+	if withAttachments {
+		for k, v := range dst.Attachments {
+			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
 		}
-	}
-	if _, err := fmt.Fprint(wc, "\r\n"); err != nil {
-		return err
-	}
 
-	// write body
-	bodyWriter := quotedprintable.NewWriter(wc)
-	if err := p.BodyTemplate.Execute(bodyWriter, attrs); err != nil {
-		return err
+		buf.WriteString("--")
 	}
-	if err := bodyWriter.Close(); err != nil {
-		return err
-	}
+	auth := smtp.PlainAuth("", p.Username, p.Password, dst.Host)
+	addr := dst.Addr()
+	err := smtp.SendMail(addr, auth, p.Username, addrs, buf.Bytes())
 
-	// close email
-	if err := wc.Close(); err != nil {
-		return err
-	}
-
-	if err := c.Quit(); err != nil {
+	if err != nil {
 		return err
 	}
 
