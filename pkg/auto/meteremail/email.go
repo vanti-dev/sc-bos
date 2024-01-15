51240fa6364b0115f04e4828513d09a5448a8658
DeanR <dean.redfern@vanti.co.uk>
2024-01-15 10:52:04 +0000

    refactor to use mime/multipart instead of manual

--- pkg/auto/meteremail/email.go
+++ pkg/auto/meteremail/email.go
@@ -5,14 +5,16 @@ import (
 	"encoding/base64"
 	"fmt"
 	"github.com/vanti-dev/sc-bos/pkg/auto/meteremail/config"
+	"mime/multipart"
 	"net/http"
 	"net/smtp"
+	"net/textproto"
 	"strings"
 )
 
 func sendEmail(dst config.Destination, attrs Attrs) error {
 	buf := bytes.NewBuffer(nil)
-	withAttachments := len(dst.Attachments) > 0
+	withAttachments := len(dst.Attachment) > 0
 	p := dst.Parsed
 
 	// Subject
@@ -20,62 +22,78 @@ func sendEmail(dst config.Destination, attrs Attrs) error {
 	if err := p.SubjectTemplate.Execute(&subj, attrs); err != nil {
 		return err
 	}
-	buf.WriteString(fmt.Sprintf("Subject: %s\n", subj.String()))
+	_, _ = fmt.Fprintf(buf, "Subject: %s\n", subj.String())
 
 	// From
-	buf.WriteString(fmt.Sprintf("From: %s\n", p.From.String()))
+	_, _ = fmt.Fprintf(buf, "From: %s\n", p.From.String())
 
 	// To
 	var addrs []string
 	for _, a := range p.To {
 		addrs = append(addrs, a.Address)
-		buf.WriteString(fmt.Sprintf("To: %s\n", a.String()))
+		_, _ = fmt.Fprintf(buf, "To: %s\n", a.String())
 	}
 
-	buf.WriteString("MIME-Version: 1.0\n")
+	_, _ = fmt.Fprintf(buf, "MIME-Version: 1.0\n")
 
-	// add body part
-	mainMessageBoundary := "MixedBoundaryString"
+	// Main message
+	mainMessageWriter := multipart.NewWriter(buf)
+	mainMessageBoundary := mainMessageWriter.Boundary()
 	buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"%s\"\n", mainMessageBoundary))
-	buf.WriteString("\n--" + mainMessageBoundary + "\n")
 
-	attachmentBoundaryString := "AttachmentBoundaryString"
-	buf.WriteString(fmt.Sprintf("Content-Type: multipart/related; boundary=\"%s\"\n", attachmentBoundaryString))
-	buf.WriteString("\n--" + attachmentBoundaryString + "\n")
-
-	htmlMessageBoundary := "HtmlBoundaryString"
-	buf.WriteString(fmt.Sprintf("Content-Type: multipart/alternative; boundary=\"%s\"\n", htmlMessageBoundary))
-	buf.WriteString("\n--" + htmlMessageBoundary + "\n")
-
-	buf.WriteString("Content-Type: text/html; charset=utf-8\n")
-	buf.WriteString("Content-Transfer-Encoding: quoted-printable\n")
-	if err := p.BodyTemplate.Execute(buf, attrs); err != nil {
+	// Create a part for the attachment
+	attachmentWriter := multipart.NewWriter(buf)
+	attachmentBoundaryString := attachmentWriter.Boundary()
+	attachmentHeader := make(textproto.MIMEHeader)
+	attachmentHeader.Set("Content-Type", fmt.Sprintf("multipart/related; boundary=\"%s\"\n", attachmentBoundaryString))
+	if _, err := mainMessageWriter.CreatePart(attachmentHeader); err != nil {
 		return err
 	}
-	buf.WriteString("--" + htmlMessageBoundary + "--")
 
-	if withAttachments {
-		boundary := attachmentBoundaryString
-
-		for k, v := range dst.Attachments {
-			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
-			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
-			buf.WriteString("Content-Transfer-Encoding: base64\n")
-			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
-
-			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
-			base64.StdEncoding.Encode(b, v)
-			buf.Write(b)
-		}
-
-		buf.WriteString(fmt.Sprintf("\n\n--%s--\n", boundary))
+	// Create a part for the HTML email
+	htmlWriter := multipart.NewWriter(buf)
+	htmlMessageBoundary := htmlWriter.Boundary()
+	htmlHeader := make(textproto.MIMEHeader)
+	htmlHeader.Set("Content-Type", fmt.Sprintf("multipart/alternative; boundary=\"%s\"\n", htmlMessageBoundary))
+	if _, err := attachmentWriter.CreatePart(htmlHeader); err != nil {
+		return err
 	}
 
-	buf.WriteString("\n--" + mainMessageBoundary + "--")
+	header := make(textproto.MIMEHeader)
+	header.Set("Content-Type", "text/html; charset=utf-8")
+	header.Set("Content-Transfer-Encoding", "quoted-printable")
+	partWriter, _ := htmlWriter.CreatePart(header)
+	if err := p.BodyTemplate.Execute(partWriter, attrs); err != nil {
+		return err
+	}
+	_ = htmlWriter.Close()
+
+	if withAttachments {
+		v := dst.Attachment
+		k := dst.AttachmentName
+		header := make(textproto.MIMEHeader)
+		header.Set("Content-Type", http.DetectContentType(v))
+		header.Set("Content-Transfer-Encoding", "base64")
+		header.Set("Content-Disposition", "attachment; filename="+k)
+		partWriter, err := attachmentWriter.CreatePart(header)
+		if err != nil {
+			return err
+		}
+		b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+		base64.StdEncoding.Encode(b, v)
+		_, _ = partWriter.Write(b)
+		_, _ = partWriter.Write([]byte("\n"))
+		_ = attachmentWriter.Close()
+
+	}
+
+	_ = mainMessageWriter.Close()
 
 	auth := smtp.PlainAuth("", p.Username, p.Password, dst.Host)
 	addr := dst.Addr()
 	err := smtp.SendMail(addr, auth, p.Username, addrs, buf.Bytes())
+
+	print(buf.String())
 
 	if err != nil {
 		return err
