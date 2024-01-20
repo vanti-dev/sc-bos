e1f9f7671a7695f67246eadca1516a4e567e086c
richardOven <richard.oven@vanti.co.uk>
2024-01-20 19:26:36 +0000

    ops/ui: refactor `localAuth` setup into a composable

--- ui/conductor/src/api/localLogin.js
+++ ui/conductor/src/api/localLogin.js
@@ -1,6 +1,18 @@
 const api = import.meta.env.VITE_AUTH_URL || '';
 
-const localLogin = (username, password) => {
+/**
+ * @typedef {Object} LocalAuthResponse
+ * @property {string} access_token
+ */
+
+/**
+ * Performs local login.
+ *
+ * @param {string} username
+ * @param {string} password
+ * @return {Promise<Response>} A promise that resolves to the fetch response.
+ */
+export const localLogin = (username, password) => {
   const formData = new FormData();
   formData.set('grant_type', 'password');
   formData.set('username', username);
@@ -10,5 +22,3 @@ const localLogin = (username, password) => {
     body: formData
   });
 };
-
-export default localLogin;
