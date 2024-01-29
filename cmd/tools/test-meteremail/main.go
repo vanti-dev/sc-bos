77022e9e74ffb0c7ea759b85d5f1be220515e207
DeanR <dean.redfern@vanti.co.uk>
2024-01-29 09:22:38 +0000

    auto/meteremail: add meteremail automation for sending instananeous meter readings at a given interval
    Adds an automation which sends an email at a configurable interval. The email consists of an attachment of readings for all meters given in the config, and summarises the results in the email body.

---
+++ cmd/tools/test-meteremail/main.go
@@ -0,0 +1,86 @@
+// Command test-meteremail tests the [meteremail] package, sending to a real email address.
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/vanti-dev/sc-bos/pkg/auto"
+	"github.com/vanti-dev/sc-bos/pkg/auto/meteremail"
+	"github.com/vanti-dev/sc-bos/pkg/node"
+	"github.com/vanti-dev/sc-bos/pkg/node/alltraits"
+)
+
+var sampleNow = time.Date(2024, 01, 19, 0, 0, 0, 0, time.Local)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	root := node.New("test")
+	alltraits.AddSupport(root)
+
+	now, _ := time.Parse(time.DateTime, "2023-11-15 11:36:00")
+	now = now.Round(time.Second) // get rid of millis, etc
+
+	now = sampleNow
+
+	serv := auto.Services{
+		Logger: logger,
+		Node:   root,
+		Now: func() time.Time {
+			return now.Add(-2 * time.Second)
+		},
+	}
+	lifecycle := meteremail.Factory.New(serv)
+	defer lifecycle.Stop()
+	cfg := `{
+	"name": "emails", "type": "meteremail",
+	"destination": {
+	"host": "smtp.gmail.com",
+	"from": "OCW Paradise Build <vantiocwdev@gmail.com>",
+	"to": ["Dean Redfern <dean.redfern@vanti.co.uk>"],
+	"passwordFile" : ".localpassword",
+	"sendTime": "0 0 * * MON-FRI"
+	},
+	"serverAddr" : "172.16.100.10:23557",
+	"electricMeters" : [
+					"uk-ocw/floors/01/devices/CE1-electric-meter/WestDBA/T1HVACTotalEnergy",
+					"uk-ocw/floors/01/devices/CE1-electric-meter/WestDBA/T1LightingTotalEnergy",
+					"uk-ocw/floors/01/devices/CE1-electric-meter/WestDBA/T1TotalLoadTotalEnergy",
+					"uk-ocw/floors/01/devices/CE2-electric-meter/SouthSideDBB/T1LPHVACTotalEnergy",
+					"uk-ocw/floors/01/devices/CE2-electric-meter/SouthSideDBB/T1LPLightingTotalEnergy",
+					"uk-ocw/floors/01/devices/CE2-electric-meter/SouthSideDBB/T1LPTotalLoadTotalEnergy",
+					"uk-ocw/floors/06/devices/CE11-electric-meter/WestDBA/T6HVACTotalEnergy",
+					"uk-ocw/floors/06/devices/CE11-electric-meter/WestDBA/T6LightingTotalEnergy",
+					"uk-ocw/floors/06/devices/CE11-electric-meter/WestDBA/T6TotalLoadTotalEnergy",
+					"uk-ocw/floors/06/devices/CE12-electric-meter/EASTSideDBB/T6HVACTotalEnergy",
+					"uk-ocw/floors/06/devices/CE12-electric-meter/EASTSideDBB/T6LightingTotalEnergy",
+					"uk-ocw/floors/06/devices/CE12-electric-meter/EASTSideDBB/T6TotalLoadTotalEnergy"
+					],
+	"waterMeters" : [ 
+					"uk-ocw/floors/01/devices/CE1-water-meter/FirstFloorWestBCWSMeter",
+					"uk-ocw/floors/01/devices/CE2-water-meter/1stFloorEastBCWSMeter",
+					"uk-ocw/floors/06/devices/CE11-water-meter/SixthFloorWestBCWSMeter",
+					"uk-ocw/floors/06/devices/CE12-water-meter/6thFloorEastBCWSMeter"],
+	"insecureSkipVerify" : true
+}`
+
+	_, err = lifecycle.Configure([]byte(cfg))
+	if err != nil {
+		panic(err)
+	}
+	_, err = lifecycle.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	<-ctx.Done()
+}
---
+++ pkg/auto/meteremail/template.go
@@ -0,0 +1,49 @@
+package meteremail
+
+import (
+	"sort"
+	"time"
+
+	"github.com/vanti-dev/sc-bos/pkg/auto/meteremail/config"
+)
+
+const MeterTypeWater = 1
+const MeterTypeElectric = 2
+
+type MeterType uint8
+
+type Attrs struct {
+	Now                  time.Time
+	Stats                []Stats
+	ReadingsByFloorZone  map[string]map[string][]Stats // map floor -> zone -> reading
+	EnergySummaryReports []SummaryReport
+	WaterSummaryReports  []SummaryReport
+}
+
+// grab the floors and sort them so we can iterate over consistently
+func (a *Attrs) getFloorKeys() []string {
+
+	floorKeys := make([]string, 0, len(a.ReadingsByFloorZone))
+	for k := range a.ReadingsByFloorZone {
+		floorKeys = append(floorKeys, k)
+	}
+	sort.Strings(floorKeys)
+	return floorKeys
+}
+
+type Stats struct {
+	Source       config.Source
+	MeterReading MeterReading
+}
+
+type MeterReading struct {
+	MeterType MeterType
+	Date      time.Time
+	Reading   float32
+}
+
+type SummaryReport struct {
+	Floor        string
+	Zone         string
+	TotalReading float32
+}
