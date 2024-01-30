aab278065c7e8b9f2d58929255ba080d12679477
Matt Nathan <matt.nathan@vanti.co.uk>
2024-01-30 15:16:33 +0000

    auth: add support for device authentication to keycloak and the login poc
    The realm export for keycloak has been updated to enable OAuth 2.0 Device Authorization Grant. The POC ui has also been updated to allow this flow to be used.

--- ui/keycloak-login-prototype/src/App.vue
+++ ui/keycloak-login-prototype/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
+import PreJson from '@/component/PreJson.vue';
 import {ref} from 'vue'
-import {allScopes, keycloak} from './auth'
+import {decodeToken, keycloak, useOpenIDConnect} from './auth'
 import GetTest from './component/GetTest.vue'
 import UpdateTest from './component/UpdateTest.vue';
 
 const selectedScopes = ref(['profile'])
 const accessToken = ref(/** @type {null|string} */ null)
+const refreshToken = ref(/** @type {null|string} */ null)
 const keycloakClaims = ref(/** @type {null|KeycloakTokenParsed} */ null)
 
 keycloak.init({})
@@ -13,6 +15,7 @@ keycloak.init({})
       if (authenticated) {
         keycloakClaims.value = keycloak.idTokenParsed
         accessToken.value = keycloak.token
+        refreshToken.value = keycloak.refreshToken
       }
     })
     .catch(console.error)
@@ -29,6 +32,43 @@ async function loginKeycloak() {
   }
 }
 
+const oidc = useOpenIDConnect();
+const beginDeviceResponse = ref(null);
+
+async function beginDeviceAuth() {
+  beginDeviceResponse.value = await oidc.beginDeviceAuth(selectedScopes.value);
+}
+
+const checkDeviceResponse = ref(null);
+const checkDeviceError = ref(null);
+
+async function checkDeviceAuth() {
+  const lastResponse = checkDeviceResponse.value ?? beginDeviceResponse.value;
+  if (!lastResponse) return;
+  try {
+    checkDeviceResponse.value = await oidc.checkDeviceToken(lastResponse);
+    checkDeviceError.value = null;
+
+    accessToken.value = checkDeviceResponse.value.access_token;
+    refreshToken.value = checkDeviceResponse.value.refresh_token;
+    keycloakClaims.value = decodeToken(accessToken.value);
+
+  } catch (e) {
+    checkDeviceError.value = e;
+    checkDeviceResponse.value = null;
+  }
+}
+
+const refreshTokenResponse = ref(null);
+
+async function doRefreshToken() {
+  const response = await oidc.doRefreshToken(refreshToken.value);
+  refreshTokenResponse.value = response;
+  accessToken.value = response.access_token;
+  refreshToken.value = response.refresh_token;
+  keycloakClaims.value = decodeToken(accessToken.value);
+}
+
 async function logoutKeycloak() {
   return keycloak.logout();
 }
@@ -40,20 +80,40 @@ async function logoutKeycloak() {
     <h2>Keycloak</h2>
     <div>
       <h3>Scopes to Request</h3>
-      <div v-for="scope in allScopes">
-        <input type="checkbox" :id="scope" :value="scope" v-model="selectedScopes">
-        <label :for="scope">{{ scope }}</label>
+      <div v-for="s in oidc.allScopes.value">
+        <label :key="s"><input type="checkbox" :value="s" v-model="selectedScopes"> {{ s }}</label>
       </div>
     </div>
-    <button @click="loginKeycloak">Login</button>
+    <button @click="loginKeycloak">Begin Login Flow</button>
+    or
+    <button @click="beginDeviceAuth">Begin Device Flow</button>
     <div v-if="keycloakClaims !== null">
       Logged in as {{ keycloakClaims.name }} ({{ keycloakClaims.email }})
+    </div>
+  </div>
+
+  <div v-if="beginDeviceResponse !== null">
+    <h2>Begin Device Response</h2>
+    <pre-json :value="beginDeviceResponse" class="wrap"/>
+
+    <button @click="checkDeviceAuth">Check Device Token</button>
+    <div v-if="checkDeviceResponse !== null">
+      <h2>Check Device Response</h2>
+      <pre-json :value="checkDeviceResponse" class="wrap"/>
+    </div>
+    <div v-if="checkDeviceError !== null">
+      <h2>Check Device Error</h2>
+      <pre class="wrap">{{ checkDeviceError }}</pre>
     </div>
   </div>
 
   <div v-if="accessToken !== null">
     <h2>Access Token</h2>
     <pre class="wrap">{{ accessToken }}</pre>
+  </div>
+  <div v-if="refreshToken !== null">
+    <h2>Refresh Token</h2>
+    <pre class="wrap">{{ refreshToken }}</pre>
   </div>
 
   <div v-if="keycloakClaims !== null">
@@ -63,13 +123,20 @@ async function logoutKeycloak() {
 
   <div v-if="accessToken !== null">
     <button @click="logoutKeycloak">Log out</button>
+    <button v-if="refreshToken !== null" @click="doRefreshToken">Refresh Token</button>
+  </div>
+  <div v-if="refreshTokenResponse !== null">
+    <h2>Refresh Token Response</h2>
+    <pre-json :value="refreshTokenResponse" class="wrap"/>
+  </div>
+  <div v-if="accessToken !== null">
     <GetTest :token="accessToken"/>
     <UpdateTest :token="accessToken"/>
   </div>
 </template>
 
 <style>
-pre.wrap {
+.wrap {
   white-space: pre-wrap;
   word-break: break-all;
 }
---
+++ ui/keycloak-login-prototype/src/component/PreJson.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {computed} from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Object,
+    required: true
+  }
+})
+
+const jsonHtml = computed(() => {
+  let jsonStr = JSON.stringify(props.value, null, 2)
+  jsonStr = jsonStr.replaceAll(/("https?:\/\/[^"]+")/g, '<a href=$1>$1</a>');
+  return jsonStr
+})
+</script>
+
+<template>
+<pre v-html="jsonHtml"/>
+</template>
