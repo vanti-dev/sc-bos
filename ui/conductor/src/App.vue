ca889d7b3977f70efbe6dc1670e9b1877682df5b
richardOven <richard.oven@vanti.co.uk>
2024-02-05 02:51:28 +0000

    opsui/theme: update theme colors from config
    With the use of `useVuetify` composable, we now can access the Vuetify instance.
    Using this, we now dynamically set the theme colors depending on a config given or a default value exist.
    
    Jira: N/A

--- ui/conductor/src/App.vue
+++ ui/conductor/src/App.vue
@@ -13,10 +13,40 @@ import AppBar from '@/components/default/AppBar.vue';
 import NavigationDrawer from '@/components/default/NavigationDrawer.vue';
 import ErrorView from '@/components/ui-error/ErrorView.vue';
 
-import {onMounted} from 'vue';
+import {onMounted, onBeforeMount} from 'vue';
+import {storeToRefs} from 'pinia';
+import {useAppConfigStore} from '@/stores/app-config';
 import {useControllerStore} from '@/stores/controller';
+import useVuetify from '@/composables/useVuetify';
 
 const controller = useControllerStore();
+const appConfig = useAppConfigStore();
+const {appBranding} = storeToRefs(appConfig);
+const vuetifyInstance = useVuetify();
+
+onBeforeMount(async () => {
+  await appConfig.loadConfig();
+
+  // Access the vuetify instance from the Vue app
+  if (vuetifyInstance) {
+    const brandColors = appBranding.value?.brandColors;
+
+    if (brandColors) {
+      // Loop through each color key (e.g., 'primary', 'secondary', etc.)
+      Object.entries(brandColors).forEach(([colorKey, colorVariants]) => {
+        // Now loop through each variant of the color (e.g., 'base', 'lighten1', etc.)
+        Object.entries(colorVariants).forEach(([variantKey, variantValue]) => {
+          // Update the corresponding Vuetify theme color
+          // Check if the variantKey exists to safely update the value
+          if (vuetifyInstance.theme.themes.dark[colorKey] && variantValue) {
+            vuetifyInstance.theme.themes.dark[colorKey][variantKey] = variantValue;
+          }
+        });
+      });
+    }
+  }
+});
+
 onMounted(() => {
   controller.sync();
 });
