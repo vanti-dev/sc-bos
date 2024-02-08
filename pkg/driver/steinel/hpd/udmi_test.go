3db1b5e93f702e0dfba183a2bcc72c1eb8741fe3
DeanR <dean.redfern@vanti.co.uk>
2024-02-08 17:12:09 +0000

    make test runs parralel

--- pkg/driver/steinel/hpd/udmi_test.go
+++ pkg/driver/steinel/hpd/udmi_test.go
@@ -86,6 +86,7 @@ func Test_PullExportMessages(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
