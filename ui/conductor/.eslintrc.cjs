ae301bb8a1dfa4eb032bfc8cb9bd31121c2fc09f
Matt Nathan <matt.nathan@vanti.co.uk>
2024-02-15 16:10:53 +0000

    opsui: replace eslint babel use with standard es parser
    This allows node:20 to work

--- ui/conductor/.eslintrc.cjs
+++ ui/conductor/.eslintrc.cjs
@@ -1,4 +1,9 @@
 module.exports = {
+  parserOptions: {
+    parser: null, // override the parser pulled in from @vanti/eslint-config-vue (babel)
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
   'extends': [
     '@vanti/eslint-config-vue'
   ],
@@ -7,7 +12,13 @@ module.exports = {
     // allow custom v-models
     'vue/no-v-model-argument': 0,
     // allow errors and warnings to console
-    'no-console': ['warn', {allow: ['warn', 'error']}]
+    'no-console': ['warn', {allow: ['warn', 'error']}],
+    // tags should have no lines between them (the default), except @example which we allow any
+    // number of lines between to allow better separation of these tags
+    'jsdoc/tag-lines': ['warn', 'never', {
+      'startLines': 1, // this was added in a newer version of the plugin
+      'tags': {'example': {lines: 'any'}}
+    }]
   },
   'ignorePatterns': ['**/dist/*']
 };
