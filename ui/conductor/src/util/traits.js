5e358326e54b7544631b230efb3e527a53be5c6b
richardOven <richard.oven@vanti.co.uk>
2024-02-21 11:01:46 +0000

    opsui/traits: update traits with toQueryObject function and refactor watchResource
    Jira: N/A

--- ui/conductor/src/util/traits.js
+++ ui/conductor/src/util/traits.js
@@ -1,44 +1,69 @@
 import {closeResource} from '@/api/resource';
 import {toValue} from '@/util/vue';
-import {watch} from 'vue';
+import {computed, watch} from 'vue';
 import {deepEqual} from 'vuetify/src/util/helpers';
 
 /**
- * Calls apiCalls each time name changes, tracking and managing resource cleanup for you.
+ * Converts a ref or reactive object to a computed property that evaluates to a query object.
  *
- * @param {MaybeRefOrGetter<string>} name - string representing the name of the device
- * @param {MaybeRefOrGetter<boolean>} paused - boolean representing whether the data stream is paused
- * @param {(name: string) => ResourceValue<any, any>} apiCalls - array of functions that return a resource
+ * @template T
+ * @param {MaybeRefOrGetter<T>} input - input value to be converted into a query object.
+ * @return {
+ *  import('vue').ComputedRef<{name: string} | T | null>
+ * } - computed property that evaluates to a query object.
+ */
+export const toQueryObject = (input) => {
+  return computed(() => {
+    // If no input present, return null
+    if (!toValue(input)) return null;
+
+    // If input is a string, return an object with the name property
+    if (typeof toValue(input) === 'string') {
+      return {name: toValue(input)};
+      //
+      // If input is an object, return the object
+    } else {
+      return toValue(input);
+    }
+  });
+};
+
+/**
+ * Calls apiCalls each time the query changes, tracking and managing resource cleanup.
+ *
+ * @template T
+ * @param {MaybeRefOrGetter<T>} query - object representing the request to the API
+ * @param {MaybeRefOrGetter<boolean>} [paused] - boolean representing whether the data stream is paused
+ * @param {(req: T) => T<any, any>} apiCalls - array of functions that return a resource
  * @example
  * watchResource(
- *   () => props.name,
- *   () => props.paused,
- *   (name) => {
- *     pullAirTemperature({name}, resource);
+ *   () => toValue(toQueryObject(query)),
+ *   () => toValue(paused),
+ *   (req) => {
+ *     pullAirTemperature(req, resource);
  *     return resource;
  *   }
  * );
  */
-export const watchResource = (name, paused, ...apiCalls) => {
+export const watchResource = (query, paused = false, ...apiCalls) => {
   let resources = [];
 
   watch(
-      [() => toValue(name), () => toValue(paused)],
-      (newProps, oldProps) => {
+      [() => toValue(query), () => toValue(paused)],
+      (newSource, oldSource) => {
         // Check if the props have changed
-        const oldNewEqual = deepEqual(newProps, oldProps);
+        const oldNewEqual = deepEqual(newSource, oldSource);
 
         // If the props haven't changed, do nothing
         if (oldNewEqual) return;
         // Separate the array values into readable variables
-        const name = newProps[0];
-        const paused = newProps[1];
+        const name = newSource[0];
+        const paused = newSource[1];
 
         // If the props have changed (either the name or the paused state or both), close the old resources
         // and empty the array
         resources.forEach((resource) => closeResource(resource));
         resources = [];
-
         if (!paused) { // If not paused, pull new resource
           apiCalls.forEach((apiCall) => resources.push(apiCall(name)));
         }
