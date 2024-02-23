43d4e6faa27fdcbb9cd1fcc9a9aa92969a5c1f69
deanredfern26 <52207282+deanredfern26@users.noreply.github.com>
2024-02-23 08:23:46 +0000

    auto/udmi/config: add set password to mqtt broker opts (#212)

--- pkg/auto/udmi/config/mqtt.go
+++ pkg/auto/udmi/config/mqtt.go
@@ -1,6 +1,11 @@
 package config
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"os"
+	"strings"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 type MQTTBroker struct {
 	Host         string `json:"host,omitempty"`
@@ -12,6 +17,23 @@ type MQTTBroker struct {
 func (b MQTTBroker) ClientOptions() (*mqtt.ClientOptions, error) {
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(b.Host)
+	password := b.Password
+	if password == "" {
+		if b.PasswordFile != "" {
+			var passFileBody []byte
+			passFileBody, err := os.ReadFile(b.PasswordFile)
+			if err != nil {
+				return nil, err
+			} else {
+				password = strings.TrimSpace(string(passFileBody))
+			}
+		}
+	}
+
+	if password != "" { // allow connection without password if no password provided
+		opts.SetPassword(password)
+	}
+
 	opts.SetUsername(b.Username)
 	opts.SetOrderMatters(false)
 	return opts, nil
