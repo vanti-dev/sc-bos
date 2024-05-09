d8d906907bf8fa69149d6d302ba0887dab864025
Matt Nathan <matt.nathan@vanti.co.uk>
2024-05-09 17:14:49 +0100

    opsui: fix dragging the sidebar was janky
    This was caused by the logic using incorrect apis to perform the drag. It was attempting to use document events to drag things, instead it should have been using el.setPointerCapture. This API is designed for this type of thing and even works if you drag outside the browser window. No more issues and things feel much more responsive.

--- ui/conductor/src/components/page-layout/SidebarPage.vue
+++ ui/conductor/src/components/page-layout/SidebarPage.vue
@@ -11,11 +11,12 @@
         ref="sidebarDOMElement"
         app
         class="sidebarDOMElement pa-0"
+        :class="{resizing}"
         clipped
         color="neutral"
         floating
         right
-        :width="sideBar.width">
+        :width="sideBarWidth">
       <router-view name="sidebar"/>
     </v-navigation-drawer>
   </div>
@@ -25,98 +26,90 @@
 import {usePage} from '@/components/page';
 import {usePageStore} from '@/stores/page';
 import {storeToRefs} from 'pinia';
-import {ref, watchEffect} from 'vue';
+import {onUnmounted, ref, watchEffect} from 'vue';
+
+const props = defineProps({
+  minWidth: {
+    type: Number,
+    default: 275
+  },
+  maxWidth: {
+    type: Number,
+    default: 600
+  }
+});
 
 const {hasSidebar} = usePage();
 const sidebarDOMElement = ref(null);
-const drawerBorder = ref(null);
-const sideBar = ref({
-  width: 275,
-  borderSize: 4
-});
+const drawerBorder = ref(/** @type {HTMLDivElement} */ null);
+const sideBarWidth = ref(props.minWidth);
+
+const resizing = ref(false);
+const handleShift = ref(0);
+const pointerId = ref(0);
 
 const pageStore = usePageStore();
 const {showSidebar} = storeToRefs(pageStore);
 
-let originalCursor = '';
-
-const handleMouseDown = (event) => {
-  originalCursor = originalCursor || document.body.style.cursor; // Store original cursor value
-  sidebarDOMElement.value.$el.style.transition = 'none'; // Add transition
-  drawerBorder.value.style.backgroundColor = 'var(--v-primary-darken1)'; // Highlight border while moving
-  drawerBorder.value.style.width = '8px'; // Set border width
-  document.addEventListener('mousemove', handleMouseMove, false); // Add event listener
-
-  sidebarDOMElement.value.$el.style.userSelect = 'none'; // Disable text selection
+const beginDrag = (e) => {
+  resizing.value = true; // update styles
+  pointerId.value = e.pointerId; // for cleaning up later if needed
+  // receive mouse events even when mouse is outside the element/browser window
+  drawerBorder.value.setPointerCapture(e.pointerId);
+  // record where in the border the mouse was pressed to anchor it there to avoid jumps
+  handleShift.value = e.clientX - drawerBorder.value.getBoundingClientRect().left;
+  drawerBorder.value.addEventListener('pointermove', drag, false); // Add event listener
 };
 
-// Handle mouse move for sidebar resizing
-const handleMouseMove = (event) => {
-  document.body.style.cursor = 'ew-resize'; // Set cursor
-  drawerBorder.value.style.width = '8px'; // Set border width
-  let forced = document.body.scrollWidth - event.clientX; // Calculate width
-
-  forced = Math.min(forced, 600); // Set max width
-  forced = Math.max(forced, 275); // Set min width
-
-  sidebarDOMElement.value.$el.style.width = `${forced}px`; // Set width
+const drag = (event) => {
+  // the width of the sidebar is how far from the right edge of the viewport the mouse is
+  let clamped = document.body.scrollWidth - event.clientX + handleShift.value;
+  clamped = Math.min(clamped, props.maxWidth);
+  clamped = Math.max(clamped, props.minWidth);
+  sideBarWidth.value = clamped;
 };
 
-const handleMouseUp = () => {
-  if (sidebarDOMElement.value && sidebarDOMElement.value.$el) {
-    sidebarDOMElement.value.$el.style.transition = ''; // Remove transition
-    sideBar.value.width = sidebarDOMElement.value.$el.style.width; // Update sidebar width
-  }
-
-  if (drawerBorder.value) {
-    drawerBorder.value.style.backgroundColor = ''; // Remove border highlight on button release
-    drawerBorder.value.style.width = ''; // Reset border width
-  }
-
-  document.removeEventListener('mousemove', handleMouseMove, false); // Remove event listener
-
-  document.body.style.cursor = originalCursor; // Restore original cursor value
-
-  if (sidebarDOMElement.value && sidebarDOMElement.value.$el) {
-    sidebarDOMElement.value.$el.style.userSelect = ''; // Enable text selection
-  }
+const endDrag = (e) => {
+  resizing.value = false;
+  drawerBorder.value.releasePointerCapture(e.pointerId);
+  drawerBorder.value.removeEventListener('pointermove', drag, false);
 };
 
 // Set event listeners for sidebar resizing
-const setEvents = () => {
-  drawerBorder.value.addEventListener('mousedown', handleMouseDown);
-  document.addEventListener('mouseup', handleMouseUp);
-};
-
 const setUp = () => {
   if (hasSidebar.value && sidebarDOMElement.value.$el) {
-    sideBar.value.width = 275; // Set sidebar width to default
+    sideBarWidth.value = props.minWidth; // Set sidebar width to default
 
     drawerBorder.value = sidebarDOMElement.value.$el.querySelector(
         '.v-navigation-drawer__border'
     ); // Get border element
-    setEvents(); // Set event listeners
+
+    drawerBorder.value.addEventListener('pointerdown', beginDrag);
+    drawerBorder.value.addEventListener('pointerup', endDrag);
   }
 };
 
-
 const cleanUp = () => {
-  drawerBorder.value?.removeEventListener('mousedown', handleMouseDown, false); // Remove event listener
-  document.removeEventListener('mousemove', handleMouseMove, false); // Remove event listener
-  document.removeEventListener('mouseup', handleMouseUp, false); // Remove event listener
+  // clean up listeners and event state
+  drawerBorder.value?.removeEventListener('pointerdown', beginDrag, false);
+  drawerBorder.value?.removeEventListener('pointerup', endDrag, false);
+  drawerBorder.value?.removeEventListener('pointermove', drag, false);
+  drawerBorder.value?.releasePointerCapture(pointerId.value);
+
+  resizing.value = false; // Reset resizing styles
+  pointerId.value = 0; // Reset pointer ID
   showSidebar.value = false; // Close sidebar
-  sideBar.value.width = 275; // Reset sidebar width
-  drawerBorder.value = null; // Remove border element leftover
-  sidebarDOMElement.value = null; // Remove sidebar DOM element leftover
+  sideBarWidth.value = props.minWidth; // Reset sidebar width
 };
 
 // Watch for sidebar DOM element
 watchEffect(() => {
   if (sidebarDOMElement.value?.$el) {
     setUp(); // Set event listeners
-  } else {
-    cleanUp(); // Remove event listeners
   }
+});
+onUnmounted(() => {
+  cleanUp(); // Remove event listeners
 });
 </script>
 
@@ -125,11 +118,20 @@ watchEffect(() => {
   width: 4px;
   background-color: var(--v-primary-darken4);
   transition: all 0.2s ease-in-out;
+  cursor: ew-resize;
 
   &:hover {
     width: 8px;
-    cursor: ew-resize;
     background-color: var(--v-primary-darken1);
+  }
+}
+
+.sidebarDOMElement.resizing {
+  transition: none;
+
+  > .v-navigation-drawer__border {
+    background-color: var(--v-primary-darken1);
+    width: 8px;
   }
 }
 </style>
