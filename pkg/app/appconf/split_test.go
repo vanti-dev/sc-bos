bcb99eb9a2bcb8be3a1af5e4b68f2981f6ab8145
DeanR <dean.redfern@vanti.co.uk>
2024-07-02 15:40:20 +0100

    refactor tests to use go pattern

--- pkg/app/appconf/split_test.go
+++ pkg/app/appconf/split_test.go
@@ -3,8 +3,10 @@ package appconf
 import (
 	"encoding/json"
 	"io/fs"
+	"net/netip"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/spf13/afero"
@@ -45,12 +47,30 @@ func (m MockFs) mockIsDir(path string) (bool, error) {
 	return afero.IsDir(m.fs, path)
 }
 
+func unwrapValue(value interface{}) interface{} {
+
+	result := value
+	val := reflect.ValueOf(value)
+	if val.Kind() == reflect.Ptr {
+		result = reflect.ValueOf(value).Elem().Interface()
+
+		switch result.(type) {
+		case *netip.AddrPort:
+			result = result.(*netip.AddrPort).String()
+		}
+
+	} else if val.Kind() == reflect.Slice {
+
+	}
+	return result
+}
+
 // read the split file, so we know how to split the file into parts we want to edit
 // we can now create the db file structure based on what we have read in
 // tests we can create the directory structure for db
 // test modifying the db & joining the db with the ext (appconf.Config)
 // appconf.Config should then contain the edits in db & the original values
-func TestBasicMetadataSplit(t *testing.T) {
+func TestMetadataConfigPatch(t *testing.T) {
 
 	// first set up the mock filesystem, read & add the metadata & split files
 	// is more readable to do it this way
@@ -78,82 +98,94 @@ func TestBasicMetadataSplit(t *testing.T) {
 	assert := assert.New(t)
 	dbRootPath := filepath.Join("testdata", "db")
 
-	t.Run("TestBasicMetadataSplit", func(t *testing.T) {
+	appConfig, err := LoadLocalConfig("", mockFsConfigFileName)
 
-		splits, err := readSplits(mockFsSplitFileName)
+	if err != nil {
+		t.Errorf("failed to LoadLocalConfig: %s", err)
+	}
 
-		if err != nil {
-			t.Errorf("error reading split file: %s", err)
-		}
+	tests := []struct {
+		name       string
+		fs         MockFs
+		which      interface{}
+		preExpect  any
+		patchFile  string
+		patchValue any
+		post       func(interface{}, interface{}, ...interface{}) bool
+	}{
+		{
+			name:       "Floor",
+			fs:         mockFs,
+			which:      &appConfig.Metadata.Location.Floor,
+			preExpect:  "Floor 1",
+			patchFile:  filepath.Join("testdata", "db", "metadata", "Location", "Floor"),
+			patchValue: "New Floor",
+		},
+		{
+			name:       "Manufacturer",
+			fs:         mockFs,
+			which:      &appConfig.Metadata.Product.Manufacturer,
+			preExpect:  "Vanti",
+			patchFile:  filepath.Join("testdata", "db", "metadata", "Product", "Manufacturer"),
+			patchValue: "New Manufacturer",
+		},
+		{
+			name:       "Model",
+			fs:         mockFs,
+			which:      &appConfig.Metadata.Product.Model,
+			preExpect:  "Smart Core BOS",
+			patchFile:  filepath.Join("testdata", "db", "metadata", "Product", "Model"),
+			patchValue: "New Model",
+		},
+		{
+			name:       "Membership",
+			fs:         mockFs,
+			which:      appConfig.Metadata.Membership,
+			preExpect:  traits.Metadata_Membership{Subsystem: "smart"},
+			patchFile:  filepath.Join("testdata", "db", "metadata", "Membership"),
+			patchValue: traits.Metadata_Membership{Subsystem: "New Subsystem"},
+		},
+		{
+			name:       "Traits",
+			fs:         mockFs,
+			which:      appConfig.Metadata.Traits,
+			preExpect:  []*traits.TraitMetadata{{Name: "oldTrait"}},
+			patchFile:  filepath.Join("testdata", "db", "metadata", "Traits"),
+			patchValue: []*traits.TraitMetadata{{Name: "newTrait"}},
+		},
+		{
+			name:  "MoreMap",
+			fs:    mockFs,
+			which: &appConfig.Metadata.More,
+			preExpect: map[string]string{
+				"type":     "sensor",
+				"function": "temperature",
+			},
+			patchFile: filepath.Join("testdata", "db", "metadata", "More"),
+			patchValue: map[string]string{
+				"type":     "newType",
+				"function": "newFunction",
+			},
+		},
+	}
 
-		err = writeSplitStructure(dbRootPath, splits)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
-		if err != nil {
-			t.Errorf("error writing split file structure: %s", err)
-		}
+			assert.Equal(tt.preExpect, unwrapValue(tt.which))
 
-		expectedPaths := []string{
-			filepath.Join("testdata", "db", "metadata", "Location", "Floor"),
-			filepath.Join("testdata", "db", "metadata", "Product", "Manufacturer"),
-			filepath.Join("testdata", "db", "metadata", "Product", "Model"),
-			filepath.Join("testdata", "db", "metadata", "Membership"),
-			filepath.Join("testdata", "db", "metadata", "Traits"),
-			filepath.Join("testdata", "db", "metadata", "More"),
-		}
-
-		for _, p := range expectedPaths {
-			_, err := readFile(p)
+			err := writePageFile(tt.patchFile, "", tt.patchValue)
 			if err != nil {
-				t.Errorf("failed checking expected paths: %s %s", p, err)
+				t.Errorf("failed to write patch file: %s", err)
 			}
-		}
+			err = mergeDbWithExtConfig(appConfig, dbRootPath)
+			if err != nil {
+				t.Errorf("failed to join app config & db: %s", err)
+			}
 
-		appConfig, err := LoadLocalConfig("", mockFsConfigFileName)
-
-		if err != nil {
-			t.Errorf("failed to LoadLocalConfig: %s", err)
-		}
-
-		assert.Equal("Floor 1", appConfig.Metadata.Location.Floor)
-		assert.Equal("Vanti", appConfig.Metadata.Product.Manufacturer)
-		assert.Equal("Smart Core BOS", appConfig.Metadata.Product.Model)
-		assert.Equal("smart", appConfig.Metadata.Membership.Subsystem)
-		assert.Equal("sensor", appConfig.Metadata.More["type"])
-		assert.Equal("temperature", appConfig.Metadata.More["function"])
-		assert.Equal(appConfig.Metadata.Traits[0].Name, "oldTrait")
-		assert.Equal(1, len(appConfig.Metadata.Traits))
-		assert.Equal(2, len(appConfig.Metadata.More))
-
-		// now update the db files to simulate a user edit.
-		// i.e. we are creating the page files that would contain the user edits to overlay onto our app config
-		writePageFile(expectedPaths[0], "", "New Floor")
-		writePageFile(expectedPaths[1], "", "New Manufacturer")
-		writePageFile(expectedPaths[2], "", "New Model")
-		writePageFile(expectedPaths[3], "", traits.Metadata_Membership{Subsystem: "New Subsystem"})
-		// we want to replace the whole array here to test that we can do that
-		writePageFile(expectedPaths[4], "", []traits.TraitMetadata{{Name: "newTrait"}})
-		writePageFile(expectedPaths[5], "", map[string]string{"type": "newType", "function": "newFunction"})
-
-		err = mergeDbWithExtConfig(appConfig, dbRootPath)
-		if err != nil {
-			t.Errorf("failed to join app config & db: %s", err)
-		}
-
-		// at this point our appConfig should have been updated with the db values from above
-		assert.Equal("New Floor", appConfig.Metadata.Location.Floor)
-		assert.Equal("New Manufacturer", appConfig.Metadata.Product.Manufacturer)
-		assert.Equal("New Model", appConfig.Metadata.Product.Model)
-		assert.Equal("New Subsystem", appConfig.Metadata.Membership.Subsystem)
-		assert.Equal("newTrait", appConfig.Metadata.Traits[0].Name)
-		assert.Equal(1, len(appConfig.Metadata.Traits))
-		assert.Equal(2, len(appConfig.Metadata.More))
-		assert.Equal("newType", appConfig.Metadata.More["type"])
-		assert.Equal("newFunction", appConfig.Metadata.More["function"])
-		//_, err = mockFs.mockIsDir("")
-		//if err != nil {
-		//	return
-		//}
-	})
+			assert.Equal(tt.patchValue, unwrapValue(tt.which))
+		})
+	}
 }
 
 // tests the ability of the config system to update the property of a specific device in the config
@@ -161,7 +193,7 @@ func TestBasicMetadataSplit(t *testing.T) {
 // when the split-config encounters the key property being present in the split file, for a given split
 // it will search through the array / map of objects in the config for the object with the matching key
 // and then follow the same process
-func TestDeviceSpecificBmsPage(t *testing.T) {
+func TestBacnetDriverConfigPatch(t *testing.T) {
 
 	// first set up the mock filesystem, read & add the metadata & split files
 	// is more readable to do it this way
@@ -189,61 +221,79 @@ func TestDeviceSpecificBmsPage(t *testing.T) {
 
 	assert := assert.New(t)
 	dbRootPath := filepath.Join("testdata", "db")
+	appConfig, err := LoadLocalConfig("", mockFsConfigFileName)
+	if err != nil {
+		t.Errorf("failed to LoadLocalConfig: %s", err)
+	}
 
-	t.Run("TestDeviceSpecificBmsPage", func(t *testing.T) {
+	var bacnetConfig config.Root
+	err = json.Unmarshal(appConfig.Drivers[0].Raw, &bacnetConfig)
+	if err != nil {
+		t.Errorf("failed to unmarshall bacnet config: %s", err)
+	}
 
-		appConfig, err := LoadLocalConfig("", mockFsConfigFileName)
+	tests := []struct {
+		name       string
+		fs         MockFs
+		which      interface{}
+		preExpect  any
+		patchFile  string
+		patchValue any
+		post       func(interface{}, interface{}, ...interface{}) bool
+	}{
+		{
+			name:       "localInterface",
+			fs:         mockFs,
+			which:      &bacnetConfig.LocalInterface,
+			preExpect:  "eth0",
+			patchFile:  filepath.Join("testdata", "db", "drivers", "bacnet", "localInterface"),
+			patchValue: "New Interface",
+		},
+		{
+			name:       "localPort",
+			fs:         mockFs,
+			which:      &bacnetConfig.LocalPort,
+			preExpect:  uint16(47808),
+			patchFile:  filepath.Join("testdata", "db", "drivers", "bacnet", "localPort"),
+			patchValue: uint16(12345),
+		},
+		{
+			name:       "device1IP",
+			fs:         mockFs,
+			which:      &bacnetConfig.Devices[0].Comm.IP,
+			preExpect:  "172.16.8.115:47808",
+			patchFile:  filepath.Join("testdata", "db", "drivers", "bacnet", "devices", normaliseDeviceName("uk-ocw/floors/01/devices/CE1"), "comm", "ip"),
+			patchValue: "188.88.8.71:8888",
+		},
+		{
+			name:       "device2IP",
+			fs:         mockFs,
+			which:      &bacnetConfig.Devices[1].Comm.IP,
+			preExpect:  "172.16.8.117:47808",
+			patchFile:  filepath.Join("testdata", "db", "drivers", "bacnet", "devices", normaliseDeviceName("uk-ocw/floors/01/devices/CE2"), "comm", "ip"),
+			patchValue: "22.22.2.71:2222",
+		},
+	}
 
-		if err != nil {
-			t.Errorf("failed to LoadLocalConfig: %s", err)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 
-		assert.Equal(1, len(appConfig.Drivers))
-		assert.Equal("floor-01/bms", appConfig.Drivers[0].Name)
-		var bacnetConfig config.Root
-		err = json.Unmarshal(appConfig.Drivers[0].Raw, &bacnetConfig)
-		if err != nil {
-			t.Errorf("failed to unmarshall bacnet config: %s", err)
-		}
-		assert.Equal("eth0", bacnetConfig.LocalInterface)
-		assert.Equal(uint16(47808), bacnetConfig.LocalPort)
-		assert.Equal(2, len(bacnetConfig.Devices))
-		assert.Equal("uk-ocw/floors/01/devices/CE1", bacnetConfig.Devices[0].Name)
-		assert.Equal("172.16.8.115:47808", bacnetConfig.Devices[0].Comm.IP.String())
-		assert.Equal("uk-ocw/floors/01/devices/CE2", bacnetConfig.Devices[1].Name)
-		assert.Equal("172.16.8.117:47808", bacnetConfig.Devices[1].Comm.IP.String())
+			assert.Equal(tt.preExpect, unwrapValue(tt.which))
 
-		writePageFile(filepath.Join("testdata", "db", "drivers", "bacnet", "localInterface"), "", "New Interface") //local interface
-		writePageFile(filepath.Join("testdata", "db", "drivers", "bacnet", "localPort"), "", 12345)                //local port
-		normalName := normaliseDeviceName("uk-ocw/floors/01/devices/CE1")
-		devicesPath := filepath.Join("testdata", "db", "drivers", "bacnet", "devices")
-		devicesPath = filepath.Join(devicesPath, normalName, "comm", "ip")
-		writePageFile(devicesPath, "", "188.88.8.71:8888") // update IP address of "uk-ocw/floors/01/devices/CE1"
-		normalName = normaliseDeviceName("uk-ocw/floors/01/devices/CE2")
-		devicesPath = filepath.Join("testdata", "db", "drivers", "bacnet", "devices")
-		devicesPath = filepath.Join(devicesPath, normalName, "comm", "ip")
-		writePageFile(devicesPath, "", "22.22.2.71:2222")
+			err := writePageFile(tt.patchFile, "", tt.patchValue)
+			if err != nil {
+				t.Errorf("failed to write patch file: %s", err)
+			}
+			err = mergeDbWithExtConfig(appConfig, dbRootPath)
+			if err != nil {
+				t.Errorf("failed to join app config & db: %s", err)
+			}
+			err = json.Unmarshal(appConfig.Drivers[0].Raw, &bacnetConfig)
+			if err != nil {
+				t.Errorf("failed to unmarshall bacnet config: %s", err)
+			}
 
-		err = mergeDbWithExtConfig(appConfig, dbRootPath)
-		if err != nil {
-			t.Errorf("failed to join app config & db: %s", err)
-		}
-
-		var newBacnetConfig config.Root
-		err = json.Unmarshal(appConfig.Drivers[0].Raw, &newBacnetConfig)
-		if err != nil {
-			t.Errorf("failed to unmarshall bacnet config: %s", err)
-		}
-		assert.Equal("New Interface", newBacnetConfig.LocalInterface)
-		assert.Equal(uint16(12345), newBacnetConfig.LocalPort)
-		assert.Equal("uk-ocw/floors/01/devices/CE1", bacnetConfig.Devices[0].Name)
-		assert.Equal("188.88.8.71:8888", newBacnetConfig.Devices[0].Comm.IP.String())
-		assert.Equal("uk-ocw/floors/01/devices/CE2", bacnetConfig.Devices[1].Name)
-		assert.Equal("22.22.2.71:2222", newBacnetConfig.Devices[1].Comm.IP.String())
-
-		_, err = mockFs.mockIsDir("")
-		if err != nil {
-			return
-		}
-	})
+			assert.Equal(tt.patchValue, unwrapValue(tt.which))
+		})
+	}
 }
