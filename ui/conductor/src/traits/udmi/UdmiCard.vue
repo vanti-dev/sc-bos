39d2f055b00100c303f7f0bcb204d70aee5f99ab
Matt Nathan <matt.nathan@vanti.co.uk>
2024-08-29 16:39:03 +0100

    ui/ops[part]: fix layout/style of udmi card component

--- ui/conductor/src/traits/udmi/UdmiCard.vue
+++ ui/conductor/src/traits/udmi/UdmiCard.vue
@@ -1,20 +1,18 @@
 <template>
   <v-card elevation="0" tile>
-    <v-list tile class="ma-0 pa-0" lines="three">
+    <v-list tile class="ma-0 pa-0" lines="three" density="compact">
       <v-list-subheader class="text-title-caps-large text-neutral-lighten-3">UDMI Event</v-list-subheader>
       <v-list-item class="py-1 mb-2" v-if="message.updateTime">
         <v-list-item-title class="text-body-small text-capitalize">Last updated</v-list-item-title>
-        <template #append>
-          <v-list-item-subtitle class="text-capitalize text-body-1">
-            {{ Intl.DateTimeFormat('en-GB', {dateStyle: 'short', timeStyle: 'long'}).format(message.updateTime) }}
-          </v-list-item-subtitle>
-        </template>
+        <v-list-item-subtitle class="text-capitalize text-body-1">
+          {{ Intl.DateTimeFormat('en-GB', {dateStyle: 'short', timeStyle: 'long'}).format(message.updateTime) }}
+        </v-list-item-subtitle>
       </v-list-item>
       <v-list-item class="py-1 mb-2" v-if="message.value">
         <v-list-item-title class="text-body-small text-capitalize">Topic</v-list-item-title>
         <v-list-item-subtitle class="text-capitalize">{{ message.value?.topic }}</v-list-item-subtitle>
       </v-list-item>
-      <v-list-item class="py-1" v-for="(value, key) in messagePayload" :key="key">
+      <v-list-item class="py-1" v-for="(value, key) in messagePayload" :key="key" lines="one">
         <v-list-item-title class="text-body-small text-capitalize flex-fill">{{ key }}</v-list-item-title>
         <template #append>
           <v-list-item-subtitle class="text-capitalize text-end flex-fill text-body-1">
