0c541e0d6b4290feb0791c5e5fcd84aca80f2f77
Matt Nathan <matt.nathan@vanti.co.uk>
2024-09-04 10:27:28 +0100

    ui/ops: new cap utility function

--- ui/conductor/src/util/number.js
+++ ui/conductor/src/util/number.js
@@ -1,1 +1,13 @@
 export const MAX_INT32 = 2147483647; // 2^31 - 1
+
+/**
+ * Cap a number between min and max.
+ *
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ * @return {number}
+ */
+export function cap(value, min, max) {
+  return Math.max(min, Math.min(max, value));
+}
