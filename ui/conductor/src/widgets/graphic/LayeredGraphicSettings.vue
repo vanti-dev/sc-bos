71caada6d0af2bd12e84d384ffc40c9e62d72a0d
Matt Nathan <matt.nathan@vanti.co.uk>
2024-09-09 16:36:54 +0100

    ui/ops: allow toggling the visibility of graphical layers

---
+++ ui/conductor/src/widgets/graphic/LayeredGraphicSettings.vue
@@ -0,0 +1,33 @@
+<template>
+  <side-bar>
+    <v-card-subtitle class="mt-4">Layers</v-card-subtitle>
+    <v-list :items="layerList" v-model:selected="visibleLayers" selectable select-strategy="independent">
+      <template #append="{isSelected}">
+        <v-list-item-action>
+          <v-switch :model-value="isSelected" readonly hide-details density="compact"/>
+        </v-list-item-action>
+      </template>
+    </v-list>
+  </side-bar>
+</template>
+
+<script setup>
+import SideBar from '@/components/SideBar.vue';
+import {useSidebarStore} from '@/stores/sidebar.js';
+import {computed} from 'vue';
+
+const sidebar = useSidebarStore();
+const layerList = computed(() => sidebar.data.layerList);
+const visibleLayers = computed({
+  get: () => sidebar.data.visibleLayers,
+  set: (v) => {
+    if (sidebar.data.visibleLayers) {
+      sidebar.data.visibleLayers = v;
+    }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
