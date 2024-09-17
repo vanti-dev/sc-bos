7c7279a90759ea77cbc496b0cbf50cde95b69763
Loveen <loveen.dyall@vanti.co.uk>
2024-09-17 11:22:13 +0100

    [SC-527] feat: add comments

--- internal/util/jose/jose.go
+++ internal/util/jose/jose.go
@@ -2,6 +2,8 @@ package jose
 
 import "github.com/go-jose/go-jose/v4"
 
+// ConvertToNativeJose
+// Converts string Signature Algorithms names into go-jose/v4 SignatureAlgorithm types
 func ConvertToNativeJose(algs []string) []jose.SignatureAlgorithm {
 	var joseAlgs []jose.SignatureAlgorithm
 	for _, alg := range algs {
