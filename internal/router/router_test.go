9615a9125573db4849b61af0b122ef5c7f418376
Matt Randell <matt.randell@vanti.co.uk>
2024-09-25 10:51:04 +0100

    router: allow removing service support
    Add method DeleteService to remove support for a service from a router.
    
    Rename SupportService -> AddService for a
    consistent naming convention.
    
    Add Service method KeyRoutable.
    
    Change SupportsService -> GetService, returning *Service:
    allows an API consumer to use GetService in combination with KeyRoutable, to detect if an existing service is routable by key.

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/reflect/protoregistry"
+	"google.golang.org/protobuf/testing/protocmp"
 
 	"github.com/smart-core-os/sc-api/go/traits"
 	"github.com/smart-core-os/sc-golang/pkg/resource"
@@ -19,9 +21,9 @@ import (
 // tests overall type behavior: registering services, adding routes, and routing requests with correct priority.
 func TestRouter(t *testing.T) {
 	r := New()
-	r.SupportService(routedRegistryService(t, traits.OnOffApi_ServiceDesc.ServiceName, "name"))
-	r.SupportService(routedRegistryService(t, traits.OccupancySensorApi_ServiceDesc.ServiceName, "name"))
-	r.SupportService(routedRegistryService(t, traits.AirQualitySensorApi_ServiceDesc.ServiceName, "name"))
+	check(t, r.AddService(routedRegistryService(t, traits.OnOffApi_ServiceDesc.ServiceName, "name")))
+	check(t, r.AddService(routedRegistryService(t, traits.OccupancySensorApi_ServiceDesc.ServiceName, "name")))
+	check(t, r.AddService(routedRegistryService(t, traits.AirQualitySensorApi_ServiceDesc.ServiceName, "name")))
 
 	fooModel := onoff.NewModel(resource.WithInitialValue(&traits.OnOff{State: traits.OnOff_OFF}))
 	defaultModel := onoff.NewModel(resource.WithInitialValue(&traits.OnOff{State: traits.OnOff_ON}))
@@ -91,6 +93,61 @@ func TestRouter(t *testing.T) {
 
 }
 
+func TestRouter_AddService(t *testing.T) {
+	r := New()
+	model := onoff.NewModel(resource.WithInitialValue(&traits.OnOff{State: traits.OnOff_OFF}))
+	client := traits.NewOnOffApiClient(NewLoopback(r))
+
+	// add a device route
+	err := r.AddRoute("", "foo", wrap.ServerToClient(traits.OnOffApi_ServiceDesc, onoff.NewModelServer(model)))
+	if err != nil {
+		t.Fatalf("(1) failed to add route: %v", err)
+	}
+
+	// route shouldn't match because the service hasn't been added yet
+	_, err = client.GetOnOff(context.Background(), &traits.GetOnOffRequest{Name: "foo"})
+	if statusErr, _ := status.FromError(err); statusErr.Code() != codes.Unimplemented {
+		t.Errorf("(2) expected Unimplemented, got %v", statusErr)
+	}
+
+	// service should not be registered, then after we add it, it should be
+	srvName := traits.OnOffApi_ServiceDesc.ServiceName
+	srv := r.GetService(srvName)
+	if srv != nil {
+		t.Errorf("(3) service %q should not exist yet", srvName)
+	}
+	srv = routedRegistryService(t, srvName, "name")
+	if err = r.AddService(srv); err != nil {
+		t.Errorf("(4) cannot add service %s: %v", srvName, err)
+	}
+	srv = r.GetService(srvName)
+	if srv == nil {
+		t.Errorf("(5) service %q should exist", srvName)
+	}
+
+	// route should now match
+	res, err := client.GetOnOff(context.Background(), &traits.GetOnOffRequest{Name: "foo"})
+	if err != nil {
+		t.Errorf("(6) failed to get onoff for foo: %v", err)
+	}
+	expect := &traits.OnOff{State: traits.OnOff_OFF}
+	if diff := cmp.Diff(expect, res, protocmp.Transform()); diff != "" {
+		t.Errorf("(7) unexpected response (-want +got):\n%s", diff)
+	}
+
+	// delete the service and the route should stop matching
+	if !r.DeleteService(srvName) {
+		t.Errorf("(8) service %q should exist", srvName)
+	}
+	if r.GetService(srvName) != nil {
+		t.Errorf("(9) service %q should not exist", srvName)
+	}
+	_, err = client.GetOnOff(context.Background(), &traits.GetOnOffRequest{Name: "foo"})
+	if statusErr, _ := status.FromError(err); statusErr.Code() != codes.Unimplemented {
+		t.Errorf("(10) expected Unimplemented, got %v", statusErr)
+	}
+}
+
 func routedRegistryService(t *testing.T, serviceName, keyName string) *Service {
 	t.Helper()
 	desc, err := protoregistry.GlobalFiles.FindDescriptorByName(protoreflect.FullName(serviceName))
@@ -106,4 +163,11 @@ func routedRegistryService(t *testing.T, serviceName, keyName string) *Service {
 		t.Fatalf("failed to create routed service: %v", err)
 	}
 	return s
+}
+
+func check(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
 }
