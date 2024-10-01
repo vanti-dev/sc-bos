b8ff0da15aa45a64eb71ca4c47f44a745f688f8b
Matt Nathan <matt.nathan@vanti.co.uk>
2024-10-01 17:59:33 +0100

    ui/ops: introduce a consistent scale concept for interactive graphics
    Before the graphic would be laid out at 100% width, no matter the scale of the graphic.
    Now you can configure your graphic to have a pixel scale (20px/m or something) and the graphic to have a target pixel scale (50px/m).
    The widget will now scale these consistently to give a more natural view into floor plans.

--- ui/conductor/src/dynamic/widgets/graphic/LayeredGraphic.vue
+++ ui/conductor/src/dynamic/widgets/graphic/LayeredGraphic.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-card :height="props.height" class="overflow-hidden pa-3 d-flex flex-column">
+  <v-card :height="props.height" class="overflow-hidden pa-3 d-flex flex-column" :key="props.background">
     <v-card-title v-if="showTitle" class="text-h4 ma-0 pa-0 d-flex" style="z-index: 1">
       <span class="mr-auto">{{ props.title }}</span>
       <v-btn v-if="!props.fixed" icon="mdi-cog" variant="flat" @click="toggleSettings"/>
     </v-card-title>
-    <overlay-stack v-if="props.fixed">
-      <img v-if="bgSrc" :src="bgSrc" alt="Background for other layers">
+    <overlay-stack v-if="props.fixed" :style="stackStyle">
+      <img v-if="bgSrc" :src="bgSrc" alt="Background for other layers" ref="bgRef">
       <graphic-layer
           v-for="(layer, i) in visibleLayers"
           :key="layer.title ?? i"
@@ -14,10 +14,10 @@
           :selected="selectionsByLayer[i]"
           @update:selected="onLayerSelectUpdate(i, $event)"/>
     </overlay-stack>
-    <pinch-zoom v-else class="fill-height" :hide-controls="props.hideControls">
+    <pinch-zoom v-else class="fill-height" :hide-controls="props.hideControls" center>
       <template #default>
-        <overlay-stack>
-          <img v-if="bgSrc" :src="bgSrc" alt="Background for other layers">
+        <overlay-stack :style="stackStyle">
+          <img v-if="bgSrc" :src="bgSrc" alt="Background for other layers" ref="bgRef">
           <graphic-layer
               v-for="(layer, i) in visibleLayers"
               :key="layer.title ?? i"
@@ -35,12 +35,13 @@
 import {getMetadata} from '@/api/sc/traits/metadata.js';
 import OverlayStack from '@/components/zoom/OverlayStack.vue';
 import PinchZoom from '@/components/zoom/PinchZoom.vue';
-import DeviceSideBar from '@/routes/devices/components/DeviceSideBar.vue';
-import {useSidebarStore} from '@/stores/sidebar.js';
 import GraphicLayer from '@/dynamic/widgets/graphic/GraphicLayer.vue';
+import {useNaturalSize} from '@/dynamic/widgets/graphic/img.js';
 import LayeredGraphicSettings from '@/dynamic/widgets/graphic/LayeredGraphicSettings.vue';
 import {usePathUtils} from '@/dynamic/widgets/graphic/path.js';
 import {nameFromRequest} from '@/dynamic/widgets/graphic/traits.js';
+import DeviceSideBar from '@/routes/devices/components/DeviceSideBar.vue';
+import {useSidebarStore} from '@/stores/sidebar.js';
 import {computed, ref, watch} from 'vue';
 
 const props = defineProps({
@@ -64,6 +65,16 @@ const props = defineProps({
     type: Object,
     default: null
   },
+  // How many pixels are per meter in the graphic
+  pixelsPerMeter: {
+    type: [Number, String],
+    default: null // null means no scaling will be applied
+  },
+  // When rendering the graphic how many pixels should fit into a meter at scale 1
+  targetPixelsPerMeter: {
+    type: [Number, String],
+    default: 50
+  },
   layers: {
     type: Array,
     default: () => []
@@ -72,6 +83,19 @@ const props = defineProps({
 
 const {toPath} = usePathUtils();
 const bgSrc = computed(() => toPath(props.background?.svgPath));
+const bgRef = ref(null);
+const {naturalWidth} = useNaturalSize(bgRef);
+const scaledWidth = computed(() => {
+  if (!naturalWidth.value || !props.pixelsPerMeter || !props.targetPixelsPerMeter) return undefined;
+  return naturalWidth.value * props.targetPixelsPerMeter / props.pixelsPerMeter;
+});
+const stackStyle = computed(() => {
+  const res = {};
+  if (scaledWidth.value) {
+    res.width = `${scaledWidth.value}px`;
+  }
+  return res;
+});
 
 const showTitle = computed(() => Boolean(props.title || !props.fixed));
 
@@ -160,5 +184,7 @@ const onLayerSelectUpdate = (layerIdx, selected) => {
 </script>
 
 <style scoped>
-
+img {
+  min-width: 0;
+}
 </style>
