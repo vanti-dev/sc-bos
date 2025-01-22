a8a07c09660bd4cdf54c0b20924ba09264f632a4
Matt Nathan <matt.nathan@vanti.co.uk>
2025-01-22 14:50:00 +0000

    ui/ops: (lint) sort the rules in eslint config

--- ui/ops/.eslintrc.cjs
+++ ui/ops/.eslintrc.cjs
@@ -29,40 +29,39 @@ module.exports = {
     'linebreak-style': 0,
     // we have modern monitors these days
     'max-len': ['off'],
+    // allow errors and warnings to console
+    'no-console': ['warn', {allow: ['warn', 'error']}],
     // allow console debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
+    // we use the jsdoc plugin instead
+    'require-jsdoc': 'off',
+    'valid-jsdoc': 'off',
+
     'vue/attributes-order': 0,
+    // Arrange your attributes in any order you like
+    'vue/first-attribute-linebreak': ['off'],
+    'vue/html-closing-bracket-newline': ['error', {'multiline': 'never'}],
+    'vue/html-closing-bracket-spacing': ['error', {'selfClosingTag': 'never'}],
+    'vue/html-indent': ['error', 2, {'attribute': 2}],
+    'vue/max-attributes-per-line': [2, {'singleline': 10, 'multiline': 10}],
     // let us call things single words
     'vue/multi-word-component-names': 0,
-    'vue/html-closing-bracket-newline': ['error', {
-      'multiline': 'never'
-    }],
-    'vue/html-closing-bracket-spacing': ['error', {
-      'selfClosingTag': 'never'
-    }],
-    'vue/html-indent': ['error', 2, {
-      'attribute': 2
-    }],
-    'vue/max-attributes-per-line': [2, {
-      'singleline': 10,
-      'multiline': 10
-    }],
     // This rule is in conflict with the vue/no-v-for-template-key-on-child rule, which should be used instead.
     // This rule seems to be enabled by default, even though we're using vue 3, not sure why.
     'vue/no-v-for-template-key': 0,
+    // allow custom v-models
+    'vue/no-v-model-argument': 0,
     'vue/no-multiple-template-root': 0, // vue 3 has no problems with this
-    // Arrange your attributes in any order you like
-    'vue/first-attribute-linebreak': ['off'],
-    'vue/singleline-html-element-content-newline': ['off'],
     // 'turn on' script-setup-uses-vars to allow declaration of vars in <script setup> tags
     'vue/script-setup-uses-vars': 'error',
+    'vue/singleline-html-element-content-newline': ['off'],
+
     'vuetify/no-deprecated-colors': ['error', {
       // from src/plugins/vuetify.js
       themeColors: ['primary', 'secondary', 'primaryTeal', 'accent', 'neutral', 'error', 'success', 'info', 'warning']
     }],
-    // we use the jsdoc plugin instead
-    'valid-jsdoc': 'off',
-    'require-jsdoc': 'off',
+
     // because we haven't implemented modules correctly across most of our code
     'jsdoc/no-undefined-types': 0,
     // because it's the types that are more important, it's still preferred but we don't want
@@ -70,11 +69,6 @@ module.exports = {
     'jsdoc/require-param-description': 0,
     'jsdoc/require-property-description': 0,
     'jsdoc/require-returns-description': 0,
-    'no-unused-vars': 'warn',
-    // allow custom v-models
-    'vue/no-v-model-argument': 0,
-    // allow errors and warnings to console
-    'no-console': ['warn', {allow: ['warn', 'error']}],
     // tags should have no lines between them (the default), except @example which we allow any
     // number of lines between to allow better separation of these tags
     'jsdoc/tag-lines': ['warn', 'never', {
