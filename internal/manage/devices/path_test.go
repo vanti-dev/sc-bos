46375a60350c3b51e99714b6f2638e0b5c975927
Loveen <loveen.dyall@vanti.co.uk>
2025-01-28 11:40:28 +0000

    manage/devices: be more explicit about errors in parsePath
    I figure it's generally a good idea to be explicit about what error is being returned and test for this.
    
    It can also be a bad idea to return `errors.New("some error")`
    
    According to the documentation:
    >Each call to New returns a distinct error value even if the text is identical
    Example where this could be an issue: https://go.dev/play/p/MDAsU493G7n.

--- internal/manage/devices/path_test.go
+++ internal/manage/devices/path_test.go
@@ -1,6 +1,7 @@
 package devices
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -18,48 +19,48 @@ func Test_parsePath(t *testing.T) {
 	tests := []struct {
 		path    string
 		want    []pathSegment
-		wantErr bool
+		wantErr error
 	}{
-		{"", []pathSegment{}, true},
-		{".", []pathSegment{}, true},
-		{"a", []pathSegment{n("a")}, false},
-		{"a.", []pathSegment{n("a")}, true},
-		{".a", []pathSegment{}, true},
-		{"abc", []pathSegment{n("abc")}, false},
-		{"世界", []pathSegment{n("世界")}, false},
-		{"a.b", []pathSegment{n("a"), n("b")}, false},
-		{"a.b.", []pathSegment{n("a"), n("b")}, true},
-		{"a..b", []pathSegment{n("a")}, true},
-		{"a.世界", []pathSegment{n("a"), n("世界")}, false},
-		{"世.界", []pathSegment{n("世"), n("界")}, false},
-		{"世界.b", []pathSegment{n("世界"), n("b")}, false},
-		{"a[0]", []pathSegment{n("a"), i(0)}, false},
-		{"a[10]", []pathSegment{n("a"), i(10)}, false},
-		{"a[+10]", []pathSegment{n("a"), i(10)}, false},
-		{"a[-10]", []pathSegment{n("a"), i(-10)}, false},
-		{"a[foo]", []pathSegment{n("a")}, true},
-		{"世界[1]", []pathSegment{n("世界"), i(1)}, false},
-		{"a[1].b", []pathSegment{n("a"), i(1), n("b")}, false},
-		{"a.b[1]", []pathSegment{n("a"), n("b"), i(1)}, false},
-		{"a[", []pathSegment{n("a")}, true},
-		{"a[1", []pathSegment{n("a")}, true},
-		{"[", []pathSegment{}, true},
-		{"[1]", []pathSegment{i(1)}, false},
-		{"[1.1]", []pathSegment{}, true},
-		{"[1][2]", []pathSegment{i(1), i(2)}, false},
-		{"a[1][2]", []pathSegment{n("a"), i(1), i(2)}, false},
-		{"a[1][2].b", []pathSegment{n("a"), i(1), i(2), n("b")}, false},
-		{"a[1].b[2].c", []pathSegment{n("a"), i(1), n("b"), i(2), n("c")}, false},
-		{"[1]b", []pathSegment{i(1)}, true},
-		{"a[1]b", []pathSegment{n("a"), i(1)}, true},
-		{"a[1]b[2]", []pathSegment{n("a"), i(1)}, true},
-		{"a.[1]", []pathSegment{n("a")}, true},
-		{"a[1].[2]", []pathSegment{n("a"), i(1)}, true},
+		{"", []pathSegment{}, emptyPathErr},
+		{".", []pathSegment{}, leadingDotErr},
+		{"a", []pathSegment{n("a")}, nil},
+		{"a.", []pathSegment{n("a")}, parsingNameErr},
+		{".a", []pathSegment{}, leadingDotErr},
+		{"abc", []pathSegment{n("abc")}, nil},
+		{"世界", []pathSegment{n("世界")}, nil},
+		{"a.b", []pathSegment{n("a"), n("b")}, nil},
+		{"a.b.", []pathSegment{n("a"), n("b")}, parsingNameErr},
+		{"a..b", []pathSegment{n("a")}, parsingNameErr},
+		{"a.世界", []pathSegment{n("a"), n("世界")}, nil},
+		{"世.界", []pathSegment{n("世"), n("界")}, nil},
+		{"世界.b", []pathSegment{n("世界"), n("b")}, nil},
+		{"a[0]", []pathSegment{n("a"), i(0)}, nil},
+		{"a[10]", []pathSegment{n("a"), i(10)}, nil},
+		{"a[+10]", []pathSegment{n("a"), i(10)}, nil},
+		{"a[-10]", []pathSegment{n("a"), i(-10)}, nil},
+		{"a[foo]", []pathSegment{n("a")}, parsingIndexErr},
+		{"世界[1]", []pathSegment{n("世界"), i(1)}, nil},
+		{"a[1].b", []pathSegment{n("a"), i(1), n("b")}, nil},
+		{"a.b[1]", []pathSegment{n("a"), n("b"), i(1)}, nil},
+		{"a[", []pathSegment{n("a")}, parsingIndexErr},
+		{"a[1", []pathSegment{n("a")}, parsingIndexErr},
+		{"[", []pathSegment{}, parsingIndexErr},
+		{"[1]", []pathSegment{i(1)}, nil},
+		{"[1.1]", []pathSegment{}, parsingIndexErr},
+		{"[1][2]", []pathSegment{i(1), i(2)}, nil},
+		{"a[1][2]", []pathSegment{n("a"), i(1), i(2)}, nil},
+		{"a[1][2].b", []pathSegment{n("a"), i(1), i(2), n("b")}, nil},
+		{"a[1].b[2].c", []pathSegment{n("a"), i(1), n("b"), i(2), n("c")}, nil},
+		{"[1]b", []pathSegment{i(1)}, parsingPathErr},
+		{"a[1]b", []pathSegment{n("a"), i(1)}, parsingPathErr},
+		{"a[1]b[2]", []pathSegment{n("a"), i(1)}, parsingPathErr},
+		{"a.[1]", []pathSegment{n("a")}, parsingNameErr},
+		{"a[1].[2]", []pathSegment{n("a"), i(1)}, parsingNameErr},
 	}
 	for _, test := range tests {
 		t.Run(test.path, func(t *testing.T) {
 			ps, err := parsePath(test.path)
-			if (err != nil) != test.wantErr {
+			if !errors.Is(err, test.wantErr) {
 				t.Errorf("parsePath(%q) error = %v, wantErr %v", test.path, err, test.wantErr)
 			}
 			if diff := cmp.Diff(test.want, ps, cmpopts.EquateEmpty()); diff != "" {
