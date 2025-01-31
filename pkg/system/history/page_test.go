7040b8598b53de51e55f144af5fa7e95d093c66e
Matt Nathan <matt.nathan@vanti.co.uk>
2025-01-31 12:29:31 +0000

    history: don't call Slice.Len when it's unlikely to have changed (#337)
    Calling history.Slice.Len for each page has large performance implications on the application, especially in cases where many pages are being read. In situations where Len is backed by a database call it's generally implemented via a `select count(*) where d>%T AND d<=%T` which can be very slow.
    
    The problem is easy to avoid as the total size for a pagination operation is unlikely to change between page calls, so we solve this by calling Len for the first page and keeping that value in the page token for reuse by the next page calls.
    
    During testing this changed history download from ~60KB/s to ~2MB/s, or to put it another way downloading ~1m records now takes seconds instead of hours.

--- pkg/system/history/page_test.go
+++ pkg/system/history/page_test.go
@@ -13,32 +13,36 @@ import (
 
 func Test_unmarshalPageToken(t *testing.T) {
 	tests := []struct {
-		name    string
-		token   string
-		want    history.Record
-		wantErr bool
+		name       string
+		token      string
+		wantRecord history.Record
+		wantTotal  int
+		wantErr    bool
 	}{
-		{"empty", "", history.Record{}, true},
-		{"invalid", "!@`", history.Record{}, true},
-		{"bad proto", "abc", history.Record{}, true},
-		{"token.id", makeToken(&gen.HistoryRecord{Id: "foo"}), history.Record{ID: "foo"}, false},
+		{"empty", "", history.Record{}, 0, true},
+		{"invalid", "!@`", history.Record{}, 0, true},
+		{"bad proto", "abc", history.Record{}, 0, true},
+		{"token.id", makeToken(&gen.HistoryRecord{Id: "foo"}, 10), history.Record{ID: "foo"}, 10, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := unmarshalPageToken(tt.token)
+			gotRecord, gotTotal, err := unmarshalPageToken(tt.token)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("unmarshalPageToken() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("unmarshalPageToken() got = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(gotRecord, tt.wantRecord) {
+				t.Errorf("unmarshalPageToken() gotRecord = %v, wantRecord %v", gotRecord, tt.wantRecord)
+			}
+			if gotTotal != tt.wantTotal {
+				t.Errorf("unmarshalPageToken() gotTotal = %v, wantTotal %v", gotTotal, tt.wantTotal)
 			}
 		})
 	}
 }
 
-func makeToken(r *gen.HistoryRecord) string {
-	bs, err := proto.Marshal(r)
+func makeToken(r *gen.HistoryRecord, total int) string {
+	bs, err := proto.Marshal(&PageToken{Record: r, TotalSize: int32(total)})
 	if err != nil {
 		panic(err)
 	}
