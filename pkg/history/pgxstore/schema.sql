7afefd5a67a2676a4ca92939f9665c9372108a9d
Matt Nathan <matt.nathan@vanti.co.uk>
2025-02-05 15:19:42 +0000

    history: fix performance issues with reading history pages (#340)
    * history: fix performance issues with reading history pages
    
    This change adjusts the queries and indexes used by the postgres history store to be ~10,000x more performant. In general the changes involved replacing AND clauses with inner selects in certain cases where indexes could not be used for either selection or sorting.

--- pkg/history/pgxstore/schema.sql
+++ pkg/history/pgxstore/schema.sql
@@ -6,5 +6,7 @@ CREATE TABLE IF NOT EXISTS history
     payload     BYTEA       NULL
 );
 
-CREATE INDEX IF NOT EXISTS history_source_idx ON history (source);
-CREATE INDEX IF NOT EXISTS history_create_time_idx ON history (create_time);
+DROP INDEX IF EXISTS history_create_time_idx; -- replaced by history_id_source_idx;
+DROP INDEX IF EXISTS history_source_idx; -- replaced by history_source_create_time_idx
+CREATE INDEX IF NOT EXISTS history_id_source_idx ON history (id, source);
+CREATE INDEX IF NOT EXISTS history_source_create_time_idx ON history (source, create_time);
