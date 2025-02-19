0fe85c34cbaa5011378677f90468249e193dc5ff
Dean Redfern <dean.redfern@vanti.co.uk>
2025-02-19 11:38:12 +0000

    scripts: Support both versions of router/wrapper

--- scripts/gen-proto.sh
+++ scripts/gen-proto.sh
@@ -2,12 +2,26 @@
 
 pushd ../pkg/gen
 go generate
-# Remove imports of the gen package: `gen "github.com/vanti-dev/sc-bos/pkg/gen"`.
-sed -i.bak '/gen "github.com\/vanti-dev\/sc-bos\/pkg\/gen"/d' ../trait/gen/*.pb.go
-# Remove any `gen.` qualifiers on statements: `gen.Foo` => `Foo`.
-sed -i.bak 's/\bgen\.//g' ../trait/gen/*.pb.go
-sed -i.bak 's/[[:<:]]gen\.//g' ../trait/gen/*.pb.go
-mv -f ../trait/gen/*.pb.go .
+if [ -d ../trait/gen ]; then
+  # old version fixes to files
+  # Remove imports of the gen package: `gen "github.com/vanti-dev/sc-bos/pkg/gen"`.
+  sed -i.bak '/gen "github.com\/vanti-dev\/sc-bos\/pkg\/gen"/d' ../trait/gen/*.pb.go
+  # Remove any `gen.` qualifiers on statements: `gen.Foo` => `Foo`.
+  sed -i.bak 's/\bgen\.//g' ../trait/gen/*.pb.go
+  sed -i.bak 's/[[:<:]]gen\.//g' ../trait/gen/*.pb.go
+  mv -f ../trait/gen/*.pb.go .
+fi
+if [ -d ../trait/genpb ]; then
+  # new version fixes to files
+  # Remove imports of the gen package: `gen "github.com/vanti-dev/sc-bos/pkg/gen"`.
+  sed -i.bak '/gen "github.com\/vanti-dev\/sc-bos\/pkg\/gen"/d' ../trait/genpb/*.pb.go
+  # Remove any `gen.` qualifiers on statements: `gen.Foo` => `Foo`.
+  sed -i.bak 's/\bgen\.//g' ../trait/genpb/*.pb.go
+  sed -i.bak 's/[[:<:]]gen\.//g' ../trait/genpb/*.pb.go
+  # Rename the package from genpb to gen.
+  sed -i.bak 's/package genpb/package gen/g' ../trait/genpb/*.pb.go
+  mv -f ../trait/genpb/*.pb.go .
+fi
 rm -rf ../trait
 popd
 
