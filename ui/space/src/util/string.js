2d5595170250e513516502df8dba2b0d030aa3d5
Matt Nathan <matt.nathan@vanti.co.uk>
2025-02-20 09:38:51 +0000

    ui/space: add air quality widget (#348)
    * ui/space: add air quality widget
    
    Also support controlling which widgets are shows via the ui config.

---
+++ ui/space/src/util/string.js
@@ -0,0 +1,9 @@
+/**
+ * Inserts spaces into camelCased string
+ *
+ * @param {string} key
+ * @return {string}
+ */
+export function camelToSentence(key) {
+  return key.replace(/([A-Z])/g, ' $1');
+}
