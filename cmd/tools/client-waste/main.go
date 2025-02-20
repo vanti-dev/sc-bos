5cf9e8330362f90b03a2b236af86543484ecfffb
DeanR <dean.redfern@vanti.co.uk>
2025-02-20 14:54:09 +0000

    drivers/mock: add support for the waste trait

---
+++ cmd/tools/client-waste/main.go
@@ -0,0 +1,97 @@
+// Command client_waste provides a CLI tool for interacting with the [traits.WasteApiClient].
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/vanti-dev/sc-bos/pkg/util/client"
+)
+
+var clientConfig client.Config
+
+func init() {
+	flag.StringVar(&clientConfig.Endpoint, "endpoint", "localhost:23557", "smart core endpoint")
+	flag.BoolVar(&clientConfig.Get, "get", true, "perform a get request")
+	flag.BoolVar(&clientConfig.Pull, "pull", true, "pull changes")
+	flag.StringVar(&clientConfig.Name, "name", "", "smart core name for requests")
+	flag.BoolVar(&clientConfig.TLS.InsecureNoClientCert, "insecure-no-client-cert", false, "")
+	flag.BoolVar(&clientConfig.TLS.InsecureSkipVerify, "insecure-skip-verify", false, "")
+}
+
+func main() {
+	flag.Parse()
+
+	err := run()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+}
+
+func run() error {
+	log.Printf("dialling: %s", clientConfig.Endpoint)
+	conn, err := client.NewConnection(clientConfig)
+	if err != nil {
+		return err
+	}
+	log.Printf("dialled")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	wc := traits.NewWasteApiClient(conn)
+
+	get := func(c context.Context, name string) error {
+		req := traits.ListWasteRecordsRequest{Name: name}
+		for {
+			log.Printf("ListWasteRecords %s", name)
+			res, err := wc.ListWasteRecords(c, &req)
+			if err != nil {
+				return err
+			}
+			log.Printf("got %d events for %s", len(res.WasteRecords), name)
+			if res.NextPageToken == "" {
+				break
+			}
+			req.PageToken = res.NextPageToken
+		}
+		return nil
+	}
+
+	pull := func(c context.Context, name string) error {
+		log.Printf("PullWasteRecords %s", name)
+		stream, err := wc.PullWasteRecords(c, &traits.PullWasteRecordsRequest{Name: name})
+		if err != nil {
+			return err
+		}
+		if err != nil {
+			return err
+		}
+		for {
+			res, err := stream.Recv()
+			if err != nil {
+				return err
+			}
+			for _, change := range res.Changes {
+				log.Printf("got change for %s: %v", name, change.NewValue)
+			}
+		}
+	}
+
+	grp, ctx := errgroup.WithContext(ctx)
+	if clientConfig.Get {
+		grp.Go(func() error {
+			return get(ctx, clientConfig.Name)
+		})
+	}
+	if clientConfig.Pull {
+		grp.Go(func() error {
+			return pull(ctx, clientConfig.Name)
+		})
+	}
+
+	return grp.Wait()
+}
