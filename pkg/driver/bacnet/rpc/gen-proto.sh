38adf1f6225087ae43e5a57e66f8af6b0211cd08
deanredfern26 <52207282+deanredfern26@users.noreply.github.com>
2025-02-21 11:57:09 +0000

    scripts: add gen proto script for bacnet rpc (#352)

---
+++ pkg/driver/bacnet/rpc/gen-proto.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+go generate
+if [ -d ../../../../pkg/trait/rpc ]; then
+  sed -i.bak '/rpc "github.com\/vanti-dev\/sc-bos\/pkg\/driver\/bacnet\/rpc"/d' ../../../../pkg/trait/rpc/*.pb.go
+  sed -i.bak 's/\brpc\.//g' ../../../../pkg/trait/rpc/*.pb.go
+  sed -i.bak 's/package rpcpb/package rpc/g' ../../../../pkg/trait/rpc/*.pb.go
+  mv -f ../../../../pkg/trait/rpc/*.pb.go .
+fi
+if [ -d ../../../../pkg/trait/rpcpb ]; then
+  sed -i.bak '/rpc "github.com\/vanti-dev\/sc-bos\/pkg\/driver\/bacnet\/rpc"/d' ../../../../pkg/trait/rpcpb/*.pb.go
+  sed -i.bak 's/\brpc\.//g' ../../../../pkg/trait/rpcpb/*.pb.go
+  sed -i.bak 's/package rpcpb/package rpc/g' ../../../../pkg/trait/rpcpb/*.pb.go
+  mv -f ../../../../pkg/trait/rpcpb/*.pb.go .
+fi
+
+rm -rf ../../../../pkg/trait/
+
+
