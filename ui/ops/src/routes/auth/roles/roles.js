dd143c7ec88eead37b11c59804b4fd775ac93f87
Matt Nathan <matt.nathan@vanti.co.uk>
2025-03-14 17:38:29 +0000

    ui/ops/accounts: allow editing permissions for roles

---
+++ ui/ops/src/routes/auth/roles/roles.js
@@ -0,0 +1,66 @@
+import {usePermissionsStore} from '@/stores/permissions.js';
+import {computed, toValue} from 'vue';
+
+/**
+ * @typedef {Permission.AsObject} AssignedPermission
+ * @property {boolean} assigned
+ */
+
+/**
+ * @param {import('vue').MaybeRefOrGetter<string[]>} perms
+ * @return {{
+ *   toggleList: ComputedRef<AssignedPermission[]>,
+ *   assignedList: ComputedRef<AssignedPermission[]>
+ * }}
+ */
+export function useAssignedPermissions(perms) {
+  const permIndex = computed(() => toValue(perms).reduce((acc, perm) => {
+    acc[perm] = true;
+    return acc;
+  }, {}));
+  const permissionsStore = usePermissionsStore();
+  const toggleList = computed(() => permissionsStore.permissionsList.map((perm) => {
+    return {
+      ...perm,
+      assigned: permIndex.value[perm.id] ?? false,
+      implies: permissionsStore.impliedPermissions(perm.id),
+      dependsOn: permissionsStore.dependentPermissions(perm.id),
+    };
+  }));
+  const assignedList = computed(() => toggleList.value.filter((perm) => perm.assigned));
+
+  /**
+   * @param {string | AssignedPermission} idOrPerm
+   * @return {string[]}
+   */
+  const addPermission = (idOrPerm) => {
+    const perm = typeof idOrPerm === 'string' ? permissionsStore.permissionsById(idOrPerm) : idOrPerm;
+    if (!perm) {
+      return toValue(perms);
+    }
+    const idx = {...permIndex.value};
+    idx[perm.id] = true;
+    for (const impliedPermission of permissionsStore.impliedPermissions(perm.id)) {
+      idx[impliedPermission] = true;
+    }
+    return Object.keys(idx).sort();
+  }
+  const removePermission = (idOrPerm) => {
+    const perm = typeof idOrPerm === 'string' ? permissionsStore.permissionsById(idOrPerm) : idOrPerm;
+    if (!perm) {
+      return toValue(perms);
+    }
+    const idx = {...permIndex.value};
+    delete idx[perm.id];
+    for (const dependentPermission of permissionsStore.dependentPermissions(perm.id)) {
+      delete idx[dependentPermission];
+    }
+    return Object.keys(idx).sort();
+  }
+  return {
+    toggleList,
+    assignedList,
+    addPermission,
+    removePermission
+  };
+}
---
+++ ui/ops/src/stores/permissions.js
@@ -0,0 +1,132 @@
+import {acceptHMRUpdate, defineStore} from 'pinia';
+import {computed, ref} from 'vue';
+
+// todo: stop using a fixed permissions list
+/**
+ * @type {import('@vanti-dev/sc-bos-ui-gen/proto/account_pb').Permission.AsObject[]}
+ * @private
+ */
+const _permissions = [
+  {
+    'id': 'account:read',
+    'displayName': 'Account - Read All',
+    'description': 'Read-only access to all account details',
+    'impliesIdsList': [
+      'account:self:read'
+    ]
+  },
+  {
+    'id': 'account:self:read',
+    'displayName': 'Account - Read Own Account',
+    'description': 'Read details of your own account',
+    'impliesIdsList': []
+  },
+  {
+    'id': 'account:self:write',
+    'displayName': 'Account - Write Own Account',
+    'description': 'Update your own account details, including credentials',
+    'impliesIdsList': [
+      'account:self:read'
+    ]
+  },
+  {
+    'id': 'account:write',
+    'displayName': 'Account - Write All',
+    'description': 'Manage all accounts; Create and assign roles',
+    'impliesIdsList': [
+      'account:read'
+    ]
+  },
+  {
+    'id': 'traits:history:read',
+    'displayName': 'Traits - Read History',
+    'description': 'Read all historical data from devices',
+    'impliesIdsList': []
+  },
+  {
+    'id': 'traits:read',
+    'displayName': 'Traits - Read',
+    'description': 'Read all live data from devices',
+    'impliesIdsList': []
+  },
+  {
+    'id': 'traits:write',
+    'displayName': 'Traits - Write',
+    'description': 'Send commands to devices and update device state',
+    'impliesIdsList': [
+      'traits:read'
+    ]
+  }
+]
+
+export const usePermissionsStore = defineStore('permissions', () => {
+  const permissionsList = ref(_permissions);
+  const permissionsById = computed(() => permissionsList.value.reduce((acc, curr) => {
+    acc[curr.id] = curr
+    return acc
+  }, {}));
+
+  /**
+   * A map from permission id to a array of all transitively implied permissions.
+   *
+   * @type {ComputedRef<Record<string,string[]>>}
+   */
+  const impliedPermissions = computed(() => {
+    const implied = {};
+    const permById = permissionsById.value;
+    for (const perm of permissionsList.value) {
+      const idx = {};
+      const toAdd = [perm];
+      while (toAdd.length) {
+        const p = toAdd.pop();
+        if (idx[p.id]) {
+          continue;
+        }
+        idx[p.id] = true;
+        toAdd.push(...p.impliesIdsList.map((id) => permById[id]).filter((p) => p));
+      }
+      delete idx[perm.id];
+      implied[perm.id] = Object.keys(idx).sort();
+    }
+    return implied;
+  });
+  /**
+   * A map from permission id to a array of all permissions that imply it.
+   *
+   * @type {ComputedRef<Record<string, string[]>>}
+   */
+  const dependentPermissions = computed(() => {
+    const dependent = {};
+    for (const [dep, implies] of Object.entries(impliedPermissions.value)) {
+      if (!dependent[dep]) {
+        dependent[dep] = [];
+      }
+      for (const imply of implies) {
+        if (!dependent[imply]) {
+          dependent[imply] = [];
+        }
+        dependent[imply].push(dep);
+      }
+    }
+    return dependent;
+  })
+
+  return {
+    loading: ref(false),
+    loaded: ref(true),
+    permissionsList,
+    permissionsById(id) {
+      return permissionsById.value[id];
+    },
+    impliedPermissions(id) {
+      return impliedPermissions.value[id];
+    },
+    dependentPermissions(id) {
+      return dependentPermissions.value[id];
+    }
+  }
+});
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(usePermissionsStore, import.meta.hot));
+}
