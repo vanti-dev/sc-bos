9e6d6616a85a8f57aebee293accf1c54d441ac55
Matt Nathan <matt.nathan@vanti.co.uk>
2025-03-14 17:38:29 +0000

    ui/ops/account: allow editing permissions for roles

--- ui/ops/src/routes/auth/roles/RolesSideBar.vue
+++ ui/ops/src/routes/auth/roles/RolesSideBar.vue
@@ -20,15 +20,39 @@
     </template>
     <v-list density="compact">
       <v-list-subheader>{{ permissionsListTitle }}</v-list-subheader>
-      <v-list-item v-for="perm in permissionsList" :key="perm">
-        <v-list-item-title>{{ perm }}</v-list-item-title>
-      </v-list-item>
+      <v-expand-transition group>
+        <v-list-item
+            v-for="perm in permissionsToggleList" :key="perm.id"
+            density="compact"
+            @click="onPermissionClick(perm)">
+          <template #prepend>
+            <v-checkbox-btn :model-value="perm.assigned" color="primary" density="compact" class="mr-2"/>
+          </template>
+          <v-list-item-title>{{ perm.displayName }}</v-list-item-title>
+          <v-menu activator="parent" location="left" open-on-hover>
+            <v-card width="20em">
+              <v-card-title class="pb-0">{{ perm.displayName }}</v-card-title>
+              <v-card-subtitle>{{ perm.id }}</v-card-subtitle>
+              <v-card-text>{{ perm.description }}</v-card-text>
+              <template v-if="perm.implies.length > 0">
+                <v-card-subtitle class="pt-2">Implies</v-card-subtitle>
+                <v-card-text class="pt-0">{{ perm.implies.join(', ') }}</v-card-text>
+              </template>
+              <template v-if="perm.dependsOn.length > 0">
+                <v-card-subtitle class="pt-2">Depends On</v-card-subtitle>
+                <v-card-text class="pt-0">{{ perm.dependsOn.join(', ') }}</v-card-text>
+              </template>
+            </v-card>
+          </v-menu>
+        </v-list-item>
+      </v-expand-transition>
     </v-list>
   </side-bar>
 </template>
 
 <script setup>
 import SideBar from '@/components/SideBar.vue';
+import {useAssignedPermissions} from '@/routes/auth/roles/roles.js';
 import {useSidebarStore} from '@/stores/sidebar.js';
 import {computed, ref} from 'vue';
 import {useRouter} from 'vue-router';
@@ -48,13 +72,29 @@ const permissionsListTitle = computed(() => {
   }
 });
 
+const {
+  toggleList: permissionsToggleList,
+  addPermission, removePermission
+} = useAssignedPermissions(permissionsList);
+const onPermissionClick = async (perm) => {
+  const newRole = {
+    ...role.value
+  }
+  if (perm.assigned) {
+    newRole.permissionIdsList = removePermission(perm);
+  } else {
+    newRole.permissionIdsList = addPermission(perm);
+  }
+  await sidebar.data.updateRole({role: newRole, updateMask: ['permission_ids']});
+}
+
 const editMode = ref(false);
 const saving = ref(false);
 const saveError = ref(null);
 
 const router = useRouter();
 const onCloseClick = () => {
-  router.push({name: 'accounts'});
+  router.push({name: 'roles'});
 }
 const onEditClick = () => {
   editDescriptionModel.value = role.value?.description ?? '';
@@ -70,7 +110,7 @@ const onSaveClick = async () => {
       ...role.value,
       description: editDescriptionModel.value
     }
-    await sidebar.data.updateRole({role: newRole});
+    await sidebar.data.updateRole({role: newRole, updateMask: ['description']});
     saveError.value = null;
   } catch (e) {
     saveError.value = e;
