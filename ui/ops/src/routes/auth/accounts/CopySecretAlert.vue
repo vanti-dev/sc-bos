cf95e4a6e6a2786d10cddf653983a16a4cdc776d
Matt Nathan <matt.nathan@vanti.co.uk>
2025-03-17 10:43:06 +0000

    ui/ops/accounts: show more details in the account sidebar
    Also factor out CopyDiv to reuse a copyable bit of text

--- ui/ops/src/routes/auth/accounts/CopySecretAlert.vue
+++ ui/ops/src/routes/auth/accounts/CopySecretAlert.vue
@@ -7,25 +7,16 @@
       <p class="mb-3">
         Make sure to save your secret token somewhere private. You won't be able to see it again.
       </p>
-      <div class="secret d-flex align-center">
-        <pre class="ml-4 my-2 mr-auto text-h6">{{ props.credential }}</pre>
-        <v-btn :icon="secretCopied ? 'mdi-check' : 'mdi-content-copy'" variant="text" @click="onCopySecret">
-          <v-icon size="24"/>
-          <v-menu activator="parent"
-                  :open-on-click="false"
-                  :model-value="secretCopied"
-                  location="bottom"
-                  offset="6">
-            <v-card text="Secret copied to clipboard" color="success"/>
-          </v-menu>
-        </v-btn>
-      </div>
+      <copy-div class="secret" copied-text="Secret copied to clipboard">
+        <pre class="ml-4 my-2 text-h6">{{ props.credential }}</pre>
+      </copy-div>
     </template>
   </v-alert>
 </template>
 
 <script setup>
-import {onScopeDispose, ref, watch} from 'vue';
+
+import CopyDiv from '@/components/CopyDiv.vue';
 
 const props = defineProps({
   credential: {
@@ -33,22 +24,7 @@ const props = defineProps({
     default: undefined,
   }
 });
-const emit = defineEmits(["close"]);
-
-const secretCopied = ref(false);
-const onCopySecret = () => {
-  navigator.clipboard.writeText(props.credential);
-  secretCopied.value = true;
-}
-let secretCopiedTimeout = 0;
-watch(secretCopied, (value) => {
-  clearTimeout(secretCopiedTimeout);
-  if (!value) return;
-  secretCopiedTimeout = setTimeout(() => {
-    secretCopied.value = false;
-  }, 5000);
-});
-onScopeDispose(() => clearTimeout(secretCopiedTimeout));
+const emit = defineEmits(['close']);
 </script>
 
 <style scoped>
