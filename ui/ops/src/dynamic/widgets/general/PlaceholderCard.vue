8f52d1b4c2b2fd9c0ee52a20d31db0b61c67dad0
Matt Nathan <matt.nathan@vanti.co.uk>
2025-05-27 16:30:47 +0100

    ui/ops/widgets: add PlaceholderCard widget

---
+++ ui/ops/src/dynamic/widgets/general/PlaceholderCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card disabled/>
+</template>
+
+<style scoped>
+.v-card {
+  --c1: rgba(var(--v-theme-surface), .4);
+  --c2: rgba(var(--v-theme-error), 0.2);
+  background-color: var(--c2);
+  background-image: linear-gradient(45deg,
+  var(--c1) 45%,
+  var(--c2) 45%, var(--c2) 50%,
+  var(--c1) 50%, var(--c1) 95%,
+  var(--c2) 95%, var(--c2) 100%);
+  background-repeat: repeat;
+  background-attachment: fixed;
+  background-size: 40px 40px;
+}
+</style>
