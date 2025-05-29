1f47603334d0d80be579c20d4fe3d72a09e401d1
Matt Nathan <matt.nathan@vanti.co.uk>
2025-05-29 10:01:03 +0100

    ui/ops/widgets: add more display options to OccupancyCard
    Things like hiding the header, hiding totals, etc.

--- ui/ops/src/dynamic/widgets/occupancy/OccupancyCard.vue
+++ ui/ops/src/dynamic/widgets/occupancy/OccupancyCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <content-card class="d-flex flex-column pa-8">
-    <div class="d-flex flex-row mb-6 pl-4 pr-2 align-baseline">
-      <h4 class="text-h4 ma-0">Occupancy</h4>
-      <v-spacer/>
-      <occupancy-people-count :people-count="peopleCount" :error="streamError" class="text-h2"/>
-    </div>
-    <occupancy-graph class="flex-grow-1" :source="historyName" style="min-height: 300px"/>
-  </content-card>
+  <v-card class="d-flex flex-column px-8 py-4" :class="props.class" :style="props.style">
+    <v-toolbar v-if="!hideTitle" color="transparent" class="mb-4">
+      <v-toolbar-title class="text-h4">{{ title }}</v-toolbar-title>
+      <template v-if="!hideTotals">
+        <occupancy-people-count :people-count="peopleCount" :error="streamError" class="text-h2 align-self-center"/>
+      </template>
+    </v-toolbar>
+    <occupancy-graph class="flex-grow-1" :source="historyName" :span="span" v-bind="$attrs"/>
+  </v-card>
 </template>
 
 <script setup>
-import ContentCard from '@/components/ContentCard.vue';
-import {useOccupancy, usePullOccupancy} from '@/traits/occupancy/occupancy.js';
 import OccupancyGraph from '@/dynamic/widgets/occupancy/OccupancyGraph.vue';
+import {useOccupancy, usePullOccupancy} from '@/traits/occupancy/occupancy.js';
 import {computed} from 'vue';
 import OccupancyPeopleCount from './OccupancyPeopleCount.vue';
 
 const props = defineProps({
+  class: {type: [String, Array, Object], default: null},
+  style: {type: [String, Array, Object], default: null},
   source: {
     type: String,
     default: null
@@ -24,8 +26,25 @@ const props = defineProps({
   history: {
     type: String,
     default: null
+  },
+  title: {
+    type: String,
+    default: 'Occupancy'
+  },
+  hideTitle: {
+    type: Boolean,
+    default: false
+  },
+  hideTotals: {
+    type: Boolean,
+    default: false
+  },
+  span: {
+    type: Number,
+    default: undefined, // in ms
   }
 });
+defineOptions({inheritAttrs: false});
 
 const sourceName = computed(() => props.source);
 const historyName = computed(() => props.history ?? sourceName.value);
