5e0b4813061fac8b63d3eeb631609f63c93004e6
deanredfern26 <52207282+deanredfern26@users.noreply.github.com>
2025-05-29 15:40:43 +0100

    drivers/opcua: add driver for opcua (#411)
    * drivers/opcua: add driver for opcua
    
    * run go mod tidy
    
    * driver/opcua: add meter info support
    
    - announce transport info
    - use service.WithParser to check config & node IDs
    - add random client handle
    
    * driver/opcua: add omitzero tag to all json durations and refactor announcer
    
    * use ua.NodeID over string and define equals method

---
+++ pkg/driver/opcua/driver.go
@@ -0,0 +1,128 @@
+package opcua
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/gopcua/opcua"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+
+	"github.com/vanti-dev/sc-bos/pkg/driver"
+	"github.com/vanti-dev/sc-bos/pkg/gen"
+	"github.com/vanti-dev/sc-bos/pkg/gentrait/meter"
+	"github.com/vanti-dev/sc-bos/pkg/gentrait/transport"
+	"github.com/vanti-dev/sc-bos/pkg/gentrait/udmipb"
+	"github.com/vanti-dev/sc-bos/pkg/node"
+	"github.com/vanti-dev/sc-bos/pkg/task/service"
+
+	"github.com/vanti-dev/sc-bos/pkg/driver/opcua/config"
+)
+
+const DriverName = "opcua"
+
+var Factory driver.Factory = factory{}
+
+type factory struct{}
+
+func (f factory) New(services driver.Services) service.Lifecycle {
+	logger := services.Logger.Named(DriverName)
+
+	d := &Driver{
+		announcer: node.NewReplaceAnnouncer(services.Node),
+	}
+	d.Service = service.New(
+		service.MonoApply(d.applyConfig),
+		service.WithParser(config.ReadBytes),
+		service.WithRetry[config.Root](service.RetryWithLogger(func(logContext service.RetryContext) {
+			logContext.LogTo("applyConfig", logger)
+		}), service.RetryWithMinDelay(5*time.Second), service.RetryWithInitialDelay(5*time.Second)),
+	)
+	d.logger = services.Logger.Named(DriverName)
+	return d
+}
+
+type Driver struct {
+	*service.Service[config.Root]
+	logger    *zap.Logger
+	announcer *node.ReplaceAnnouncer
+}
+
+func (d *Driver) applyConfig(ctx context.Context, cfg config.Root) error {
+
+	a := d.announcer.Replace(ctx)
+
+	opcClient, err := opcua.NewClient(cfg.Conn.Endpoint)
+	if err != nil {
+		d.logger.Warn("NewClient error", zap.String("error", err.Error()))
+		return err
+	}
+
+	err = opcClient.Connect(ctx)
+	if err != nil {
+		d.logger.Warn("Connect error", zap.String("error", err.Error()))
+		return err
+	}
+
+	client := NewClient(opcClient, d.logger, cfg.Conn.SubscriptionInterval.Duration, cfg.Conn.ClientId)
+
+	if cfg.Meta != nil {
+		a.Announce(cfg.Name, node.HasMetadata(cfg.Meta))
+	}
+
+	grp, ctx := errgroup.WithContext(ctx)
+	var errs error
+	for _, dev := range cfg.Devices {
+		var allFeatures []node.Feature
+		opcDev := NewDevice(&dev, d.logger, client)
+
+		for _, t := range dev.Traits {
+			switch t.Kind {
+			case meter.TraitName:
+				opcDev.meter, err = newMeter(dev.Name, t, d.logger)
+				if err != nil {
+					errs = fmt.Errorf("failed to add trait for device %s: %w", dev.Name, err)
+				} else {
+					allFeatures = append(allFeatures, node.HasTrait(meter.TraitName, node.WithClients(gen.WrapMeterApi(opcDev.meter), gen.WrapMeterInfo(opcDev.meter))))
+				}
+			case transport.TraitName:
+				opcDev.transport, err = newTransport(dev.Name, t, d.logger)
+				if err != nil {
+					errs = fmt.Errorf("failed to add trait for device %s: %w", dev.Name, err)
+				} else {
+					allFeatures = append(allFeatures, node.HasTrait(transport.TraitName, node.WithClients(gen.WrapTransportApi(opcDev.transport), gen.WrapTransportInfo(opcDev.transport))))
+				}
+			case udmipb.TraitName:
+				opcDev.udmi, err = newUdmi(dev.Name, t, d.logger)
+				if err != nil {
+					errs = fmt.Errorf("failed to add trait for device %s: %w", dev.Name, err)
+				} else {
+					allFeatures = append(allFeatures, node.HasTrait(udmipb.TraitName, node.WithClients(gen.WrapUdmiService(opcDev.udmi))))
+				}
+			default:
+				d.logger.Error("unknown trait", zap.String("trait", t.Name))
+			}
+		}
+
+		if dev.Meta != nil {
+			allFeatures = append(allFeatures, node.HasMetadata(dev.Meta))
+		}
+
+		if errs != nil {
+			d.logger.Error("errors encountered whilst loading driver", zap.String("device", dev.Name), zap.Error(errs))
+		}
+
+		a.Announce(dev.Name, allFeatures...)
+		grp.Go(func() error {
+			return opcDev.run(ctx)
+		})
+	}
+
+	go func() {
+		err := grp.Wait()
+		d.logger.Error("run error", zap.String("error", err.Error()))
+		_ = opcClient.Close(ctx)
+	}()
+	return nil
+}
