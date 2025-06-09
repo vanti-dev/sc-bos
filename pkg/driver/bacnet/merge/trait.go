884550ad78d7b746a53b86fc0aa9badca57e1ffc
deanredfern26 <52207282+deanredfern26@users.noreply.github.com>
2025-06-09 12:57:49 +0100

    driver/bacnet: add support for security event trait (#419)
    * driver/bacnet: add support for security event trait
    
    * add logic to prevent consistent security events
    
    * only append security event when it is not nil
    
    * only add security event when it is not nil
    
    * order securityevent case after mode
    
    * change type of EventType to *gen.SecurityEvent_EventType
    
    * separate security event config from runtime state

--- pkg/driver/bacnet/merge/trait.go
+++ pkg/driver/bacnet/merge/trait.go
@@ -8,6 +8,7 @@ import (
 	"github.com/vanti-dev/sc-bos/pkg/driver/bacnet/config"
 	"github.com/vanti-dev/sc-bos/pkg/driver/bacnet/known"
 	"github.com/vanti-dev/sc-bos/pkg/gentrait/meter"
+	"github.com/vanti-dev/sc-bos/pkg/gentrait/securityevent"
 	"github.com/vanti-dev/sc-bos/pkg/gentrait/statuspb"
 	"github.com/vanti-dev/sc-bos/pkg/gentrait/udmipb"
 	"github.com/vanti-dev/sc-bos/pkg/node"
@@ -32,6 +33,8 @@ func IntoTrait(client *gobacnet.Client, devices known.Context, statuses *statusp
 		return newMeter(client, devices, statuses, traitConfig, logger)
 	case trait.Mode:
 		return newMode(client, devices, statuses, traitConfig, logger)
+	case securityevent.TraitName:
+		return newSecurityEvent(client, devices, statuses, traitConfig, logger)
 	case statuspb.TraitName:
 		return newStatus(client, devices, statuses, traitConfig, logger)
 	case UdmiMergeName, udmipb.TraitName:
