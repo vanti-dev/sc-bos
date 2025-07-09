4008e77c31e864d5562a31c0698a2b4b196af655
Matt Nathan <matt.nathan@vanti.co.uk>
2025-07-09 15:46:11 +0100

    ui/ops/ops: allow security events to be configured as a dashboard page

--- ui/ops/src/dynamic/layout/pallet.js
+++ ui/ops/src/dynamic/layout/pallet.js
@@ -4,5 +4,6 @@ export const builtinLayouts = {
   'LayoutMainSide': defineAsyncComponent(() => import('@/dynamic/layout/LayoutMainSide.vue')),
   'LayoutGrid': defineAsyncComponent(() => import('@/dynamic/layout/LayoutGrid.vue')),
   // full pages, not widget containers
+  'page/SecurityEvents': defineAsyncComponent(() => import('@/routes/ops/security-events/SecurityEventsTable.vue')),
   'page/Waste': defineAsyncComponent(() => import('@/routes/ops/waste/WasteTable.vue'))
 };
