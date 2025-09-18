98c933106c51319303eca1aeee2aa0691739f64b
Matt Nathan <matt.nathan@vanti.co.uk>
2025-09-18 11:41:02 +0100

    internal/proto/protopath2: add new package based on pending change to upstream proto module
    I don't know when the upstream will land, but we need this now so copy it in.

---
+++ internal/protobuf/protopath2/doc.go
@@ -0,0 +1,4 @@
+// Package protopath2 provides path parsing and traversal.
+// The package is copied from https://go-review.googlesource.com/c/protobuf/+/582735 which is has not yet landed in the main protobuf module.
+// See https://github.com/golang/protobuf/issues/1612 for discussion.
+package protopath2
