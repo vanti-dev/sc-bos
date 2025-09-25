1ae8e0b30f46cb5cbe6eba5ab0c356bbee598166
deanredfern26 <deanredfern26@gmail.com>
2025-09-25 11:46:38 +0100

    zones: add on/off support and add on/off zone to Vanti ugs example

---
+++ pkg/zone/feature/onoff/onoff.go
@@ -0,0 +1,58 @@
+package onoff
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-golang/pkg/trait"
+	"github.com/smart-core-os/sc-golang/pkg/trait/onoffpb"
+	"github.com/vanti-dev/sc-bos/pkg/node"
+	"github.com/vanti-dev/sc-bos/pkg/task/service"
+	"github.com/vanti-dev/sc-bos/pkg/zone"
+	"github.com/vanti-dev/sc-bos/pkg/zone/feature/onoff/config"
+)
+
+var Feature = zone.FactoryFunc(func(services zone.Services) service.Lifecycle {
+	services.Logger = services.Logger.Named("onoff")
+	f := &feature{
+		announcer: node.NewReplaceAnnouncer(services.Node),
+		devices:   services.Devices,
+		clients:   services.Node,
+		logger:    services.Logger,
+	}
+	f.Service = service.New(service.MonoApply(f.applyConfig))
+	return f
+})
+
+type feature struct {
+	*service.Service[config.Root]
+	announcer *node.ReplaceAnnouncer
+	devices   *zone.Devices
+	clients   node.ClientConner
+	logger    *zap.Logger
+}
+
+func (f *feature) applyConfig(ctx context.Context, cfg config.Root) error {
+	announce := f.announcer.Replace(ctx)
+	logger := f.logger
+
+	client := traits.NewOnOffApiClient(f.clients.ClientConn())
+	announceGroup := func(name string, devices []string) {
+		if len(devices) == 0 {
+			return
+		}
+
+		group := &Group{
+			client: client,
+			names:  devices,
+			logger: logger,
+		}
+		f.devices.Add(devices...)
+		announce.Announce(name, node.HasTrait(trait.OnOff, node.WithClients(onoffpb.WrapApi(group))))
+	}
+
+	announceGroup(cfg.Name, cfg.OnOffs)
+	return nil
+}
