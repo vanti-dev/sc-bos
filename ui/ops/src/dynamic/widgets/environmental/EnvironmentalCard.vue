8384a019df63aac9efe2a2fa968200a36e50c6c6
deanredfern26 <deanredfern26@gmail.com>
2025-10-01 15:09:49 +0100

    ui/ops: add sound sensor apit & environmental card support

--- ui/ops/src/dynamic/widgets/environmental/EnvironmentalCard.vue
+++ ui/ops/src/dynamic/widgets/environmental/EnvironmentalCard.vue
@@ -38,6 +38,21 @@
           Avg. Humidity
         </template>
       </circular-gauge>
+      <circular-gauge
+          v-if="indoorSoundLevel > 0"
+          :value="indoorSoundLevel"
+          :color="props.gaugeColor"
+          :min="0"
+          :max="85"
+          segments="30"
+          class="mt-7 mx-6">
+        <span class="align-baseline text-h1 ml-2">
+          {{ indoorSoundLevelStr }}<span style="font-size: 0.7em;">dB</span>
+        </span>
+        <template #title>
+          Avg. Humidity
+        </template>
+      </circular-gauge>
     </v-card-text>
   </content-card>
 </template>
@@ -47,6 +62,7 @@ import CircularGauge from '@/components/CircularGauge.vue';
 import ContentCard from '@/components/ContentCard.vue';
 
 import {useAirTemperature, usePullAirTemperature} from '@/traits/airTemperature/airTemperature.js';
+import {usePullSoundLevel} from '@/traits/sound/sound.js';
 import {isNullOrUndef} from '@/util/types.js';
 import {computed} from 'vue';
 
@@ -62,6 +78,10 @@ const props = defineProps({
   gaugeColor: {
     type: String,
     default: 'primary'
+  },
+  soundSensor : {
+    type: String,
+    default: null
   }
 });
 
@@ -73,6 +93,7 @@ const {
 } = useAirTemperature(indoorValue);
 const {value: outdoorValue} = usePullAirTemperature(() => props.external);
 const {temp: outdoorTemperature} = useAirTemperature(outdoorValue);
+const {soundLevel: indoorSoundLevel} = usePullSoundLevel(() => props.soundSensor);
 
 const vOrDash = (r) => {
   const v = r.value ?? '-';
---
+++ ui/ops/src/traits/sound/sound.js
@@ -0,0 +1,70 @@
+
+/**
+ * @typedef {import('@vanti-dev/sc-bos-ui-gen/proto/sound_sensor_pb').SoundLevel} SoundLevel
+ * @typedef {import('@vanti-dev/sc-bos-ui-gen/proto/sound_sensor_pb').SoundLevelSupport} SoundLevelSupport
+ * @typedef {import('@vanti-dev/sc-bos-ui-gen/proto/sound_sensor_pb').PullSoundLevelRequest} PullSoundLevelRequest
+ * @typedef {import('@vanti-dev/sc-bos-ui-gen/proto/sound_sensor_pb').PullSoundLevelResponse} PullSoundLevelResponse
+ * @typedef {import('@vanti-dev/sc-bos-ui-gen/proto/sound_sensor_pb').DescribeSoundLevelRequest} DescribeSoundLevelRequest
+ * @typedef {import('vue').ComputedRef} ComputedRef
+ * @typedef {import('vue').Ref} Ref
+ * @typedef {import('vue').ToRefs} ToRefs
+ * @typedef {import('@/api/resource').ResourceValue} ResourceValue
+ * @typedef {import('@/api/resource').ActionTracker} ActionTracker
+ */
+
+import {closeResource, newActionTracker, newResourceValue} from '@/api/resource.js';
+import {describeSoundLevel, pullSoundLevel} from '@/api/sc/traits/sound-sensor.js';
+import {toQueryObject, watchResource} from '@/util/traits.js';
+import {computed, onScopeDispose, reactive, toRefs, toValue} from 'vue';
+
+/**
+ * @param {MaybeRefOrGetter<string|PullSoundLevelRequest.AsObject>} query - The name of the device or a query object
+ * @param {MaybeRefOrGetter<boolean>=} paused - Whether to pause the data stream
+ * @return {ToRefs<ResourceValue<SoundLevel.AsObject, PullSoundLevelResponse>>}
+ */
+export function usePullSoundLevel(query, paused = false) {
+  const resource = reactive(
+      /** @type {ResourceValue<SoundLevel.AsObject, PullSoundLevelResponse>} */
+      newResourceValue()
+  );
+  onScopeDispose(() => closeResource(resource));
+
+  const queryObject = computed(() => toQueryObject(query));
+
+  watchResource(
+      () => toValue(queryObject),
+      () => toValue(paused),
+      (req) => {
+        pullSoundLevel(req, resource);
+        return () => closeResource(resource);
+      }
+  );
+
+  return toRefs(resource);
+}
+
+/**
+ * @param {MaybeRefOrGetter<string|DescribeSoundLevelRequest.AsObject>} query - The name of the device or a query
+ *   object
+ * @return {ToRefs<ActionTracker<SoundLevelSupport.AsObject>>}
+ */
+export function useDescribeSoundLevel(query) {
+  const tracker = reactive(
+      /** @type {ActionTracker<SoundLevelSupport.AsObject>} */
+      newActionTracker()
+  );
+  
+  const queryObject = computed(() => toQueryObject(query));
+  
+  watchResource(
+      () => toValue(queryObject),
+      false,
+      (req) => {
+        describeSoundLevel(req, tracker)
+          .catch(() => {}); // errors are tracked by tracker
+        return () => closeResource(tracker);
+      }
+  );
+ 
+ return toRefs(tracker);
+}
