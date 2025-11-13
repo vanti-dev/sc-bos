7caa088e8fdde4687ed19857a6b27a5dc4a1f016
deanredfern26 <deanredfern26@gmail.com>
2025-11-13 10:25:36 +0000

    remove debug

--- pkg/driver/helvarnet/light.go
+++ pkg/driver/helvarnet/light.go
@@ -699,7 +699,6 @@ func (l *Light) loadTestResults() error {
 
 func (l *Light) saveTestResults() error {
 
-	l.logger.Debug("Saving test results", zap.String("name", l.conf.Name))
 	value := l.testResultSet.Get().(*gen.TestResultSet)
 	var testResult TestResults
 	if value.FunctionTest != nil {
